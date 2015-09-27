815ab6741d9e64d0d62d4bb05795c24d212138e5
Tianchenglipu <670204195@qq.com>
2015-09-28 00:12:39 +0800

    Update c81816475.lua

--- c81816475.lua
+++ c81816475.lua
@@ -17,19 +17,28 @@ end
 function c81816475.filter(c)
 	return c:IsFaceup() and c:IsType(TYPE_XYZ)
 end
+function c81816475.cfilter(c)
+	return c:GetOverlayCount()>0
+end
 function c81816475.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetOverlayCount(tp,0,1)~=0
 		and Duel.IsExistingMatchingCard(c81816475.filter,tp,LOCATION_MZONE,0,1,nil) end
 end
 function c81816475.operation(e,tp,eg,ep,ev,re,r,rp)
-	local g1=Duel.GetOverlayGroup(tp,0,1)
+	local sg=Duel.GetMatchingGroup(c81816475.cfilter,tp,0,LOCATION_MZONE,nil)
+	if sg:GetCount()>1 then
+		Duel.Hint(HINT_SELECTMSG,tp,532)
+		sg=sg:Select(tp,1,1,nil)
+		Duel.HintSelection(sg)
+	end
+	local oc=sg:GetFirst()
+	local g1=oc:GetOverlayGroup()
 	local g2=Duel.GetMatchingGroup(c81816475.filter,tp,LOCATION_MZONE,0,nil)
 	if g1:GetCount()==0 or g2:GetCount()==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(81816475,2))
 	local mg=g1:Select(tp,1,1,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(81816475,1))
 	local tc=g2:Select(tp,1,1,nil):GetFirst()
-	local oc=mg:GetFirst():GetOverlayTarget()
 	Duel.Overlay(tc,mg)
 	Duel.RaiseSingleEvent(oc,EVENT_DETACH_MATERIAL,e,0,0,0,0)
 end
