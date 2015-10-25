a16e562284f3631c264a9e4095b6925ccaece09f
VanillaSalt <vanillasalt5197@gmail.com>
2015-10-25 11:00:34 +0900

    fix

--- c14733538.lua
+++ c14733538.lua
@@ -19,14 +19,14 @@ function c14733538.filter2(c,e,tp,b1)
 		and (b1 or c:IsCanBeSpecialSummoned(e,0,tp,false,false))
 end
 function c14733538.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local b1=Duel.CheckLocation(tp,LOCATION_SZONE,6) and Duel.CheckLocation(tp,LOCATION_SZONE,7)
+	local b1=Duel.CheckLocation(tp,LOCATION_SZONE,6) or Duel.CheckLocation(tp,LOCATION_SZONE,7)
 	local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 	if chk==0 then return (b1 or b2)
 		and Duel.IsExistingMatchingCard(c14733538.filter1,tp,LOCATION_DECK,0,1,nil,e,tp,b1)
 		and Duel.IsExistingMatchingCard(c14733538.filter2,tp,LOCATION_DECK,0,1,nil,e,tp,b1) end
 end
 function c14733538.activate(e,tp,eg,ep,ev,re,r,rp)
-	local b1=Duel.CheckLocation(tp,LOCATION_SZONE,6) and Duel.CheckLocation(tp,LOCATION_SZONE,7)
+	local b1=Duel.CheckLocation(tp,LOCATION_SZONE,6) or Duel.CheckLocation(tp,LOCATION_SZONE,7)
 	local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 	if not b1 and not b2 then return end
 	local g1=Duel.GetMatchingGroup(c14733538.filter1,tp,LOCATION_DECK,0,nil,e,tp,b1)
