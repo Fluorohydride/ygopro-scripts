2b35ff0bc61e6acb9050d39f8f22f7050f1f5e44
Tianchenglipu <670204195@qq.com>
2015-10-25 11:57:42 +0800

    Update c14733538.lua

--- c14733538.lua
+++ c14733538.lua
@@ -26,7 +26,7 @@ function c14733538.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		and Duel.IsExistingMatchingCard(c14733538.filter2,tp,LOCATION_DECK,0,1,nil,e,tp,b1) end
 end
 function c14733538.activate(e,tp,eg,ep,ev,re,r,rp)
-	local b1=Duel.CheckLocation(tp,LOCATION_SZONE,6) or Duel.CheckLocation(tp,LOCATION_SZONE,7)
+	local b1=not (Duel.CheckLocation(tp,LOCATION_SZONE,6) and Duel.CheckLocation(tp,LOCATION_SZONE,7))
 	local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 	if not b1 and not b2 then return end
 	local g1=Duel.GetMatchingGroup(c14733538.filter1,tp,LOCATION_DECK,0,nil,e,tp,b1)
