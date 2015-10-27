5f498f8fe43170d612c77f1187805f49d236c31f
Lsty <281606174@qq.com>
2015-10-27 21:36:08 +0800

    Update c4333086.lua
    按照神鸟攻击的写法进行了修复

--- c4333086.lua
+++ c4333086.lua
@@ -13,24 +13,54 @@ function c4333086.initial_effect(c)
 	e1:SetOperation(c4333086.activate)
 	c:RegisterEffect(e1)
 end
-function c4333086.rfilter(c)
-	return c:IsRace(RACE_ZOMBIE) and Duel.IsExistingTarget(Card.IsDestructable,0,LOCATION_ONFIELD,LOCATION_ONFIELD,2,c)
-end
 function c4333086.filter(c)
 	return c:IsSetCard(0xd9) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemove()
 end
 function c4333086.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroup(tp,c4333086.rfilter,1,nil) end
-	local g=Duel.SelectReleaseGroup(tp,c4333086.rfilter,1,1,nil)
-	Duel.Release(g,REASON_COST)
+	e:SetLabel(1)
+	return true
+end
+function c4333086.costfilter(c,e,dg)
+	if not c:IsRace(RACE_ZOMBIE) then return false end
+	local a=0
+	if dg:IsContains(c) then a=1 end
+	if c:GetEquipCount()==0 then return dg:GetCount()-a>=2 end
+	local eg=c:GetEquipGroup()
+	local tc=eg:GetFirst()
+	while tc do
+		if dg:IsContains(tc) then a=a+1 end
+		tc=eg:GetNext()
+	end
+	return dg:GetCount()-a>=2
+end
+function c4333086.tgfilter(c,e)
+	return c:IsDestructable() and c:IsCanBeEffectTarget(e)
 end
 function c4333086.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and chkc:IsDestructable() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,2,nil)
-		and Duel.IsExistingMatchingCard(c4333086.filter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then
+		if chkc then return chkc:IsOnField() and chkc:IsDestructable() end
+		if e:GetLabel()==1 then
+			e:SetLabel(0)
+			local rg=Duel.GetReleaseGroup(tp)
+			local dg=Duel.GetMatchingGroup(c4333086.tgfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler(),e)
+			local res=rg:IsExists(c4333086.costfilter,1,e:GetHandler(),e,dg)
+			return res and Duel.IsExistingMatchingCard(c4333086.filter,tp,LOCATION_DECK,0,1,nil)
+		else
+			return Duel.IsExistingTarget(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,2,e:GetHandler())
+				and Duel.IsExistingMatchingCard(c4333086.filter,tp,LOCATION_DECK,0,1,nil)
+		end
+	end
+	if e:GetLabel()==1 then
+		e:SetLabel(0)
+		local rg=Duel.GetReleaseGroup(tp)
+		local dg=Duel.GetMatchingGroup(c4333086.tgfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler(),e)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		local sg=rg:FilterSelect(tp,c4333086.costfilter,1,1,e:GetHandler(),e,dg)
+		Duel.Release(sg,REASON_COST)
+	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,2,2,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,2,0,0)
+	local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,2,2,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 end
 function c4333086.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
