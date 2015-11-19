70cb8126e3e0772a0490c089490a55b436a2e47f
Tianchenglipu <670204195@qq.com>
2015-11-19 19:51:45 +0800

    Update c97466438.lua

--- c97466438.lua
+++ c97466438.lua
@@ -1,9 +1,10 @@
 --ヨコシマウマ
 function c97466438.initial_effect(c)
 	--pendulum summon
-	aux.AddPendulumProcedure(c)
+	aux.EnablePendulumAttribute(c,false)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(1160)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(c97466438.reg)
