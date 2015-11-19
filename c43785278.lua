cfff5d81f407bbd47b09451f91a9776492ad38db
Tianchenglipu <670204195@qq.com>
2015-11-19 19:08:10 +0800

    Update c43785278.lua

--- c43785278.lua
+++ c43785278.lua
@@ -1,7 +1,7 @@
 --フーコーの魔砲石
 function c43785278.initial_effect(c)
 	--pendulum summon
-	aux.EnablePendulumAttribute(c,true)
+	aux.EnablePendulumAttribute(c,false)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(1160)
