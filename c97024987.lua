53fcca3cd2e10637fa071501832fdb1ae5471a0f
Tianchenglipu <670204195@qq.com>
2015-11-19 18:15:05 +0800

    Update c97024987.lua

--- c97024987.lua
+++ c97024987.lua
@@ -1,21 +1,16 @@
 --イグナイト・ドラグノフ
 function c97024987.initial_effect(c)
 	--pendulum summon
-	aux.AddPendulumProcedure(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
+	aux.EnablePendulumAttribute(c)
 	--tohand
-	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_PZONE)
-	e2:SetCondition(c97024987.thcon)
-	e2:SetTarget(c97024987.thtg)
-	e2:SetOperation(c97024987.thop)
-	c:RegisterEffect(e2)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCondition(c97024987.thcon)
+	e1:SetTarget(c97024987.thtg)
+	e1:SetOperation(c97024987.thop)
+	c:RegisterEffect(e1)
 end
 function c97024987.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local seq=e:GetHandler():GetSequence()
