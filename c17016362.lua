5bfcf6f94255fb01d56b3065b2ce0bc9299c4c63
salix5 <salix5@livemail.tw>
2015-11-19 23:35:30 +0800

    fix

--- c17016362.lua
+++ c17016362.lua
@@ -61,7 +61,7 @@ function c17016362.mtop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	local fid=c:GetFieldID()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_EXTRA_ATTACK)
