127f4fbe42e30f0c967f2c0dd972f385c01ae65c
Tianchenglipu <670204195@qq.com>
2015-11-25 11:46:56 +0800

    Update c6859683.lua

--- c6859683.lua
+++ c6859683.lua
@@ -9,13 +9,14 @@ function c6859683.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c6859683.filter(c)
-	return c:IsFacedown() or not c:IsType(TYPE_PENDULUM)
+	return (c:IsFacedown() or not c:IsType(TYPE_PENDULUM)) and c:IsAbleToGrave()
 end
 function c6859683.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetFieldGroup(tp,0,LOCATION_EXTRA)
 	if chk==0 then return g:FilterCount(c6859683.filter,nil)>=2 end
 end
 function c6859683.operation(e,tp,eg,ep,ev,re,r,rp)
+	if g:FilterCount(c6859683.filter,nil)<2 then return end
 	local cg=Duel.GetFieldGroup(tp,0,LOCATION_EXTRA)
 	Duel.ConfirmCards(tp,cg)
 	local g=Duel.GetMatchingGroup(Card.IsType,1-tp,LOCATION_EXTRA,0,nil,TYPE_FUSION)
