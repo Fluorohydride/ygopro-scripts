ad3432e394ab8995236be33451433a2650fc22ce
Tianchenglipu <670204195@qq.com>
2015-11-25 11:52:46 +0800

    Update c80159717.lua

--- c80159717.lua
+++ c80159717.lua
@@ -15,7 +15,7 @@ function c80159717.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c80159717.excost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_EXTRA)
+	local ct=Duel.GetMatchingGroupCount(Card.IsAbleToGrave,tp,0,LOCATION_EXTRA,nil)
 	if ct>3 then ct=3 end
 	if chk==0 then return ct>0 and Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_ONFIELD,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
@@ -24,7 +24,7 @@ function c80159717.excost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(g:GetCount())
 end
 function c80159717.exop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetFieldGroup(tp,0,LOCATION_EXTRA)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,0,LOCATION_EXTRA,nil)
 	local ct=e:GetLabel()
 	if g:GetCount()<ct then return end
 	Duel.ConfirmCards(tp,g)
