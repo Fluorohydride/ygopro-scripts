5ddf9457abc769eaea756685daf1c68c80323108
salix5 <salix5@livemail.tw>
2015-12-12 12:33:31 +0800

    fix

--- c49080532.lua
+++ c49080532.lua
@@ -20,7 +20,7 @@ function c49080532.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
 function c49080532.filter(c,e,tp)
-	return c:GetCode()==49080532 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsCode(49080532) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c49080532.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
@@ -31,14 +31,10 @@ function c49080532.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0 then return end
 	local ct=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ct==0 then return end
-	local g=Duel.GetMatchingGroup(c49080532.filter,tp,LOCATION_DECK,0,nil,e,tp)
+	if ct>2 then ct=2 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c49080532.filter,tp,LOCATION_DECK,0,1,ct,nil,e,tp)
 	if g:GetCount()>0 then
-		local t1=g:GetFirst()
-		local t2=g:GetNext()
-		Duel.SpecialSummonStep(t1,0,tp,tp,false,false,POS_FACEUP)
-		if t2 and ct>1 and Duel.SelectYesNo(tp,aux.Stringid(49080532,1)) then
-			Duel.SpecialSummonStep(t2,0,tp,tp,false,false,POS_FACEUP)
-		end
-		Duel.SpecialSummonComplete()
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
