b6af9571a0ff6a0f96b54c4baaed9a71cc5da0c8
VanillaSalt <vanillasalt5197@gmail.com>
2016-01-10 17:09:21 +0900

    new

---
+++ c59822133.lua
@@ -0,0 +1,92 @@
+--青眼の精霊龍
+function c59822133.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(Card.IsSetCard,0xdd),1)
+	c:EnableReviveLimit()
+	--special summon limit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(59822133)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,1)
+	c:RegisterEffect(e1)
+	--negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(59822133,0))
+	e2:SetCategory(CATEGORY_NEGATE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetCondition(c59822133.negcon)
+	e2:SetTarget(c59822133.negtg)
+	e2:SetOperation(c59822133.negop)
+	c:RegisterEffect(e2)
+	--special summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(59822133,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetCondition(c59822133.spcon)
+	e3:SetCost(c59822133.spcost)
+	e3:SetTarget(c59822133.sptg)
+	e3:SetOperation(c59822133.spop)
+	c:RegisterEffect(e3)
+end
+function c59822133.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return re:GetActivateLocation()==LOCATION_GRAVE and Duel.IsChainNegatable(ev)
+end
+function c59822133.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+end
+function c59822133.negop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateActivation(ev)
+end
+function c59822133.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_SYNCHRO)==SUMMON_TYPE_SYNCHRO
+end
+function c59822133.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c59822133.spfilter(c,e,tp)
+	return c:IsRace(RACE_DRAGON) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsType(TYPE_SYNCHRO)
+		and not c:IsCode(59822133) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c59822133.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and Duel.IsExistingMatchingCard(c59822133.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c59822133.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c59822133.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENCE)~=0 then
+		tc:RegisterFlagEffect(59822133,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetLabelObject(tc)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		e1:SetCondition(c59822133.descon)
+		e1:SetOperation(c59822133.desop)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function c59822133.descon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	return tc:GetFlagEffect(59822133)~=0
+end
+function c59822133.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Destroy(tc,REASON_EFFECT)
+end
