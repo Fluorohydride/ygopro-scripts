cd3c73f69ad9274f3b2ab5b0aa9ae9693d23e59b
DailyShana <p2357111317192329@gmail.com>
2016-01-16 16:25:20 +0800

    fix

--- c4335427.lua
+++ c4335427.lua
@@ -27,8 +27,10 @@ function c4335427.initial_effect(c)
 	e4:SetCategory(CATEGORY_REMOVE)
 	e4:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
 	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetProperty(EFFECT_FLAG_CVAL_CHECK)
 	e4:SetTarget(c4335427.rmtg)
 	e4:SetOperation(c4335427.rmop)
+	e4:SetValue(c4335427.valcheck)
 	c:RegisterEffect(e4)
 end
 function c4335427.spfilter(c,tpe)
@@ -60,9 +62,11 @@ function c4335427.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0x1e,0x1e,e:GetHandler())
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,g:GetCount(),0,0)
-	Duel.SetChainLimit(aux.FALSE)
 end
 function c4335427.rmop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0x1e,0x1e,e:GetHandler())
 	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+end
+function c4335427.valcheck(e)
+	Duel.SetChainLimit(aux.FALSE)
 end
