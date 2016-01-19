e1b6e151c241be2ea5325dba03deb2f011d9f70f
Jeff Falberg <auronzeroo@aim.com>
2016-01-19 10:17:49 -0500

    New card: Lunalight Crimson Fox

---
+++ c94919024.lua
@@ -0,0 +1,71 @@
+--月光紅狐
+function c94919024.initial_effect(c)
+	--ATK to 0
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(94919024,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCondition(c94919024.atkcon)
+	e1:SetTarget(c94919024.atktg)
+	e1:SetOperation(c94919024.atkop)
+	c:RegisterEffect(e1)
+	--Negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(94919024,1))
+	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_RECOVER)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCondition(c94919024.condition)
+	e2:SetCost(c94919024.cost)
+	e2:SetTarget(c94919024.target)
+	e2:SetOperation(c94919024.operation)
+	c:RegisterEffect(e2)
+end
+
+function c94919024.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_EFFECT)
+end
+function c94919024.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and aux.nzatk(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(aux.nzatk,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,aux.nzatk,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function c94919024.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+
+function c94919024.filter(c,tp)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsSetCard(0xdf)
+end
+function c94919024.condition(e,tp,eg,ep,ev,re,r,rp)
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return g and g:IsExists(c94919024.filter,1,nil,tp)
+		and Duel.IsChainNegatable(ev)
+end
+function c94919024.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+end
+function c94919024.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+end
+function c94919024.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) then
+		Duel.Recover(tp,1000,REASON_EFFECT)
+		Duel.Recover(1-tp,1000,REASON_EFFECT)
+	end
+end
