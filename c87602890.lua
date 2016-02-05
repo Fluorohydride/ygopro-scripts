52bcd5cd41a9a646daabdad073002d15b579ca58
dest <dest.dn@gmail.com>
2016-02-05 14:55:14 +0100

    fix

--- c87602890.lua
+++ c87602890.lua
@@ -51,7 +51,7 @@ function c87602890.condition(e,tp,eg,ep,ev,re,r,rp)
 end
 function c87602890.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsDestructable() end
-	if chk==0 then return true end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
