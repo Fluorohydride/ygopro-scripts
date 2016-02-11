570425df9b3e654827c97ae1a9d9fe6e00f13381
nekrozar <unyuho.buster@gmail.com>
2016-02-11 16:06:36 +0900

    new

---
+++ c84046493.lua
@@ -0,0 +1,34 @@
+--ゴースト・ビーフ
+function c84046493.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--scale change
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(84046493,0))
+	e1:SetCategory(CATEGORY_DICE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c84046493.sctg)
+	e1:SetOperation(c84046493.scop)
+	c:RegisterEffect(e1)
+end
+function c84046493.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetLeftScale()<10 end
+	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function c84046493.scop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:GetLeftScale()>=10 then return end
+	local dc=Duel.TossDice(tp,1)
+	local sch=math.min(10-c:GetLeftScale(),dc)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_LSCALE)
+	e1:SetValue(sch)
+	e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_RSCALE)
+	c:RegisterEffect(e2)
+end
