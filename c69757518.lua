46b2a8a239bff1709bff65c183d0d321bb135c1c
VanillaSalt <vanillasalt5197@gmail.com>
2016-03-21 20:26:31 +0900

    fix

--- c69757518.lua
+++ c69757518.lua
@@ -52,7 +52,7 @@ function c69757518.atop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_DAMAGE_CAL)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE+PHASE_DAMAGE_CAL)
 	c:RegisterEffect(e1)
 end
 function c69757518.cost(e,tp,eg,ep,ev,re,r,rp,chk)
