d14780a60af21da2cc3a798c7131e270e874b6de
DailyShana <p2357111317192329@gmail.com>
2016-03-28 15:05:16 +0800

    update

--- c42776855.lua
+++ c42776855.lua
@@ -7,24 +7,54 @@ function c42776855.initial_effect(c)
 	e1:SetHintTiming(0,0x1e0)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetTarget(c42776855.target)
-	e1:SetOperation(c42776855.operation)
 	c:RegisterEffect(e1)
-	--Destroy
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetCode(EVENT_LEAVE_FIELD_P)
-	e2:SetOperation(c42776855.checkop)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_CHAIN_SOLVED)
+	e2:SetCondition(c42776855.tgcon)
+	e2:SetOperation(c42776855.tgop)
 	c:RegisterEffect(e2)
-	--Destroy2
+	--indes
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCondition(c42776855.descon2)
-	e3:SetOperation(c42776855.desop2)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetTarget(c42776855.indestg)
+	e3:SetValue(1)
 	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e4:SetValue(c42776855.efilter)
+	c:RegisterEffect(e4)
+	--destroy
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(42776855,0))
+	e5:SetCategory(CATEGORY_DESTROY)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_BATTLE_START)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCondition(c42776855.atkcon)
+	e5:SetOperation(c42776855.atkop)
+	c:RegisterEffect(e5)
+	--Destroy
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e6:SetCode(EVENT_LEAVE_FIELD_P)
+	e6:SetOperation(c42776855.checkop)
+	c:RegisterEffect(e6)
+	--Destroy2
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e7:SetRange(LOCATION_SZONE)
+	e7:SetCode(EVENT_LEAVE_FIELD)
+	e7:SetProperty(EFFECT_FLAG_DELAY)
+	e7:SetCondition(c42776855.descon2)
+	e7:SetOperation(c42776855.desop2)
+	c:RegisterEffect(e7)
 end
 function c42776855.filter(c)
 	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO)
@@ -35,54 +65,21 @@ function c42776855.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,c42776855.filter,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function c42776855.operation(e,tp,eg,ep,ev,re,r,rp)
+function c42776855.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	return re:GetHandler()==e:GetHandler()
+end
+function c42776855.tgop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+	local tc=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS):GetFirst()
+	if c:IsRelateToEffect(re) and tc:IsFaceup() and tc:IsRelateToEffect(re) then
 		c:SetCardTarget(tc)
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_OWNER_RELATE)
-		e1:SetRange(LOCATION_MZONE)
-		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-		e1:SetReset(RESET_EVENT+0x1fe0000)
-		e1:SetCondition(c42776855.rcon)
-		e1:SetValue(1)
-		tc:RegisterEffect(e1,true)
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-		e2:SetValue(c42776855.efilter)
-		tc:RegisterEffect(e2,true)
-		local e3=Effect.CreateEffect(c)
-		e3:SetDescription(aux.Stringid(42776855,0))
-		e3:SetCategory(CATEGORY_DESTROY)
-		e3:SetType(EFFECT_TYPE_QUICK_O)
-		e3:SetCode(EVENT_BATTLE_START)
-		e3:SetRange(LOCATION_SZONE)
-		e3:SetCondition(c42776855.atkcon)
-		e3:SetTarget(c42776855.atktg)
-		e3:SetOperation(c42776855.atkop)
-		e3:SetReset(RESET_EVENT+0x1fe0000)
-		c:RegisterEffect(e3)
 	end
 end
-function c42776855.rcon(e)
-	return e:GetOwner():IsHasCardTarget(e:GetHandler())
+function c42776855.indestg(e,c)
+	return e:GetHandler():IsHasCardTarget(c)
 end
 function c42776855.efilter(e,re)
 	return e:GetOwnerPlayer()~=re:GetOwnerPlayer()
-end
-function c42776855.checkop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsDisabled() then
-		e:SetLabel(1)
-	else e:SetLabel(0) end
-end
-function c42776855.descon2(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetHandler():GetFirstCardTarget()
-	return tc and eg:IsContains(tc)
-end
-function c42776855.desop2(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
 end
 function c42776855.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -90,10 +87,6 @@ function c42776855.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	if not tc then return false end
 	local bc=tc:GetBattleTarget()
 	return tc:IsLocation(LOCATION_MZONE) and bc and bc:IsFaceup() and bc:IsLocation(LOCATION_MZONE) and bc:IsLevelAbove(5)
-end
-function c42776855.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetFlagEffect(42776855)==0 end
-	e:GetHandler():RegisterFlagEffect(42776855,RESET_CHAIN,0,1)
 end
 function c42776855.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -110,4 +103,16 @@ function c42776855.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
+end
+function c42776855.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsDisabled() then
+		e:SetLabel(1)
+	else e:SetLabel(0) end
+end
+function c42776855.descon2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetHandler():GetFirstCardTarget()
+	return tc and eg:IsContains(tc)
+end
+function c42776855.desop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
 end
