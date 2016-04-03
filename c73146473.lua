0537f0645b55a48615fdf184a02f6c73a9be0e72
nekrozar <unyuho.buster@gmail.com>
2016-04-03 15:19:19 +0900

    fix

--- c73146473.lua
+++ c73146473.lua
@@ -41,7 +41,6 @@ function c73146473.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c73146473.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
-		Duel.BreakEffect()
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
