4c4a519762cb1290e8d3f533f395db6390f83371
mercury233 <me@mercury233.me>
2016-04-04 21:50:56 +0800

    fix

--- c691925.lua
+++ c691925.lua
@@ -18,7 +18,8 @@ function c691925.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.DiscardHand(tp,c691925.costfilter,1,1,REASON_COST+REASON_DISCARD,nil)
 end
 function c691925.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,4) and Duel.IsPlayerCanDiscardDeck(tp,2) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) and Duel.IsPlayerCanDiscardDeck(tp,2)
+		and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=4 end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(2)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
