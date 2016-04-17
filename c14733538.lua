fec65c9eb9b4e5f21d446f266c4284f9dd37eadb
DailyShana <p2357111317192329@gmail.com>
2016-04-17 21:59:00 +0800

    rename to SendtoExtraP

--- c14733538.lua
+++ c14733538.lua
@@ -44,5 +44,5 @@ function c14733538.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 	sg1:RemoveCard(tc)
-	Duel.PSendtoExtra(sg1,nil,REASON_EFFECT)
+	Duel.SendtoExtraP(sg1,nil,REASON_EFFECT)
 end
