87610e27bcb13b2a4d2b6f5e227bbfecce5b3de0
nekrozar <unyuho.buster@gmail.com>
2016-04-18 18:12:07 +0900

    fix Dark Designator (#370)
    * fix
    
    * revert

--- c78053598.lua
+++ c78053598.lua
@@ -10,14 +10,16 @@ function c78053598.initial_effect(c)
 end
 function c78053598.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,0,LOCATION_DECK,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,564)
+	local code=Duel.AnnounceCard(tp,TYPE_MONSTER)
+	Duel.SetTargetParam(code)
 	Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,TYPE_MONSTER)
 end
 function c78053598.filter(c,code)
 	return c:IsType(TYPE_MONSTER) and c:IsCode(code) and c:IsAbleToHand()
 end
 function c78053598.activate(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,564)
-	local code=Duel.AnnounceCard(tp,TYPE_MONSTER)
+	local code=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
 	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(1-tp,c78053598.filter,1-tp,LOCATION_DECK,0,1,1,nil,code)
 	local tc=g:GetFirst()
