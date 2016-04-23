8e2dcc53ec045e9595ce01b3c7820539d2d6f7b3
nekrozar <unyuho.buster@gmail.com>
2016-04-23 10:37:18 +0900

    new

---
+++ c71525232.lua
@@ -0,0 +1,59 @@
+--破滅竜ガンドラX
+function c71525232.initial_effect(c)
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(71525232,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(c71525232.condition)
+	e1:SetTarget(c71525232.target)
+	e1:SetOperation(c71525232.operation)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--halve LP
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(71525232,1))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(c71525232.hvcon)
+	e3:SetOperation(c71525232.hvop)
+	c:RegisterEffect(e3)
+end
+function c71525232.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_HAND)
+end
+function c71525232.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
+	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+	local atk=g:GetMaxGroup(Card.GetAttack)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,atk)
+end
+function c71525232.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,c)
+	if g:GetCount()>0 and Duel.Destroy(g,REASON_EFFECT)~=0 then
+		local og=Duel.GetOperatedGroup()
+		local mg,atk=g:GetMaxGroup(Card.GetPreviousAttackOnField)
+		local ct=Duel.Damage(1-tp,atk,REASON_EFFECT)
+		if ct>0 then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e1:SetValue(ct)
+			e1:SetReset(RESET_EVENT+0x1ff0000)
+			c:RegisterEffect(e1)
+		end
+	end
+end
+function c71525232.hvcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function c71525232.hvop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetLP(tp,math.ceil(Duel.GetLP(tp)/2))
+end
