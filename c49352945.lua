205724f279fb33122c5f244a5a4021a15055ffdf
dest <dest.dn@gmail.com>
2016-06-25 03:45:01 +0200

    fix Storm Neos

--- c49352945.lua
+++ c49352945.lua
@@ -59,49 +59,80 @@ end
 function c49352945.splimit(e,se,sp,st)
 	return not e:GetHandler():IsLocation(LOCATION_EXTRA)
 end
-function c49352945.spfilter(c,code)
-	return c:IsAbleToDeckOrExtraAsCost() and c:IsFusionCode(code)
+function c49352945.spfilter(c,code1,code2,code3)
+	return c:IsAbleToDeckOrExtraAsCost() and (c:IsFusionCode(code1) or c:IsFusionCode(code2) or c:IsFusionCode(code3))
+end
+function c49352945.spfilter1(c,mg)
+	local mg2=mg:Clone()
+	mg2:RemoveCard(c)
+	return c:IsFusionCode(89943723) and c:IsAbleToDeckOrExtraAsCost() and c:IsCanBeFusionMaterial()
+		and mg2:IsExists(c49352945.spfilter2,1,nil,mg2)
+end
+function c49352945.spfilter2(c,mg)
+	local mg2=mg:Clone()
+	mg2:RemoveCard(c)
+	return c:IsFusionCode(17955766) and c:IsAbleToDeckOrExtraAsCost() and c:IsCanBeFusionMaterial()
+		and mg2:IsExists(c49352945.spfilter3,1,nil)
+end
+function c49352945.spfilter3(c)
+	return c:IsFusionCode(54959865) and c:IsAbleToDeckOrExtraAsCost() and c:IsCanBeFusionMaterial()
+end
+function c49352945.rmfilter(c,code)
+	return c:IsFusionCode(code) and c:IsCode(code)
 end
 function c49352945.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft<-2 then return false end
-	local g1=Duel.GetMatchingGroup(c49352945.spfilter,tp,LOCATION_ONFIELD,0,nil,89943723)
-	local g2=Duel.GetMatchingGroup(c49352945.spfilter,tp,LOCATION_ONFIELD,0,nil,17955766)
-	local g3=Duel.GetMatchingGroup(c49352945.spfilter,tp,LOCATION_ONFIELD,0,nil,54959865)
-	if g1:GetCount()==0 or g2:GetCount()==0 or g3:GetCount()==0 then return false end
-	if ft>0 then return true end
-	local f1=g1:FilterCount(Card.IsLocation,nil,LOCATION_MZONE)>0 and 1 or 0
-	local f2=g2:FilterCount(Card.IsLocation,nil,LOCATION_MZONE)>0 and 1 or 0
-	local f3=g3:FilterCount(Card.IsLocation,nil,LOCATION_MZONE)>0 and 1 or 0
-	if ft==-2 then return f1+f2+f3==3
-	elseif ft==-1 then return f1+f2+f3>=2
-	else return f1+f2+f3>=1 end
+	local mg=Duel.GetMatchingGroup(c49352945.spfilter,tp,LOCATION_ONFIELD,0,nil,89943723,17955766,54959865)
+	if ft>0 then return mg:IsExists(c49352945.spfilter1,1,nil,mg) end
+	local f1=Duel.GetMatchingGroupCount(c49352945.spfilter,tp,LOCATION_MZONE,0,nil,89943723,0,0)>0 and 1 or 0
+	local f2=Duel.GetMatchingGroupCount(c49352945.spfilter,tp,LOCATION_MZONE,0,nil,17955766,0,0)>0 and 1 or 0
+	local f3=Duel.GetMatchingGroupCount(c49352945.spfilter,tp,LOCATION_MZONE,0,nil,54959865,0,0)>0 and 1 or 0
+	if ft==-2 then return f1+f2+f3==3 and mg:IsExists(c49352945.spfilter1,1,nil,mg)
+	elseif ft==-1 then return f1+f2+f3>=2 and mg:IsExists(c49352945.spfilter1,1,nil,mg)
+	else return f1+f2+f3>=1 and mg:IsExists(c49352945.spfilter1,1,nil,mg) end
 end
 function c49352945.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	local g1=Duel.GetMatchingGroup(c49352945.spfilter,tp,LOCATION_ONFIELD,0,nil,89943723)
-	local g2=Duel.GetMatchingGroup(c49352945.spfilter,tp,LOCATION_ONFIELD,0,nil,17955766)
-	local g3=Duel.GetMatchingGroup(c49352945.spfilter,tp,LOCATION_ONFIELD,0,nil,54959865)
-	g1:Merge(g2)
-	g1:Merge(g3)
+	local mg1=Duel.GetMatchingGroup(c49352945.spfilter,tp,LOCATION_ONFIELD,0,nil,89943723,17955766,54959865)
+	local mg2=mg1:Filter(Card.IsLocation,nil,LOCATION_MZONE)
 	local g=Group.CreateGroup()
 	local tc=nil
 	for i=1,3 do
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-		if ft<=0 then
-			tc=g1:FilterSelect(tp,Card.IsLocation,1,1,nil,LOCATION_MZONE):GetFirst()
-		else
-			tc=g1:Select(tp,1,1,nil):GetFirst()
+		if i==1 then
+			if ft<=0 then
+				tc=mg2:FilterSelect(tp,c49352945.spfilter1,1,1,nil,mg2):GetFirst()
+			else
+				tc=mg1:FilterSelect(tp,c49352945.spfilter1,1,1,nil,mg1):GetFirst()
+			end
+		end
+		if i==2 then
+			if ft<=0 then
+				tc=mg2:FilterSelect(tp,c49352945.spfilter2,1,1,nil,mg2):GetFirst()
+			else
+				tc=mg1:FilterSelect(tp,c49352945.spfilter2,1,1,nil,mg1):GetFirst()
+			end
+		end
+		if i==3 then
+			if ft<=0 then
+				tc=mg2:FilterSelect(tp,c49352945.spfilter3,1,1,nil,mg2):GetFirst()
+			else
+				tc=mg1:FilterSelect(tp,c49352945.spfilter3,1,1,nil,mg1):GetFirst()
+			end
 		end
 		g:AddCard(tc)
 		if tc:IsFusionCode(89943723) then
-			g1:Remove(Card.IsFusionCode,nil,89943723)
+			mg1:Remove(c49352945.rmfilter,nil,89943723)
+			mg2:Remove(c49352945.rmfilter,nil,89943723)
 		elseif tc:IsFusionCode(17955766) then
-			g1:Remove(Card.IsFusionCode,nil,17955766)
+			mg1:Remove(c49352945.rmfilter,nil,17955766)
+			mg2:Remove(c49352945.rmfilter,nil,17955766)
 		elseif tc:IsFusionCode(54959865) then
-			g1:Remove(Card.IsFusionCode,nil,54959865)
+			mg1:Remove(c49352945.rmfilter,nil,54959865)
+			mg2:Remove(c49352945.rmfilter,nil,54959865)
 		end
 		ft=ft+1
 	end
