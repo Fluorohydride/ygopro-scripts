6cb43ba146bb929e5a8eacd82bc4738ceb464c06
DiabladeZat <diamondbladezat@gmail.com>
2016-06-25 22:03:29 +0200

    Fixes to Lunalight Panther Dancer and Lunalight Cat Dancer
    Added flags to Lunalight Panther Dancer and Lunalight Cat Dancer in
    order to ensure that the effects go off correctly

--- c51777272.lua
+++ c51777272.lua
@@ -1,7 +1,9 @@
---月光舞猫姫
+--Scripted by Eerie Code
+--Credit to Michael Lawrence Dee for the multiple attacks script
+--Moonlight Cat Dancer
 function c51777272.initial_effect(c)
 	--fusion material
-	aux.AddFusionProcFunRep(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xdf),2,true)
+	aux.AddFusionProcFunRep(c,c51777272.matfil,2,true)
 	c:EnableReviveLimit()
 	--battle indestructable
 	local e1=Effect.CreateEffect(c)
@@ -29,37 +31,117 @@ function c51777272.initial_effect(c)
 	e3:SetOperation(c51777272.damop)
 	c:RegisterEffect(e3)
 end
+
+function c51777272.matfil(c)
+	return c:IsFusionSetCard(0xdf)
+end
+
 function c51777272.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsAbleToEnterBP()
+	return Duel.GetCurrentPhase()==PHASE_MAIN1
 end
 function c51777272.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsSetCard,1,e:GetHandler(),0xdf) end
-	local g=Duel.SelectReleaseGroup(tp,Card.IsSetCard,1,1,e:GetHandler(),0xdf)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c51777272.matfil,1,e:GetHandler()) end
+	local g=Duel.SelectReleaseGroup(tp,c51777272.matfil,1,1,e:GetHandler())
 	Duel.Release(g,REASON_COST)
 end
 function c51777272.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
-	e1:SetTargetRange(0,LOCATION_MZONE)
-	e1:SetValue(c51777272.indct)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
 	if c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_ATTACK_ALL)
+		e1:SetValue(2)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
 		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_ATTACK_ALL)
+		e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_DAMAGE_STEP_END)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetOperation(c51777272.unop)
 		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-		e2:SetValue(2)
 		c:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e3:SetRange(LOCATION_MZONE)
+		e3:SetTargetRange(0,LOCATION_MZONE)
+		e3:SetCode(EFFECT_DESTROY_REPLACE)
+		e3:SetTarget(c51777272.reptg)
+		e3:SetValue(c51777272.repval)
+		e3:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e3)
+		local g=Group.CreateGroup()
+		g:KeepAlive()
+		e3:SetLabelObject(g)
+		local e4=Effect.CreateEffect(c)
+		e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e4:SetRange(LOCATION_MZONE)
+		e4:SetTargetRange(0,LOCATION_MZONE)
+		e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CANNOT_DISABLE)
+		e4:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+		e4:SetTarget(c51777272.valtg)
+		e4:SetValue(c51777272.vala)
+		e4:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e4)
 	end
 end
-function c51777272.indct(e,re,r,rp)
-	if bit.band(r,REASON_BATTLE)~=0 then
-		return 1
-	else return 0 end
+function c51777272.unop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	if c:GetFlagEffect(51777272+2)==0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE)
+		c:RegisterEffect(e1)
+		c:RegisterFlagEffect(51777272+2,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE,0,1)
+	end
+	if bc then
+		if bc:GetFlagEffect(51777272)>0 then
+			bc:RegisterFlagEffect(51777272+1,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE,0,1)
+		end
+	end
 end
+function c51777272.valtg(e,c)
+	return c:GetFlagEffect(51777272+1)>0
+end
+function c51777272.vala(e,c)
+	return c==e:GetHandler()
+end
+function c51777272.indescon(e,tp,eg,ep,ev,re,r,rp)
+	local bc=e:GetHandler():GetBattleTarget()
+	return bc and bc:GetFlagEffect(51777272)==0
+end
+function c51777272.indesop(e,tp,eg,ep,ev,re,r,rp)
+	local bc=e:GetHandler():GetBattleTarget()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(1)
+	e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+	bc:RegisterEffect(e1,true)
+end
+
+function c51777272.repfilter(c,tp)
+	return c:IsFaceup() and c:IsControler(1-tp) and c:IsLocation(LOCATION_MZONE) and c:IsReason(REASON_BATTLE) and c:GetFlagEffect(51777272)==0
+end
+function c51777272.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(c51777272.repfilter,1,nil,tp) end
+	local g=eg:Filter(c51777272.repfilter,nil,tp)
+	local tc=g:GetFirst()
+	while tc do
+		tc:RegisterFlagEffect(51777272,RESET_EVENT+0x1fc0000+RESET_PHASE+PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(51777272,2))
+		tc=g:GetNext()
+	end
+	e:GetLabelObject():Clear()
+	e:GetLabelObject():Merge(g)
+	return true
+end
+function c51777272.repval(e,c)
+	local g=e:GetLabelObject()
+	return g:IsContains(c)
+end
+
 function c51777272.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(1-tp)
