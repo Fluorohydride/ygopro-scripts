503b684e779f1f07333f15a793c3b0c6db29ec5f
DailyShana <p2357111317192329@gmail.com>
2016-06-27 18:31:24 +0800

    fix

--- c39778366.lua
+++ c39778366.lua
@@ -42,17 +42,14 @@ function c39778366.operation(e,tp,eg,ep,ev,re,r,rp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft<=0 then return end
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
-	if g:GetCount()>1 and Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
-	local sg=nil
-	if g:GetCount()>ft then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		sg=g:Select(tp,ft,ft,nil)
-		g:Sub(sg)
+	if g:GetCount()==0 or (g:GetCount()>1 and Duel.IsPlayerAffectedByEffect(tp,59822133)) then return end
+	if g:GetCount()<=ft then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	else
-		sg=g
-	end
-	Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-	if g:GetCount()>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=g:Select(tp,ft,ft,nil)
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		g:Sub(sg)
 		Duel.SendtoGrave(g,REASON_RULE)
 	end
 end
