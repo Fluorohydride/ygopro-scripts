68d4fe1ea5f419fa9d4ec71859e483d0b3c0812f
nekrozar <unyuho.buster@gmail.com>
2016-06-30 06:45:17 +0900

    fix

--- c33776734.lua
+++ c33776734.lua
@@ -32,9 +32,40 @@ function c33776734.initial_effect(c)
 	local e4=e3:Clone()
 	e4:SetCode(EFFECT_SET_DEFENSE)
 	c:RegisterEffect(e4)
+	if not c33776734.global_check then
+		c33776734.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		ge1:SetCode(EVENT_CHAINING)
+		ge1:SetOperation(c33776734.checkop1)
+		Duel.RegisterEffect(ge1,0)
+		local ge2=ge1:Clone()
+		e2:SetCode(EVENT_CHAIN_NEGATED)
+		e2:SetOperation(c33776734.checkop2)
+		Duel.RegisterEffect(ge2,0)
+	end
 end
 c33776734.lvupcount=1
 c33776734.lvup={33776734}
+function c33776734.checkop1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	while tc do
+		if re and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and tc==re:GetHandler() then
+			tc:RegisterFlagEffect(33776734,RESET_EVENT+0x1fe0000,0,1) 
+		end
+		tc=eg:GetNext()
+	end
+end
+function c33776734.checkop2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	while tc do
+		if re and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and tc==re:GetHandler() then
+			tc:ResetFlagEffect(33776734)
+		end
+		tc=eg:GetNext()
+	end
+end
 function c33776734.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentChain()>=2
 end
@@ -53,7 +84,7 @@ function c33776734.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c33776734.rmtarget(e,c)
-	return c:IsFaceup() and not c:IsType(TYPE_TRAP) and c:IsStatus(STATUS_ACTIVATED)
+	return c:IsFaceup() and c:GetFlagEffect(33776734)>0
 end
 function c33776734.val(e,c)
 	return Duel.GetMatchingGroupCount(Card.IsType,c:GetControler(),0,LOCATION_GRAVE,nil,TYPE_SPELL)*500
