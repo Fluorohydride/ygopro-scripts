1dca0c085942fc9e0008583d49e168fd8773734a
VanillaSalt <vanillasalt5197@gmail.com>
2016-06-30 21:28:08 +0900

    update

--- c67287533.lua
+++ c67287533.lua
@@ -6,6 +6,10 @@ function c67287533.initial_effect(c)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetCode(EFFECT_SPSUMMON_COST)
+	e2:SetCost(c67287533.spcost)
 	c:RegisterEffect(e2)
+end
+function c67287533.spcost(e,c,tp,sumtype)
+	return sumtype==SUMMON_TYPE_SPECIAL+181
 end
--- c94772232.lua
+++ c94772232.lua
@@ -6,6 +6,10 @@ function c94772232.initial_effect(c)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetCode(EFFECT_SPSUMMON_COST)
+	e2:SetCost(c94772232.spcost)
 	c:RegisterEffect(e2)
+end
+function c94772232.spcost(e,c,tp,sumtype)
+	return sumtype==SUMMON_TYPE_SPECIAL+181
 end
