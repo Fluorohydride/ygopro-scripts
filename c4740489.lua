16540db65076cb786855ab1c03e2c3c32ca69012
dest <dest.dn@gmail.com>
2016-07-01 10:58:00 +0200

    fix Magnet Field (#468)

--- c4740489.lua
+++ c4740489.lua
@@ -59,11 +59,11 @@ function c4740489.atcon(e,tp,eg,ep,ev,re,r,rp)
 	if d:IsControler(tp) then
 		e:SetLabelObject(a)
 		return d:IsRace(RACE_ROCK) and d:IsAttribute(ATTRIBUTE_EARTH)
-			and a:IsRelateToBattle()
+			and a:IsRelateToBattle() and a:IsLocation(LOCATION_ONFIELD)
 	elseif a:IsControler(tp) then
 		e:SetLabelObject(d)
 		return a:IsRace(RACE_ROCK) and a:IsAttribute(ATTRIBUTE_EARTH)
-			and d:IsRelateToBattle()
+			and d:IsRelateToBattle() and d:IsLocation(LOCATION_ONFIELD)
 	end
 	return false
 end
