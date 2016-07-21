4582a3a311ae9b79a9357d67c5f070be20eb07c5
Mercury233 <me@mercury233.me>
2016-07-21 08:05:27 +0800

    fix (#497)

--- c94203886.lua
+++ c94203886.lua
@@ -60,7 +60,7 @@ function c94203886.lvop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CHANGE_LEVEL)
 		e1:SetValue(tc:GetLevel())
-		e1:SetReset(RESET_EVENT+0x1fe0000)
+		e1:SetReset(RESET_EVENT+0x1ff0000)
 		c:RegisterEffect(e1)
 	end
 end
