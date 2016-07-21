7f9eb02ebc1155c99a6bcdea32320824e3429c13
nekrozar <unyuho.buster@gmail.com>
2016-07-21 22:05:15 +0900

    new

---
+++ c77363314.lua
@@ -0,0 +1,34 @@
+--クリスタル・ドラゴン
+function c77363314.initial_effect(c)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(77363314,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(c77363314.thcon)
+	e1:SetTarget(c77363314.thtg)
+	e1:SetOperation(c77363314.thop)
+	c:RegisterEffect(e1)
+end
+function c77363314.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp and Duel.GetCurrentPhase()==PHASE_BATTLE_STEP and Duel.GetCurrentChain()==0
+		and e:GetHandler():GetBattledGroupCount()>0
+end
+function c77363314.thfilter(c)
+	return c:IsRace(RACE_DRAGON) and c:GetLevel()==8 and c:IsAbleToHand()
+end
+function c77363314.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c77363314.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c77363314.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c77363314.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
