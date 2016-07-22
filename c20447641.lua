3a8580c67debce25466eceaeb4cb99c2a1f27393
DailyShana <p2357111317192329@gmail.com>
2016-07-22 14:39:25 +0800

    fix
    for Trap of Darkness

--- c20447641.lua
+++ c20447641.lua
@@ -5,6 +5,7 @@ function c20447641.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(c20447641.cost)
 	e1:SetTarget(c20447641.target)
 	e1:SetOperation(c20447641.activate)
 	c:RegisterEffect(e1)
@@ -24,10 +25,17 @@ function c20447641.initial_effect(c)
 	e4:SetCode(EFFECT_LIMIT_SET_PROC)
 	c:RegisterEffect(e4)
 end
+function c20447641.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(1)
+	return true
+end
 function c20447641.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,20447641,0xaa,0x21,4,1800,1000,RACE_MACHINE,ATTRIBUTE_EARTH) end
+	if chk==0 then
+		if e:GetLabel()==0 then return false end
+		e:SetLabel(0)
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and Duel.IsPlayerCanSpecialSummonMonster(tp,20447641,0xaa,0x21,4,1800,1000,RACE_MACHINE,ATTRIBUTE_EARTH) end
+	e:SetLabel(0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function c20447641.activate(e,tp,eg,ep,ev,re,r,rp)
