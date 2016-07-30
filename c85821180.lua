98b2ec1fee8f6e9c1a75fb56b8440409c7b682bf
nekrozar <unyuho.buster@gmail.com>
2016-07-30 22:31:13 +0900

    fix

--- c85821180.lua
+++ c85821180.lua
@@ -5,6 +5,7 @@ function c85821180.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetLabel(0)
 	e1:SetCost(c85821180.cost)
 	e1:SetTarget(c85821180.target)
 	e1:SetOperation(c85821180.activate)
@@ -12,27 +13,36 @@ function c85821180.initial_effect(c)
 end
 function c85821180.cfilter(c,e,tp)
 	return c:IsType(TYPE_MONSTER) and c:IsDiscardable()
-		and Duel.IsExistingMatchingCard(c85821180.filter,tp,LOCATION_DECK,0,1,nil,e,tp,c)
+		and Duel.IsExistingMatchingCard(c85821180.filter,tp,LOCATION_DECK,0,1,nil,c:GetRace(),c:GetAttribute(),c:GetLevel(),e,tp)
 end
-function c85821180.filter(c,e,tp,dc)
-	return c:IsType(TYPE_TUNER) and c:IsRace(dc:GetRace()) and c:IsAttribute(dc:GetAttribute())
-		and c:GetLevel()==dc:GetLevel()+1 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function c85821180.filter(c,race,att,lv,e,tp)
+	return c:IsType(TYPE_TUNER) and c:IsRace(race) and c:IsAttribute(att) and c:GetLevel()==lv+1 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c85821180.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c85821180.cfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-	local g=Duel.SelectMatchingCard(tp,c85821180.cfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
-	e:SetLabelObject(g:GetFirst())
+	e:SetLabel(100)
+	return true
 end
 function c85821180.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	if chk==0 then
+		if e:GetLabel()~=100 or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return false end
+		return Duel.IsExistingMatchingCard(c85821180.cfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local g=Duel.SelectMatchingCard(tp,c85821180.cfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	e:SetLabel(tc:GetRace())
+	e:SetValue(tc:GetAttribute())
+	Duel.SetTargetParam(tc:GetLevel())
+	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function c85821180.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local race=e:GetLabel()
+	local att=e:GetValue()
+	local lv=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c85821180.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp,e:GetLabelObject())
+	local g=Duel.SelectMatchingCard(tp,c85821180.filter,tp,LOCATION_DECK,0,1,1,nil,race,att,lv,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
