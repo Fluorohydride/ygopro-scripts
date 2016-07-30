d262deb94a2dea8cbf7fbec84615da8519863385
nekrozar <nekrozar@users.noreply.github.com>
2016-07-30 19:58:09 +0900

    fix Fusion Fright Waltz (#512)

--- c34449261.lua
+++ c34449261.lua
@@ -43,8 +43,9 @@ function c34449261.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
 	local dam=tg:Filter(Card.IsFaceup,nil):GetSum(Card.GetAttack)
 	local g=Duel.GetMatchingGroup(c34449261.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,tg)
-	if g:GetCount()>0 and Duel.Destroy(g,REASON_EFFECT)~=0 then
+	if g:GetCount()>0 and Duel.Destroy(g,REASON_EFFECT)~=0 and dam>0 then
 		local dg=Duel.GetOperatedGroup()
+		Duel.BreakEffect()
 		if dg:IsExists(aux.FilterEqualFunction(Card.GetPreviousControler,tp),1,nil) then Duel.Damage(tp,dam,REASON_EFFECT,true) end
 		if dg:IsExists(aux.FilterEqualFunction(Card.GetPreviousControler,1-tp),1,nil) then Duel.Damage(1-tp,dam,REASON_EFFECT,true) end
 		Duel.RDComplete()
