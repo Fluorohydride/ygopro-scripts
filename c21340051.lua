56038bc2031aecb1978a0a061be49b9687b96186
nekrozar <unyuho.buster@gmail.com>
2016-08-07 00:15:14 +0900

    fix

--- c21340051.lua
+++ c21340051.lua
@@ -2,8 +2,10 @@
 function c21340051.initial_effect(c)
 	--self destroy
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(c21340051.destg)
 	e1:SetOperation(c21340051.desop)
 	c:RegisterEffect(e1)
 	--atk down
@@ -16,8 +18,14 @@ function c21340051.initial_effect(c)
 	e2:SetValue(-1000)
 	c:RegisterEffect(e2)
 end
+function c21340051.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+end
 function c21340051.desop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+	end
 end
 function c21340051.con(e)
 	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),0,LOCATION_MZONE)>0
