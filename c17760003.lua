bce595ec3cc77d720ae66377eabc17bb2e083130
Mercury233 <me@mercury233.me>
2016-09-18 20:49:30 +0800

    fix

--- c17760003.lua
+++ c17760003.lua
@@ -116,7 +116,7 @@ function c17760003.filter3(c)
 	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT)
 end
 function c17760003.target3(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c17760003.filter2(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c17760003.filter3(chkc) end
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
 		and Duel.IsExistingTarget(c17760003.filter3,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
