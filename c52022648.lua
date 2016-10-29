2dfd855a9e16bd125573733c23f7fc8b4450a694
Mercury233 <me@mercury233.me>
2016-10-29 20:14:12 +0800

    fix 妖精伝姫－シンデレラ (#636)

--- c52022648.lua
+++ c52022648.lua
@@ -3,7 +3,7 @@ function c52022648.initial_effect(c)
 	--cannot be target
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
 	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
