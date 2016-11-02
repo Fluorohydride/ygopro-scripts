82369963bf8a016adb693c985868598537b51523
Mercury233 <me@mercury233.me>
2016-11-02 20:35:43 +0800

    fix

--- c83272895.lua
+++ c83272895.lua
@@ -20,7 +20,7 @@ function c83272895.efop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_PIERCE)
 	e1:SetReset(RESET_EVENT+0x1fe0000)
-	rc:RegisterEffect(e1)
+	rc:RegisterEffect(e1,true)
 	if not rc:IsType(TYPE_EFFECT) then
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
