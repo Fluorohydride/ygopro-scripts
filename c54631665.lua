bdc036860cc9e319a6dd0d56ae7bf0857365e5b5
mercury233 <me@mercury233.me>
2016-11-07 00:55:42 +0800

    new cards INOV-EN

---
+++ c54631665.lua
@@ -0,0 +1,80 @@
+--SPYRAL Resort
+function c54631665.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--cannot be target
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_ONFIELD,0)
+	e2:SetTarget(c54631665.tgtg)
+	e2:SetValue(aux.tgoval)
+	c:RegisterEffect(e2)
+	--search
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(54631665,0))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(c54631665.thtg)
+	e3:SetOperation(c54631665.thop)
+	c:RegisterEffect(e3)
+	--maintain
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCountLimit(1)
+	e4:SetCondition(c54631665.mtcon)
+	e4:SetOperation(c54631665.mtop)
+	c:RegisterEffect(e4)
+end
+function c54631665.tgtg(e,c)
+	return c:IsSetCard(0xee) and c~=e:GetHandler()
+end
+function c54631665.thfilter(c)
+	return c:IsSetCard(0xee) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function c54631665.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c54631665.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c54631665.thop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c54631665.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c54631665.mtcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function c54631665.cfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeckAsCost()
+end
+function c54631665.mtop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.HintSelection(Group.FromCards(c))
+	local g=Duel.GetMatchingGroup(c54631665.cfilter,tp,LOCATION_GRAVE,0,nil)
+	local sel=1
+	if g:GetCount()~=0 then
+		sel=Duel.SelectOption(tp,aux.Stringid(54631665,1),aux.Stringid(54631665,2))
+	else
+		sel=Duel.SelectOption(tp,aux.Stringid(54631665,2))+1
+	end
+	if sel==0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local tg=g:Select(tp,1,1,nil)
+		Duel.SendtoDeck(tg,nil,2,REASON_COST)
+	else
+		Duel.Destroy(c,REASON_COST)
+	end
+end
---
+++ c65976795.lua
@@ -0,0 +1,83 @@
+--Subterror Behemoth Stygokraken
+function c65976795.initial_effect(c)
+	--flip
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(65976795,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,65976795)
+	e1:SetTarget(c65976795.target)
+	e1:SetOperation(c65976795.operation)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(65976795,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCode(EVENT_CHANGE_POS)
+	e2:SetCondition(c65976795.spcon)
+	e2:SetTarget(c65976795.sptg)
+	e2:SetOperation(c65976795.spop)
+	c:RegisterEffect(e2)
+	--turn set
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(65976795,2))
+	e3:SetCategory(CATEGORY_POSITION)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(c65976795.postg)
+	e3:SetOperation(c65976795.posop)
+	c:RegisterEffect(e3)
+end
+function c65976795.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x10ed)
+end
+function c65976795.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local ct=Duel.GetMatchingGroupCount(c65976795.filter,tp,LOCATION_MZONE,0,nil)
+	if chkc then return chkc:IsOnField() and chkc:IsFacedown() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFacedown,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,ct,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsFacedown,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,ct,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+end
+function c65976795.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	if g:GetCount()>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
+function c65976795.cfilter(c,tp)
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsFacedown() and c:IsControler(tp)
+end
+function c65976795.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c65976795.cfilter,1,nil,tp)
+		and not Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
+end
+function c65976795.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c65976795.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)==0
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+		Duel.SendtoGrave(c,REASON_RULE)
+	end
+end
+function c65976795.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsCanTurnSet() and c:GetFlagEffect(65976795)==0 end
+	c:RegisterFlagEffect(65976795,RESET_EVENT+0x1fc0000+RESET_PHASE+PHASE_END,0,1)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,c,1,0,0)
+end
+function c65976795.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		Duel.ChangePosition(c,POS_FACEDOWN_DEFENSE)
+	end
+end
