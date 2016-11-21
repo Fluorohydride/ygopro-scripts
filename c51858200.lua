dd7ec658547fec27cb1275064a157e2e901d02d0
dest <dest.dn@gmail.com>
2016-11-21 19:11:49 +0100

    fix Predator Planet

--- c51858200.lua
+++ c51858200.lua
@@ -44,6 +44,7 @@ function c51858200.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function c51858200.thop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,c51858200.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
