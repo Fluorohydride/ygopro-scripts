5734a678eec80c5d82e2ada29a344e6d94a08237
nekrozar <nekrozar@users.noreply.github.com>
2016-12-24 12:18:42 +0900

    fix Judgment of the Desert (#544)

--- c4869446.lua
+++ c4869446.lua
@@ -7,9 +7,30 @@ function c4869446.initial_effect(c)
 	c:RegisterEffect(e1)
 	--
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_CHANGE_POS)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetOperation(c4869446.posop)
 	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetTarget(c4869446.postg)
+	c:RegisterEffect(e3)
+end
+function c4869446.cfilter(c)
+	return c:IsFaceup() and c:IsPreviousPosition(POS_FACEDOWN)
+end
+function c4869446.posop(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(c4869446.cfilter,nil)
+	local tc=g:GetFirst()
+	while tc do
+		e:GetHandler():SetCardTarget(tc)
+		tc=g:GetNext()
+	end
+end
+function c4869446.postg(e,c)
+	return e:GetHandler():IsHasCardTarget(c)
 end
