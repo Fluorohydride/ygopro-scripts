c8ea72efd620a096dc93ca3135de22608d59fccd
Mercury233 <me@mercury233.me>
2016-12-26 21:06:37 +0800

    fix typo

--- c73616671.lua
+++ c73616671.lua
@@ -27,6 +27,7 @@ end
 function c73616671.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c73616671.filter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,2,nil)
+	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
