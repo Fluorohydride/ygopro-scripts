512d079e964d12b087d9b4d60cc6955843bc0156
Hello23-Ygopro <thedragonofjudgementishere@gmail.com>
2017-01-04 15:56:20 +0200

    add category & shuffle hand
    1) Added CATEGORY_REMOVE
    I dont know if CATEGORY_TOGRAVE is needed , cus its not guaranteed that ur whole hand will go to the grave ?
    
    2) Added Duel.ShuffleHand(p) , but i dont know if its added in the correct order under the "if" statement.. so i added it before the discard/banish , just like Dark World Dealings : https://github.com/Fluorohydride/ygopro-scripts/blob/master/c74117290.lua#L22
    
    Im guessing you are shuffling your hand cus if your opponent found out about a card in your hand , and they see you banishing a card of the same name you just drawn with Allure , they still know that that card is in your hand , and rare situations like that
    
    I dont know how many Ygopro games have the shuffle hand button , but cus it all happens in 1 effect , you dont get a chance to shuffle your hand before you discard , so the script does it for you

--- c1475311.lua
+++ c1475311.lua
@@ -2,7 +2,7 @@
 function c1475311.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -24,6 +24,7 @@ function c1475311.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(p,Card.IsAttribute,p,LOCATION_HAND,0,1,1,nil,ATTRIBUTE_DARK)
 	local tg=g:GetFirst()
 	if tg then
+		Duel.ShuffleHand(p)
 		if Duel.Remove(tg,POS_FACEUP,REASON_EFFECT)==0 then
 			Duel.ConfirmCards(1-p,tg)
 			Duel.ShuffleHand(p)
