8a866e8b071e6d46e9bf3571e67f016d4b809a1c
Hello23-Ygopro <Hello23-Ygopro@users.noreply.github.com>
2017-01-11 16:18:07 +0200

    Added CATEGORY_TOGRAVE
    I dont know if CATEGORY_TOGRAVE is needed , cus its not guaranteed that ur whole hand will go to the grave..

--- c1475311.lua
+++ c1475311.lua
@@ -2,7 +2,7 @@
 function c1475311.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DRAW+CATEGORY_REMOVE)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_REMOVE+CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
