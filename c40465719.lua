a79f9a440ab521e28dcb7489542f8960e22b7f31
Hello23-Ygopro <Hello23-Ygopro@users.noreply.github.com>
2017-01-17 19:53:33 +0200

    add shuffle hand for Oracle of Zefra, etc.
    same as Allure of Darkness : https://github.com/Fluorohydride/ygopro-scripts/pull/742/commits/03a6790d55d14628872a18137b271db4c98c712c

--- c40465719.lua
+++ c40465719.lua
@@ -19,6 +19,7 @@ end
 function c40465719.activate(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
+	Duel.ShuffleHand(p)
 	Duel.BreakEffect()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(p,Card.IsType,p,LOCATION_HAND,0,1,1,nil,TYPE_NORMAL)
