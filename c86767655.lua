4317a21c1956ae74607ed24fa3319fe89d3a370c
nekrozar <nekrozar@users.noreply.github.com>
2017-02-04 10:46:58 +0900

    fix Ms. Judge (#794)

--- c86767655.lua
+++ c86767655.lua
@@ -1,6 +1,13 @@
 --Ms.JUDGE
 function c86767655.initial_effect(c)
 	--disable
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCode(EVENT_CHAINING)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetOperation(aux.chainreg)
+	c:RegisterEffect(e0)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_CHAIN_SOLVING)
@@ -11,7 +18,7 @@ function c86767655.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c86767655.discon(e,tp,eg,ep,ev,re,r,rp)
-	return rp~=tp and Duel.IsChainDisablable(ev)
+	return rp~=tp and Duel.IsChainDisablable(ev) and e:GetHandler():GetFlagEffect(1)>0
 end
 function c86767655.disop(e,tp,eg,ep,ev,re,r,rp)
 	local c1,c2=Duel.TossCoin(tp,2)
