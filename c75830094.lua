7f4f5027d24f51055323e19dfcdbc4dd69a0f2c5
Hello23-Ygopro <Hello23-Ygopro@users.noreply.github.com>
2017-02-13 23:46:28 +0200

    fix Horus the Black Flame Dragon LV6, Armed Dragon LV5, etc.

--- c75830094.lua
+++ c75830094.lua
@@ -4,6 +4,7 @@ function c75830094.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_BATTLE_DESTROYING)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetOperation(c75830094.bdop)
 	c:RegisterEffect(e1)
 	--control
