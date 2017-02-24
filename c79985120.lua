b0e4436edb0271ec8feaa243493e716e5cac2417
nekrozar <unyuho.buster@gmail.com>
2017-02-24 17:12:30 +0900

    new

---
+++ c79985120.lua
@@ -0,0 +1,80 @@
+--RR－レヴォリューション・ファルコン－エアレイド
+function c79985120.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_WINDBEAST),6,3,c79985120.ovfilter,aux.Stringid(79985120,0),2,c79985120.xyzop)
+	c:EnableReviveLimit()
+	--atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(79985120,1))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(c79985120.descon)
+	e1:SetTarget(c79985120.destg)
+	e1:SetOperation(c79985120.desop)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(79985120,2))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCondition(c79985120.spcon)
+	e2:SetTarget(c79985120.sptg)
+	e2:SetOperation(c79985120.spop)
+	c:RegisterEffect(e2)
+end
+function c79985120.cfilter(c)
+	return c:IsSetCard(0x95) and c:IsType(TYPE_SPELL) and c:IsDiscardable()
+end
+function c79985120.ovfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xba) and c:IsType(TYPE_XYZ) and c:IsRankBelow(5)
+end
+function c79985120.xyzop(e,tp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c79985120.cfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,c79985120.cfilter,1,1,REASON_COST+REASON_DISCARD,nil)
+end
+function c79985120.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_XYZ
+end
+function c79985120.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,g:GetFirst():GetAttack()/2)
+end
+function c79985120.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local dam=tc:GetAttack()
+		if dam<0 or tc:IsFacedown() then dam=0 end
+		if Duel.Destroy(tc,REASON_EFFECT)~=0 then
+			Duel.Damage(1-tp,dam,REASON_EFFECT)
+		end
+	end
+end
+function c79985120.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetPreviousControler()==tp and rp==1-tp and c:IsReason(REASON_DESTROY)
+end
+function c79985120.spfilter(c,e,tp)
+	return c:IsCode(81927732) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c79985120.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c79985120.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c79985120.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c79985120.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	if g:GetCount()>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)~=0 and c:IsRelateToEffect(e) then
+		Duel.Overlay(g:GetFirst(),Group.FromCards(c))
+	end
+end
