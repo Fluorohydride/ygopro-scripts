0a0054aa7f4034189054026244a125252f8172a6
Mercury233 <me@mercury233.me>
2017-03-05 22:58:26 +0800

    fix (#817)

--- c50470982.lua
+++ c50470982.lua
@@ -2,7 +2,7 @@
 function c50470982.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DRAW+CATEGORY_COIN)
+	e1:SetCategory(CATEGORY_COIN)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(c50470982.target)
