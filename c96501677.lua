fcfd86a3a00b10cbcfd4d11271de81b1801b8893
Mercury233 <me@mercury233.me>
2017-04-07 12:54:27 +0800

    fix defense related effects vs link monsters (#841)
    * fix
    * fix
    * update
    * fix

--- c96501677.lua
+++ c96501677.lua
@@ -28,7 +28,7 @@ function c96501677.ccon(e)
 	return Duel.IsExistingMatchingCard(c96501677.cfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,e:GetHandler())
 end
 function c96501677.deftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsFaceup() and chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) end
+	if chkc then return aux.nzdef(chkc) and chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) end
 	if chk==0 then return Duel.IsExistingTarget(aux.nzdef,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,aux.nzdef,tp,0,LOCATION_MZONE,1,1,nil)
