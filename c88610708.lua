3ccc451ba70541ba4c358f5b9a02e05a07df5740
mercury233 <me@mercury233.me>
2017-07-18 11:55:44 +0800

    fix

--- c88610708.lua
+++ c88610708.lua
@@ -24,9 +24,11 @@ function c88610708.initial_effect(c)
 	c:RegisterEffect(e3)
 	--no damage
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_EQUIP)
-	e4:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e4:SetValue(1)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCondition(c88610708.damcon)
+	e4:SetOperation(c88610708.damop)
 	c:RegisterEffect(e4)
 end
 function c88610708.eqlimit(e,c)
@@ -48,4 +50,11 @@ function c88610708.operation(e,tp,eg,ep,ev,re,r,rp)
 	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		Duel.Equip(tp,c,tc)
 	end
+end
+function c88610708.damcon(e,tp,eg,ep,ev,re,r,rp)
+	local ec=e:GetHandler():GetEquipTarget()
+	return ec and ep==tp and (Duel.GetAttacker()==ec or Duel.GetAttackTarget()==ec)
+end
+function c88610708.damop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangeBattleDamage(tp,0)
 end
