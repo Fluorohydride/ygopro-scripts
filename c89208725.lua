615f12ff35bf60a947bbec680778aa44cb3de877
Mercury233 <me@mercury233.me>
2017-07-28 15:22:12 +0800

    fix

--- c89208725.lua
+++ c89208725.lua
@@ -2,7 +2,7 @@
 function c89208725.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetCategory(CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(c89208725.target)
