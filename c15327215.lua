3b095f5cd9869d722b7044a647091f784f5d0898
nekrozar <nekrozar@users.noreply.github.com>
2017-09-06 16:01:00 +0900

    new (#919)

---
+++ c15327215.lua
@@ -0,0 +1,83 @@
+--六武衆の真影
+function c15327215.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(15327215,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,15327215)
+	e1:SetCondition(c15327215.spcon)
+	e1:SetTarget(c15327215.sptg)
+	e1:SetOperation(c15327215.spop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(15327215,1))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetCountLimit(1)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCost(c15327215.cost)
+	e3:SetOperation(c15327215.operation)
+	c:RegisterEffect(e3)
+end
+function c15327215.cfilter(c,tp)
+	return c:IsFaceup() and c:GetSummonPlayer()==tp and c:IsSetCard(0x103d)
+end
+function c15327215.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c15327215.cfilter,1,nil,tp)
+end
+function c15327215.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c15327215.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c15327215.filter(c,mc)
+	return c:IsLevelBelow(4) and c:IsSetCard(0x3d) and c:IsAbleToRemoveAsCost()
+		and not (c:GetLevel()==mc:GetLevel() and c:IsAttribute(mc:GetAttribute()) and c:GetAttack()==mc:GetAttack() and c:GetDefense()==mc:GetDefense())
+end
+function c15327215.cost(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(c15327215.filter,tp,LOCATION_GRAVE,0,1,nil,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c15327215.filter,tp,LOCATION_GRAVE,0,1,1,nil,e:GetHandler())
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+	e:SetLabelObject(g:GetFirst())
+end
+function c15327215.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=e:GetLabelObject()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local lv=tc:GetLevel()
+		local att=tc:GetAttribute()
+		local atk=tc:GetAttack()
+		local def=tc:GetDefense()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL_FINAL)
+		e1:SetValue(lv)
+		e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+		e2:SetValue(att)
+		c:RegisterEffect(e2)
+		local e3=e1:Clone()
+		e3:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e3:SetValue(atk)
+		c:RegisterEffect(e3)
+		local e4=e1:Clone()
+		e4:SetCode(EFFECT_SET_DEFENSE_FINAL)
+		e4:SetValue(def)
+		c:RegisterEffect(e4)
+	end
+end
