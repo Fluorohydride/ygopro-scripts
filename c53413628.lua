c967a680d04264b5e291881a65c352922afcd4bc
mercury233 <me@mercury233.me>
2017-09-12 20:40:22 +0800

    new card 17CC

---
+++ c53413628.lua
@@ -0,0 +1,36 @@
+--コード・トーカー
+function c53413628.initial_effect(c)
+	--link summon
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkType,TYPE_EFFECT),2,2)
+	c:EnableReviveLimit()
+	--atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(c53413628.atkval)
+	c:RegisterEffect(e1)
+	--indes
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetCondition(c53413628.incon)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetValue(c53413628.inval)
+	c:RegisterEffect(e3)
+end
+function c53413628.atkval(e,c)
+	return c:GetLinkedGroupCount()*500
+end
+function c53413628.incon(e)
+	return e:GetHandler():GetLinkedGroupCount()>0
+end
+function c53413628.inval(e,re,r,rp)
+	return rp~=e:GetHandlerPlayer()
+end
