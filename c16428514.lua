ae655e096daeea99b934638e0f9c7d0bad3eedb1
nekrozar <nekrozar@users.noreply.github.com>
2017-09-30 20:04:53 +0900

    new card VJMP (#933)

---
+++ c16428514.lua
@@ -0,0 +1,76 @@
+--サブテラーの導師
+function c16428514.initial_effect(c)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(16428514,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,16428514)
+	e1:SetTarget(c16428514.thtg)
+	e1:SetOperation(c16428514.thop)
+	c:RegisterEffect(e1)
+	--position
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(16428514,1))
+	e2:SetCategory(CATEGORY_POSITION)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,16428515)
+	e2:SetCondition(c16428514.setcon1)
+	e2:SetTarget(c16428514.settg)
+	e2:SetOperation(c16428514.setop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetHintTiming(0,0x1e0)
+	e3:SetCondition(c16428514.setcon2)
+	c:RegisterEffect(e3)
+end
+function c16428514.thfilter(c)
+	return c:IsSetCard(0xed) and not c:IsCode(16428514) and c:IsAbleToHand()
+end
+function c16428514.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c16428514.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c16428514.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c16428514.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c16428514.setcfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xed)
+end
+function c16428514.setcon1(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(c16428514.setcfilter,tp,LOCATION_ONFIELD,0,1,e:GetHandler())
+end
+function c16428514.setcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c16428514.setcfilter,tp,LOCATION_ONFIELD,0,1,e:GetHandler())
+end
+function c16428514.setfilter(c)
+	return c:IsFaceup() and c:IsCanTurnSet()
+end
+function c16428514.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc~=c and c16428514.setfilter(chkc) end
+	if chk==0 then return c16428514.setfilter(c)
+		and Duel.IsExistingTarget(c16428514.setfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectTarget(tp,c16428514.setfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,c)
+	g:AddCard(c)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,2,0,0)
+end
+function c16428514.setop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsFaceup() and c:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local g=Group.FromCards(c,tc)
+		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
+	end
+end
