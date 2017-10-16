ea41f780d374794024b89f8f1c900afb8889d1c0
nekrozar <nekrozar@users.noreply.github.com>
2017-10-16 15:42:03 +0900

    fix (#955)

--- c39440937.lua
+++ c39440937.lua
@@ -19,17 +19,16 @@ function c39440937.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c39440937.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(c39440937.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	if g:GetCount()>0 then
-		Duel.ChangePosition(g,POS_FACEUP_DEFENSE)
+	if g:GetCount()>0 and Duel.ChangePosition(g,POS_FACEUP_DEFENSE)~=0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		e1:SetCondition(c39440937.tdcon)
+		e1:SetOperation(c39440937.tdop)
+		Duel.RegisterEffect(e1,tp)
 	end
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetCountLimit(1)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	e1:SetCondition(c39440937.tdcon)
-	e1:SetOperation(c39440937.tdop)
-	Duel.RegisterEffect(e1,tp)
 end
 function c39440937.tdfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsAbleToExtra()
