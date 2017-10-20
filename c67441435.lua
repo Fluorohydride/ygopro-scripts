14c974b43bf27ef9be4013de65381cb63bb110a0
Mercury233 <me@mercury233.me>
2017-10-20 20:43:54 +0800

    fix

--- c67441435.lua
+++ c67441435.lua
@@ -15,7 +15,7 @@ function c67441435.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function c67441435.operation(e,tp,eg,ep,ev,re,r,rp)
