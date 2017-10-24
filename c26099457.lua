8f19e355e5a5f6eac2d58d532f9d91b73c72bf8f
Mercury233 <me@mercury233.me>
2017-10-24 17:33:29 +0800

    fix

--- c26099457.lua
+++ c26099457.lua
@@ -11,15 +11,15 @@ function c26099457.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c26099457.filter(c)
-	return c:IsFaceup() and not c:IsType(TYPE_XYZ)
+	return c:IsFaceup() and c:IsLevelAbove(0)
 end
 function c26099457.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c26099457.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c26099457.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c26099457.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,c26099457.filter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINGMSG_LVRANK)
-	local lv=Duel.AnnounceLevel(tp,1,3)
+	local lv=Duel.AnnounceLevel(tp,1,3,g:GetFirst():GetLevel())
 	e:SetLabel(lv)
 end
 function c26099457.activate(e,tp,eg,ep,ev,re,r,rp)
