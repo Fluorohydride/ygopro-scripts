bcd9a5c22a09ca29706401be11547009866e2004
nekrozar <nekrozar@users.noreply.github.com>
2017-10-24 18:27:29 +0900

    new (#965)

---
+++ c99111753.lua
@@ -0,0 +1,35 @@
+--セキュリティ・ドラゴン
+function c99111753.initial_effect(c)
+	--link summon
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,nil,2,2)
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(99111753,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_NO_TURN_RESET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,99111753)
+	e1:SetCondition(c99111753.thcon)
+	e1:SetTarget(c99111753.thtg)
+	e1:SetOperation(c99111753.thop)
+	c:RegisterEffect(e1)
+end
+function c99111753.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetMutualLinkedGroupCount()>0
+end
+function c99111753.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+	e:GetHandler():RegisterFlagEffect(0,RESET_EVENT+0x1fe0000,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(99111753,1))
+end
+function c99111753.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
