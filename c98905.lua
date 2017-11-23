d611a47144831994c02045f4fe6200d8c24e89eb
VanillaSalt <vanillasalt5197@gmail.com>
2017-11-23 12:29:25 +0900

    fix

--- c98905.lua
+++ c98905.lua
@@ -18,15 +18,15 @@ function c98905.initial_effect(c)
 		Duel.RegisterEffect(ge1,0)
 	end
 end
-function c98905.cfilter(c,tp)
+function c98905.cfilter(c)
+	local tp=c:GetPreviousControler()
 	return c:IsSetCard(0xe5) and c:IsType(TYPE_XYZ) and c:IsControler(tp) and c:GetOverlayCount()>0
 end
 function c98905.checkop(e,tp,eg,ep,ev,re,r,rp)
-	if not eg then return end
-	local sg=eg:Filter(c98905.cfilter,nil,tp)
+	local sg=eg:Filter(c98905.cfilter,nil)
 	local tc=sg:GetFirst()
 	while tc do
-		tc:RegisterFlagEffect(98905,RESET_EVENT+0x1fe0000-RESET_TOGRAVE,0,1)
+		tc:RegisterFlagEffect(98905,RESET_EVENT+0x1fe0000,0,1)
 		tc=sg:GetNext()
 	end
 end
@@ -42,7 +42,8 @@ end
 function c98905.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return eg:IsContains(chkc) and c98905.filter(chkc,e,tp) end
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,59822133)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.GetLocationCountFromEx(tp)>0
 		and eg:IsExists(c98905.filter,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=eg:FilterSelect(tp,c98905.filter,1,1,nil,e,tp)
@@ -51,7 +52,7 @@ function c98905.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c98905.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 or Duel.GetLocationCountFromEx(tp)<=0 then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
