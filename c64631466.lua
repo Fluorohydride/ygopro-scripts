5d53d04c57da603ef643f0bfbd41f3cd0360e85a
mercury233 <me@mercury233.me>
2017-11-24 14:06:13 +0800

    update

--- c64631466.lua
+++ c64631466.lua
@@ -21,7 +21,6 @@ function c64631466.initial_effect(c)
 	e2:SetCode(EFFECT_SET_ATTACK)
 	e2:SetCondition(c64631466.adcon)
 	e2:SetValue(c64631466.atkval)
-	e2:SetLabelObject(e1)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
@@ -30,7 +29,6 @@ function c64631466.initial_effect(c)
 	e3:SetCode(EFFECT_SET_DEFENSE)
 	e3:SetCondition(c64631466.adcon)
 	e3:SetValue(c64631466.defval)
-	e3:SetLabelObject(e1)
 	c:RegisterEffect(e3)
 	--damage
 	local e4=Effect.CreateEffect(c)
@@ -40,12 +38,17 @@ function c64631466.initial_effect(c)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCondition(c64631466.damcon)
 	e4:SetOperation(c64631466.damop)
-	e4:SetLabelObject(e1)
 	c:RegisterEffect(e4)
 end
 function c64631466.eqcon(e,tp,eg,ep,ev,re,r,rp)
-	local ec=e:GetLabelObject()
-	return ec==nil or ec:GetFlagEffect(64631466)==0
+	return c64631466.can_equip_monster(e:GetHandler())
+end
+function c64631466.eqfilter(c)
+	return c:GetFlagEffect(64631466)~=0
+end
+function c64631466.can_equip_monster(c)
+	local g=c:GetEquipGroup():Filter(c64631466.eqfilter,nil)
+	return g:GetCount()==0
 end
 function c64631466.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsAbleToChangeControler() end
@@ -58,30 +61,32 @@ end
 function c64631466.eqlimit(e,c)
 	return e:GetOwner()==c
 end
+function c64631466.equip_monster(c,tp,tc)
+	if not Duel.Equip(tp,tc,c,false) then return end
+	--Add Equip limit
+	tc:RegisterFlagEffect(64631466,RESET_EVENT+0x1fe0000,0,0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
+	e1:SetCode(EFFECT_EQUIP_LIMIT)
+	e1:SetReset(RESET_EVENT+0x1fe0000)
+	e1:SetValue(c64631466.eqlimit)
+	tc:RegisterEffect(e1)
+	--substitute
+	local e2=Effect.CreateEffect(c)
+ 	e2:SetType(EFFECT_TYPE_EQUIP)
+ 	e2:SetCode(EFFECT_DESTROY_SUBSTITUTE)
+ 	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+ 	e2:SetReset(RESET_EVENT+0x1fe0000)
+ 	e2:SetValue(c64631466.repval)
+ 	tc:RegisterEffect(e2)
+end
 function c64631466.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsType(TYPE_MONSTER) and tc:IsControler(1-tp) then
 		if c:IsFaceup() and c:IsRelateToEffect(e) then
-			if not Duel.Equip(tp,tc,c,false) then return end
-			--Add Equip limit
-			tc:RegisterFlagEffect(64631466,RESET_EVENT+0x1fe0000,0,0)
-			e:SetLabelObject(tc)
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
-			e1:SetCode(EFFECT_EQUIP_LIMIT)
-			e1:SetReset(RESET_EVENT+0x1fe0000)
-			e1:SetValue(c64631466.eqlimit)
-			tc:RegisterEffect(e1)
-			--substitute
-			local e2=Effect.CreateEffect(c)
- 			e2:SetType(EFFECT_TYPE_EQUIP)
- 			e2:SetCode(EFFECT_DESTROY_SUBSTITUTE)
- 			e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
- 			e2:SetReset(RESET_EVENT+0x1fe0000)
- 			e2:SetValue(c64631466.repval)
- 			tc:RegisterEffect(e2)
+			c64631466.equip_monster(c,tp,tc)
 		else Duel.SendtoGrave(tc,REASON_EFFECT) end
 	end
 end
@@ -89,30 +94,34 @@ function c64631466.repval(e,re,r,rp)
 	return bit.band(r,REASON_BATTLE)~=0
 end
 function c64631466.damcon(e,tp,eg,ep,ev,re,r,rp)
-	local ec=e:GetLabelObject():GetLabelObject()
-	return ec and ec:GetFlagEffect(64631466)~=0 and ep==tp
+	local c=e:GetHandler()
+	local g=c:GetEquipGroup():Filter(c64631466.eqfilter,nil)
+	return g:GetCount()>0 and ep==tp
 		and (Duel.GetAttacker()==e:GetHandler() or Duel.GetAttackTarget()==e:GetHandler())
 end
 function c64631466.damop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Damage(1-tp,ev,REASON_EFFECT)
 end
 function c64631466.adcon(e)
-	local ec=e:GetLabelObject():GetLabelObject()
-	return ec and ec:GetFlagEffect(64631466)~=0
+	local c=e:GetHandler()
+	local g=c:GetEquipGroup():Filter(c64631466.eqfilter,nil)
+	return g:GetCount()>0
 end
 function c64631466.atkval(e,c)
-	local ec=e:GetLabelObject():GetLabelObject()
-	local atk=ec:GetTextAttack()
-	if ec:IsFacedown() or bit.band(ec:GetOriginalType(),TYPE_MONSTER)==0 or atk<0 then
+	local c=e:GetHandler()
+	local g=c:GetEquipGroup():Filter(c64631466.eqfilter,nil)
+	local atk=g:GetFirst():GetTextAttack()
+	if g:GetFirst():IsFacedown() or bit.band(g:GetFirst():GetOriginalType(),TYPE_MONSTER)==0 or atk<0 then
 		return 0
 	else
 		return atk
 	end
 end
 function c64631466.defval(e,c)
-	local ec=e:GetLabelObject():GetLabelObject()
-	local def=ec:GetTextDefense()
-	if ec:IsFacedown() or bit.band(ec:GetOriginalType(),TYPE_MONSTER)==0 or def<0 then
+	local c=e:GetHandler()
+	local g=c:GetEquipGroup():Filter(c64631466.eqfilter,nil)
+	local def=g:GetFirst():GetTextDefense()
+	if g:GetFirst():IsFacedown() or bit.band(g:GetFirst():GetOriginalType(),TYPE_MONSTER)==0 or def<0 then
 		return 0
 	else
 		return def
