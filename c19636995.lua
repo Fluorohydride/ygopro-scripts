d0177fdda4b73e9369830236064da0905b51c1f8
VanillaSalt <vanillasalt5197@gmail.com>
2018-03-07 23:50:14 +0900

    update

--- c19636995.lua
+++ c19636995.lua
@@ -1,5 +1,4 @@
 --急き兎馬
---not fully implemented
 function c19636995.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
@@ -16,19 +15,13 @@ function c19636995.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(19636995,0))
 	e2:SetCategory(CATEGORY_DESTROY)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_CUSTOM+19636995)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_MOVE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(c19636995.descon)
 	e2:SetTarget(c19636995.destg)
 	e2:SetOperation(c19636995.desop)
 	c:RegisterEffect(e2)
-	--custom EVENT_PLACED
-	local e2a=Effect.CreateEffect(c)
-	e2a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-	e2a:SetRange(LOCATION_MZONE)
-	e2a:SetCode(EVENT_ADJUST)
-	e2a:SetOperation(c19636995.plchk)
-	c:RegisterEffect(e2a)
 	--direct attack
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(19636995,1))
@@ -57,6 +50,12 @@ function c19636995.hspval(e,c)
 	end
 	return 0,zone
 end
+function c19636995.desfilter(c,g)
+	return not c:IsStatus(STATUS_SUMMONING+STATUS_SUMMON_DISABLED) and g:IsContains(c)
+end
+function c19636995.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetColumnGroup():IsExists(c19636995.desfilter,1,nil,eg)
+end
 function c19636995.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
@@ -64,26 +63,6 @@ end
 function c19636995.desop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsRelateToEffect(e) then
 		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
-	end
-end
-function c19636995.plfilter(c)
-	return not c:IsStatus(STATUS_SUMMONING) and not c:IsStatus(STATUS_SUMMON_DISABLED)
-end
-function c19636995.gfilter(c,g)
-	return not g:IsContains(c)
-end
-function c19636995.plchk(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local cg=c:GetColumnGroup():Filter(c19636995.plfilter,nil)
-	if c:GetFlagEffect(19636996)==0 or c:GetFlagEffectLabel(19636996)~=c:GetSequence() then
-		c:ResetFlagEffect(19636996)
-		c:RegisterFlagEffect(19636996,RESET_EVENT+0x1fd0000,0,1,c:GetSequence())
-		cg:KeepAlive()
-		e:SetLabelObject(cg)
-	elseif cg:IsExists(c19636995.gfilter,1,nil,e:GetLabelObject()) then
-		cg:KeepAlive()
-		e:SetLabelObject(cg)
-		Duel.RaiseSingleEvent(c,EVENT_CUSTOM+19636995,e,0,0,0,0)
 	end
 end
 function c19636995.datop(e,tp,eg,ep,ev,re,r,rp)
