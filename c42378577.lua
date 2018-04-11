5c9830abd14d5285be45ef2d45d464a32f65590b
mercury233 <me@mercury233.me>
2018-04-11 14:23:35 +0800

    fix

--- c42378577.lua
+++ c42378577.lua
@@ -64,7 +64,7 @@ function c42378577.penop(e,tp,eg,ep,ev,re,r,rp)
 	if not (Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1))
 		or not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
 	end
 end
