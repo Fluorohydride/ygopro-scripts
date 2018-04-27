81a8024f06e51556a9ec4a17fce4797aafe3b6ce
Aephiex <34618932+Aephiex@users.noreply.github.com>
2018-04-27 15:00:56 +0800

    Update c29146185.lua

--- c29146185.lua
+++ c29146185.lua
@@ -80,6 +80,8 @@ function c29146185.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 end
 function c29146185.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	local exc=e:GetHandler()
+	if not exc:IsRelateToEffect(e) then exc=nil end
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,exc)
 	Duel.Destroy(g,REASON_EFFECT)
 end
