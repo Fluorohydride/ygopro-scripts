bbf78b6e9326eb8ec2b3cc395ec76bb32d8f1b88
mercury233 <me@mercury233.me>
2018-05-10 15:43:36 +0800

    fix

--- c4388680.lua
+++ c4388680.lua
@@ -72,6 +72,7 @@ function c4388680.thop(e,tp,eg,ep,ev,re,r,rp)
 	local hg=Duel.SelectMatchingCard(tp,c4388680.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if hg:GetCount()>0 and Duel.SendtoHand(hg,tp,REASON_EFFECT)>0
 		and hg:GetFirst():IsLocation(LOCATION_HAND) then
+		Duel.ConfirmCards(1-tp,hg)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 		local g=Duel.SelectMatchingCard(tp,c4388680.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
 		if g:GetCount()>0 then
