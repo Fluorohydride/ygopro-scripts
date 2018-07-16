dedbfd9ac1f85547ca05c64c54883fd7c9adf8bc
DailyShana <p2357111317192329@gmail.com>
2018-07-16 12:12:43 +0800

    fix "cannot add cards from the deck to the hand"

--- c4227096.lua
+++ c4227096.lua
@@ -11,7 +11,9 @@ function c4227096.activate(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_TO_HAND)
-	e1:SetTargetRange(LOCATION_DECK,LOCATION_DECK)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,1)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsLocation,LOCATION_DECK))
 	if Duel.GetTurnPlayer()==tp then
 		e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
 	else
