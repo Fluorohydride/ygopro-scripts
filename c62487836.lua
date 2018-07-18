763e62ed4c43202ea72a4f387382cada74159b56
mercury233 <me@mercury233.me>
2018-07-18 21:24:46 +0800

    fix

--- c62487836.lua
+++ c62487836.lua
@@ -3,6 +3,7 @@ function c62487836.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(c62487836.condition)
