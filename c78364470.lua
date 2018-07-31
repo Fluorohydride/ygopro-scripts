45101317d4fa8bcbf651da664e63e5078998b0d2
Mercury233 <me@mercury233.me>
2018-07-31 16:38:46 +0800

    fix

--- c78364470.lua
+++ c78364470.lua
@@ -8,14 +8,14 @@ function c78364470.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c78364470.sumop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFlagEffect(tp,78364471)~=0 then return end
+	if Duel.GetFlagEffect(tp,78364470)~=0 then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetDescription(aux.Stringid(78364471,0))
+	e1:SetDescription(aux.Stringid(78364470,0))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
 	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
 	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x53))
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	Duel.RegisterFlagEffect(tp,78364471,RESET_PHASE+PHASE_END,0,1)
+	Duel.RegisterFlagEffect(tp,78364470,RESET_PHASE+PHASE_END,0,1)
 end
