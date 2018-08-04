7ed1bd4f78578894b4b74885962b102a26418883
Mercury233 <me@mercury233.me>
2018-08-04 15:17:46 +0800

    fix

--- c73642296.lua
+++ c73642296.lua
@@ -15,6 +15,9 @@ function c73642296.initial_effect(c)
 	e1:SetOperation(c73642296.disop)
 	c:RegisterEffect(e1)
 end
+function c73642296.cfilter(c)
+	return c:IsLocation(LOCATION_GRAVE) and c:IsType(TYPE_MONSTER)
+end
 function c73642296.discon(e,tp,eg,ep,ev,re,r,rp)
 	local ex1,g1,gc1,dp1,dv1=Duel.GetOperationInfo(ev,CATEGORY_TOHAND)
 	local ex2,g2,gc2,dp2,dv2=Duel.GetOperationInfo(ev,CATEGORY_TODECK)
@@ -24,7 +27,7 @@ function c73642296.discon(e,tp,eg,ep,ev,re,r,rp)
 	return ((ex1 and (bit.band(dv1,LOCATION_GRAVE)==LOCATION_GRAVE or g1 and g1:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE)))
 		or (ex2 and (bit.band(dv2,LOCATION_GRAVE)==LOCATION_GRAVE or g2 and g2:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE)))
 		or (ex3 and (bit.band(dv3,LOCATION_GRAVE)==LOCATION_GRAVE or g3 and g3:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE)))
-		or (ex4 and (bit.band(dv4,LOCATION_GRAVE)==LOCATION_GRAVE or g4 and g4:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE)))
+		or (ex4 and (bit.band(dv4,LOCATION_GRAVE)==LOCATION_GRAVE or g4 and g4:IsExists(c73642296.cfilter,1,nil)))
 		or (ex5 and (bit.band(dv5,LOCATION_GRAVE)==LOCATION_GRAVE or g5 and g5:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE))))
 		and Duel.IsChainNegatable(ev)
 end
