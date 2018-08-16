1706380edbc5871ef9df67ee343467923e315051
mercury233 <me@mercury233.me>
2018-08-16 15:25:41 +0800

    fix id

--- c39753577.lua
+++ c39753577.lua
@@ -1,23 +1,23 @@
 --魔妖変生
-function c39752577.initial_effect(c)
+function c39753577.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCountLimit(1,39752577+EFFECT_COUNT_CODE_OATH)
-	e1:SetCost(c39752577.cost)
-	e1:SetTarget(c39752577.target)
-	e1:SetOperation(c39752577.activate)
+	e1:SetCountLimit(1,39753577+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(c39753577.cost)
+	e1:SetTarget(c39753577.target)
+	e1:SetOperation(c39753577.activate)
 	c:RegisterEffect(e1)
-	Duel.AddCustomActivityCounter(39752577,ACTIVITY_SPSUMMON,c39752577.counterfilter)
+	Duel.AddCustomActivityCounter(39753577,ACTIVITY_SPSUMMON,c39753577.counterfilter)
 end
-function c39752577.counterfilter(c)
+function c39753577.counterfilter(c)
 	return c:GetSummonLocation()~=LOCATION_EXTRA or c:IsSetCard(0x121)
 end
-function c39752577.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetCustomActivityCount(39752577,tp,ACTIVITY_SPSUMMON)==0 and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+function c39753577.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(39753577,tp,ACTIVITY_SPSUMMON)==0 and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
 	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -26,23 +26,23 @@ function c39752577.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetTargetRange(1,0)
 	e1:SetLabelObject(e)
-	e1:SetTarget(c39752577.splimit)
+	e1:SetTarget(c39753577.splimit)
 	Duel.RegisterEffect(e1,tp)
 end
-function c39752577.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+function c39753577.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return c:IsLocation(LOCATION_EXTRA) and not c:IsSetCard(0x121)
 end
-function c39752577.spfilter(c,e,tp)
+function c39753577.spfilter(c,e,tp)
 	return c:IsSetCard(0x121) and (c:IsFaceup() or not c:IsLocation(LOCATION_REMOVED)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
-function c39752577.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and c39752577.spfilter(chkc,e,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingTarget(c39752577.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
+function c39753577.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and c39753577.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingTarget(c39753577.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g1=Duel.SelectTarget(tp,c39752577.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+	local g1=Duel.SelectTarget(tp,c39753577.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g1,1,0,0)
 end
-function c39752577.activate(e,tp,eg,ep,ev,re,r,rp)
+function c39753577.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
 		local e2=Effect.CreateEffect(e:GetHandler())
