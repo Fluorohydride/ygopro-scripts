dcd58a1d06fdedf256dbb2e6595d49c7bb64b131
DailyShana <p2357111317192329@gmail.com>
2018-08-22 17:58:35 +0800

    fix

--- c90020780.lua
+++ c90020780.lua
@@ -8,9 +8,9 @@ function c90020780.initial_effect(c)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,90020780)
-	e1:SetCondition(c90020780.dcondition)
-	e1:SetTarget(c90020780.dtarget)
-	e1:SetOperation(c90020780.doperation)
+	e1:SetCondition(c90020780.drcon)
+	e1:SetTarget(c90020780.drtg)
+	e1:SetOperation(c90020780.drop)
 	c:RegisterEffect(e1)
 	--banished
 	local e2=Effect.CreateEffect(c)
@@ -19,26 +19,26 @@ function c90020780.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_REMOVE)
 	e2:SetCountLimit(1,90020781)
-	e2:SetCondition(c90020780.spcondition)
-	e2:SetTarget(c90020780.sptarget)
-	e2:SetOperation(c90020780.spoperation)
+	e2:SetCondition(c90020780.spcon)
+	e2:SetTarget(c90020780.sptg)
+	e2:SetOperation(c90020780.spop)
 	c:RegisterEffect(e2)
 end
-function c90020780.dfilter(c,tp)
+function c90020780.cfilter(c,tp)
 	return c:IsPreviousLocation(LOCATION_GRAVE) and c:GetPreviousControler()==tp and c:IsRace(RACE_ZOMBIE)
 end
-function c90020780.dcondition(e,tp,eg,ep,ev,re,r,rp)
+function c90020780.drcon(e,tp,eg,ep,ev,re,r,rp)
 	if eg:IsContains(e:GetHandler()) then return false end
-	return eg:IsExists(c90020780.dfilter,1,e:GetHandler(),tp)
+	return eg:IsExists(c90020780.cfilter,1,e:GetHandler(),tp)
 end
-function c90020780.dtarget(e,tp,eg,ep,ev,re,r,rp,chk)
+function c90020780.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
 end
-function c90020780.doperation(e,tp,eg,ep,ev,re,r,rp)
+function c90020780.drop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	if Duel.Draw(p,d,REASON_EFFECT)~=0 then
 		Duel.ShuffleHand(tp)
@@ -46,16 +46,17 @@ function c90020780.doperation(e,tp,eg,ep,ev,re,r,rp)
 		Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT+REASON_DISCARD)
 	end
 end
-function c90020780.spcondition(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousLocation(LOCATION_GRAVE) and (e:GetHandler():IsReason(REASON_EFFECT)
-		or (e:GetHandler():IsReason(REASON_COST) and re:IsHasType(~EFFECT_TYPE_FIELD)))
+function c90020780.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_GRAVE) and (c:IsReason(REASON_EFFECT)
+		or c:IsReason(REASON_COST) and re:IsHasType(0x7e0))
 end
-function c90020780.sptarget(e,tp,eg,ep,ev,re,r,rp,chk)
+function c90020780.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c90020780.spoperation(e,tp,eg,ep,ev,re,r,rp)
+function c90020780.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
 		local e1=Effect.CreateEffect(c)
