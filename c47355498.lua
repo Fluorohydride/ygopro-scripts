17cfba4fd8333a97a5553d82c76481b0b18b0262
Mercury233 <me@mercury233.me>
2018-09-09 17:55:06 +0800

    fix Necrovalley (#1093)

--- c47355498.lua
+++ c47355498.lua
@@ -74,7 +74,7 @@ end
 function c47355498.discheck(ev,category,re,im0,im1)
 	local ex,tg,ct,p,v=Duel.GetOperationInfo(ev,category)
 	if not ex then return false end
-	if v==LOCATION_GRAVE then
+	if v==LOCATION_GRAVE and ct>0 then
 		if p==0 then return im0
 		elseif p==1 then return im1
 		elseif p==PLAYER_ALL then return im0 and im1
