407f0569630f718059d624bba6babbda2c1b980a
nekrozar <nekrozar.s@gmail.com>
2018-10-20 22:20:04 +0900

    fix

--- c6901008.lua
+++ c6901008.lua
@@ -6,5 +6,9 @@ function c6901008.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_NONTUNER)
+	e1:SetValue(c6901008.tnval)
 	c:RegisterEffect(e1)
+end
+function c6901008.tnval(e,c)
+	return e:GetHandler():IsControler(c:GetControler())
 end
