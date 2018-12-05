0b1670f9109afc2be487f0fb99d22b32ea2360dd
Mercury233 <me@mercury233.me>
2018-12-05 20:57:47 +0800

    fix

--- c52551211.lua
+++ c52551211.lua
@@ -3,7 +3,7 @@ function c52551211.initial_effect(c)
 	--tohand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(52551211,0))
-	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e1:SetCountLimit(1,52551211)
