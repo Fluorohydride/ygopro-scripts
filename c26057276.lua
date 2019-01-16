1e6b78190bdc0da6de392222f803b9573b726963
purerosefallen <78877@qq.com>
2019-01-16 19:09:14 +0800

    why is CATEGORY_SEARCH needed here?

--- c26057276.lua
+++ c26057276.lua
@@ -3,7 +3,7 @@ function c26057276.initial_effect(c)
 	--tohand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(26057276,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
