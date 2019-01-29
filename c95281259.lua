109b75137e99a6b4b66acab6937c32e6f67dc447
Mercury233 <me@mercury233.me>
2019-01-29 20:53:55 +0800

    fix

--- c95281259.lua
+++ c95281259.lua
@@ -22,7 +22,7 @@ function c95281259.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c95281259.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) and tc:IsRace(RACE_WARRIOR) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 end
