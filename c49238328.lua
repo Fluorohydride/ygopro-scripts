3aada2961d086032994fe25a627495db0b5bf1b6
Mercury233 <me@mercury233.me>
2019-03-03 22:05:07 +0800

    fix

--- c49238328.lua
+++ c49238328.lua
@@ -44,6 +44,7 @@ end
 function c49238328.activate(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
+	if not e:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_DRAW)
