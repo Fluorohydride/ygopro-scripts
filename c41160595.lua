6d101da1a3bd47dc9abb8816b1b8f8877297ece1
nekrozar <nekrozar@users.noreply.github.com>
2019-03-13 23:11:51 +0900

    fix (#1168)

--- c41160595.lua
+++ c41160595.lua
@@ -3,11 +3,9 @@ function c41160595.initial_effect(c)
 	--set
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(41160595,0))
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_TO_GRAVE)
 	e1:SetCondition(c41160595.setcon)
-	e1:SetTarget(c41160595.settg)
 	e1:SetOperation(c41160595.setop)
 	c:RegisterEffect(e1)
 end
@@ -18,10 +16,6 @@ function c41160595.setcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function c41160595.filter(c)
 	return c:GetType()==TYPE_SPELL+TYPE_QUICKPLAY and c:IsSSetable()
-end
-function c41160595.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingMatchingCard(c41160595.filter,tp,LOCATION_DECK,0,1,nil) end
 end
 function c41160595.setop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
