7832ff4dc68fa9b4dfe0b653911e9ceaaa7b3f05
Mercury233 <me@mercury233.me>
2019-03-14 22:19:42 +0800

    fix

--- c95493471.lua
+++ c95493471.lua
@@ -65,7 +65,9 @@ function c95493471.drop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Draw(p,d,REASON_EFFECT)
 end
 function c95493471.cfilter2(c,tp,zone)
-	return c:IsPreviousLocation(LOCATION_MZONE) and bit.extract(zone,c:GetPreviousSequence())~=0 and c:IsType(TYPE_XYZ)
+	local seq=c:GetPreviousSequence()
+	if c:GetPreviousControler()~=tp then seq=seq+16 end
+	return c:IsPreviousLocation(LOCATION_MZONE) and bit.extract(zone,seq)~=0 and c:IsType(TYPE_XYZ)
 		and (c:IsReason(REASON_BATTLE) or c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp)
 end
 function c95493471.spcon(e,tp,eg,ep,ev,re,r,rp)
