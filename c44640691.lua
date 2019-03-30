bf7594ae24e400a825925e4e3f142e5d3fbebba8
mercury233 <me@mercury233.me>
2019-03-30 10:11:21 +0800

    new cards VJMP

---
+++ c44640691.lua
@@ -0,0 +1,72 @@
+--ローグ・オブ・エンディミオン
+function c44640691.initial_effect(c)
+	c:EnableCounterPermit(0x1)
+	--summon success
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(44640691,0))
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,44640691)
+	e1:SetTarget(c44640691.addct)
+	e1:SetOperation(c44640691.addc)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--destroy
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(44640691,1))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,44640692)
+	e3:SetCost(c44640691.setcost)
+	e3:SetTarget(c44640691.settg)
+	e3:SetOperation(c44640691.setop)
+	c:RegisterEffect(e3)
+end
+function c44640691.addct(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x1)
+end
+function c44640691.addc(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(0x1,1)
+	end
+end
+function c44640691.costfilter(c)
+	return c:IsRace(RACE_SPELLCASTER) and c:IsDiscardable()
+end
+function c44640691.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x1,1,REASON_COST)
+		and Duel.IsExistingMatchingCard(c44640691.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	e:GetHandler():RemoveCounter(tp,0x1,1,REASON_COST)
+	Duel.DiscardHand(tp,c44640691.costfilter,1,1,REASON_DISCARD+REASON_COST,nil)
+end
+function c44640691.filter(c)
+	return c:GetType()==TYPE_SPELL+TYPE_CONTINUOUS and c:IsSSetable()
+end
+function c44640691.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c44640691.filter,tp,LOCATION_DECK,0,1,nil) end
+end
+function c44640691.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,c44640691.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.SSet(tp,tc)
+		Duel.ConfirmCards(1-tp,g)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e1:SetTargetRange(1,0)
+		e1:SetValue(c44640691.aclimit)
+		e1:SetLabel(tc:GetCode())
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function c44640691.aclimit(e,re,tp)
+	return re:GetHandler():IsCode(e:GetLabel())
+end
