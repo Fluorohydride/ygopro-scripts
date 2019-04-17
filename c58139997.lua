3c13e3cbe948d50b4b28562178d201125b00317d
Mercury233 <me@mercury233.me>
2019-04-17 10:20:42 +0800

    fix

--- c58139997.lua
+++ c58139997.lua
@@ -47,7 +47,7 @@ function c58139997.spfilter(c,e,tp)
 	return c:IsSetCard(0x128) and not c:IsCode(58139997) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c58139997.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetMZoneCount(tp,e:GetHandler())>0
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(c58139997.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
