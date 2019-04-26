53a518db3a42e23ceff5775fd8d627d36ecf9278
nekrozar <nekrozar.s@gmail.com>
2019-04-26 11:09:47 +0900

    fix
    https://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=4&cid=5164

--- c94425169.lua
+++ c94425169.lua
@@ -7,11 +7,13 @@ function c94425169.initial_effect(c)
 	c:RegisterEffect(e1)
 	--LP up
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCategory(CATEGORY_RECOVER)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetCode(EVENT_TO_HAND)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCondition(c94425169.condition)
+	e2:SetTarget(c94425169.target)
 	e2:SetOperation(c94425169.operation)
 	c:RegisterEffect(e2)
 end
@@ -21,6 +23,14 @@ end
 function c94425169.condition(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(c94425169.filter,1,nil)
 end
+function c94425169.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(500)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,500)
+end
 function c94425169.operation(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Recover(tp,500,REASON_EFFECT)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Recover(p,d,REASON_EFFECT)
 end
