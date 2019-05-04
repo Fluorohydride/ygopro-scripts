68a6a430f1706489576c4bcb72c6050c4f6bc8a0
nekrozar <nekrozar.s@gmail.com>
2019-05-04 18:19:16 +0900

    fix

--- c56337500.lua
+++ c56337500.lua
@@ -17,7 +17,7 @@ function c56337500.initial_effect(c)
 	c:RegisterEffect(e1)
 	--special summon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(56337500,0))
+	e2:SetDescription(aux.Stringid(56337500,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
