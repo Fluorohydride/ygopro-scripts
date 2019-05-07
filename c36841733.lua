90b2c6b3ebf35efac866a00ab5c52eb689afaa8a
Mercury233 <me@mercury233.me>
2019-05-07 20:57:56 +0800

    fix

--- c36841733.lua
+++ c36841733.lua
@@ -60,6 +60,9 @@ function c36841733.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(atk)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
 		c:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		c:RegisterEffect(e2)
 	end
 end
 function c36841733.spcon(e,tp,eg,ep,ev,re,r,rp)
