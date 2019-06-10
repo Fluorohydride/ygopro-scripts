0c533a546eb4728b6f23269c109c6ab69d1d685d
nekrozar <nekrozar.s@gmail.com>
2019-06-10 11:32:32 +0900

    fix Danger!

--- c90807199.lua
+++ c90807199.lua
@@ -6,7 +6,6 @@ function c90807199.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(c90807199.spcost)
 	e1:SetTarget(c90807199.sptg)
 	e1:SetOperation(c90807199.spop)
@@ -15,48 +14,44 @@ function c90807199.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(90807199,1))
 	e2:SetCategory(CATEGORY_DESTROY)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCode(EVENT_DISCARD)
 	e2:SetCountLimit(1,90807199)
-	e2:SetCondition(c90807199.descon)
 	e2:SetTarget(c90807199.destg)
 	e2:SetOperation(c90807199.desop)
 	c:RegisterEffect(e2)
-	local e3=e2:Clone()
-	e3:SetCode(EVENT_REMOVE)
-	c:RegisterEffect(e3)
 end
 function c90807199.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return not c:IsPublic() and c:GetFlagEffect(90807199)==0 end
-	c:RegisterFlagEffect(90807199,RESET_CHAIN,0,1)
+	if chk==0 then return not e:GetHandler():IsPublic() end
 end
 function c90807199.spfilter(c,e,tp)
 	return c:IsCode(90807199) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c90807199.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c90807199.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.IsPlayerCanDraw(tp) end
+		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+		and Duel.IsExistingMatchingCard(c90807199.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
 end
 function c90807199.spop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
 	if #g<1 then return end
-	local tc=g:RandomSelect(1-tp,1)
-	Duel.BreakEffect()
-	Duel.SendtoGrave(tc,REASON_EFFECT+REASON_DISCARD)
-	if not tc:GetFirst():IsCode(90807199) then
+	local tc=g:RandomSelect(1-tp,1):GetFirst()
+	if tc and Duel.SendtoGrave(tc,REASON_DISCARD+REASON_EFFECT)~=0 and not tc:IsCode(90807199)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		local spg=Duel.GetMatchingGroup(c90807199.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
+		if spg:GetCount()<=0 then return end
+		local sg=spg:GetFirst()
+		if spg:GetCount()~=1 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			sg=spg:Select(tp,1,1,nil)
+		end
 		Duel.BreakEffect()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sc=Duel.SelectMatchingCard(tp,c90807199.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-		if #sc>0 and Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP) then
+		if Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)~=0 then
 			Duel.Draw(tp,1,REASON_EFFECT)
 		end
 	end
-end
-function c90807199.descon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetPreviousLocation()==LOCATION_HAND and (r&REASON_DISCARD)~=0
 end
 function c90807199.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and chkc:IsFacedown() end
