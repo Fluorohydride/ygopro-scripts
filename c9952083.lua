610d997ea5eb0a7d2024b65fe803c2378c837260
Mercury233 <me@mercury233.me>
2019-07-07 10:43:04 +0800

    fix 二重召喚 and サモンチェーン (#1198)

--- c9952083.lua
+++ c9952083.lua
@@ -5,11 +5,22 @@ function c9952083.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(c9952083.condition)
+	e1:SetTarget(c9952083.target)
 	e1:SetOperation(c9952083.activate)
 	c:RegisterEffect(e1)
 end
 function c9952083.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentChain()>1 and Duel.CheckChainUniqueness() and Duel.GetTurnPlayer()==tp
+end
+function c9952083.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local ct=0
+		local ce={Duel.IsPlayerAffectedByEffect(tp,EFFECT_SET_SUMMON_COUNT_LIMIT)}
+		for _,te in ipairs(ce) do
+			ct=math.max(ct,te:GetValue())
+		end
+		return ct<3
+	end
 end
 function c9952083.activate(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
