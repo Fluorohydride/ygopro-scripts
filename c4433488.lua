660fb70d841e7cfcda6477a21d680832435406e4
Mercury233 <me@mercury233.me>
2019-07-14 11:09:29 +0800

    fix サイバネット・カスケード

--- c4433488.lua
+++ c4433488.lua
@@ -10,14 +10,14 @@ function c4433488.initial_effect(c)
 	e1:SetOperation(c4433488.activate)
 	c:RegisterEffect(e1)
 end
-function c4433488.cfilter(c)
-	return c:IsType(TYPE_LINK) and c:IsSummonType(SUMMON_TYPE_LINK)
+function c4433488.cfilter(c,tp)
+	return c:IsType(TYPE_LINK) and c:IsSummonType(SUMMON_TYPE_LINK) and c:GetSummonPlayer()==tp
 end
 function c4433488.spfilter(c,e,tp,g)
 	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and g and g:IsContains(c)
 end
 function c4433488.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local lc=eg:Filter(c4433488.cfilter,nil):GetFirst()
+	local lc=eg:Filter(c4433488.cfilter,nil,tp):GetFirst()
 	if chkc then return chkc:IsControler(tp) and c4433488.spfilter(chkc,e,tp,lc:GetMaterial()) end
 	if chk==0 then return lc and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingTarget(c4433488.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,lc:GetMaterial()) end
