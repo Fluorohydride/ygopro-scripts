b0b28ce729fffc04750a2eefcb44da46de267f9c
DailyShana <p2357111317192329@gmail.com>
2019-07-27 15:32:37 +0800

    fix マシュマロンのメガネ

--- c66865880.lua
+++ c66865880.lua
@@ -8,12 +8,20 @@ function c66865880.initial_effect(c)
 	--
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_ONLY_ATTACK_MONSTER)
+	e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(0,LOCATION_MZONE)
 	e2:SetCondition(c66865880.con)
 	e2:SetValue(c66865880.atlimit)
 	c:RegisterEffect(e2)
+	--
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetCondition(c66865880.con)
+	c:RegisterEffect(e3)
 end
 function c66865880.cfilter(c)
 	return c:IsFaceup() and c:IsCode(31305911)
@@ -22,5 +30,5 @@ function c66865880.con(e)
 	return Duel.IsExistingMatchingCard(c66865880.cfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
 function c66865880.atlimit(e,c)
-	return c:IsFaceup() and c:IsCode(31305911)
+	return c:IsFacedown() or not c:IsCode(31305911)
 end
