c35c6975a33425bae1c720e3491b37483242b50d
DailyShana <p2357111317192329@gmail.com>
2019-07-29 18:29:58 +0800

    fix

--- c10383554.lua
+++ c10383554.lua
@@ -2,7 +2,7 @@
 function c10383554.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcCodeFun(c,34688023,aux.FilterBoolFunction(Card.IsFusionSetCard,0xa9),1,true,false)
+	aux.AddFusionProcCodeFun(c,34688023,aux.FilterBoolFunction(Card.IsFusionSetCard,0xa9),1,true,true)
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
