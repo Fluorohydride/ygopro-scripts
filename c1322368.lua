8f37f34332f08c79c55faf2f32aae55bc07bd992
Wind2009-Louse <wind.jackey@qq.com>
2019-08-01 18:14:35 +0800

    Fix SPYRAL Double Helix
    Cannot special summon if controler's changed

--- c1322368.lua
+++ c1322368.lua
@@ -40,7 +40,11 @@ function c1322368.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=g:GetFirst()
 	local opt=e:GetLabel()
 	if (opt==0 and tc:IsType(TYPE_MONSTER)) or (opt==1 and tc:IsType(TYPE_SPELL)) or (opt==2 and tc:IsType(TYPE_TRAP)) then
-		local zone=e:GetHandler():GetLinkedZone()
+		if e:GetHandler():IsControler(tp) then
+			local zone=e:GetHandler():GetLinkedZone()
+		else
+			local zone=0
+		end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c1322368.spfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp,zone)
 		local sc=sg:GetFirst()
