9cd62a6383d2f42be35951cd2d5922b64f06a97b
mercury233 <me@mercury233.me>
2019-09-15 15:08:03 +0800

    new card VJMP

---
+++ c85692042.lua
@@ -0,0 +1,90 @@
+--塊斬機ダランベルシアン
+function c85692042.initial_effect(c)
+	--xyz summon
+	c:EnableReviveLimit()
+	aux.AddXyzProcedure(c,nil,4,2,nil,nil,99)
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(85692042,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,85692042)
+	e1:SetCondition(c85692042.thcon)
+	e1:SetCost(c85692042.thcost)
+	e1:SetTarget(c85692042.thtg)
+	e1:SetOperation(c85692042.thop)
+	e1:SetLabel(2)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetDescription(aux.Stringid(85692042,1))
+	e2:SetLabel(3)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetDescription(aux.Stringid(85692042,2))
+	e3:SetLabel(4)
+	c:RegisterEffect(e3)
+	--special summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(85692042,3))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,85692043)
+	e4:SetCost(c85692042.spcost)
+	e4:SetTarget(c85692042.sptg)
+	e4:SetOperation(c85692042.spop)
+	c:RegisterEffect(e4)
+end
+function c85692042.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+end
+function c85692042.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=e:GetLabel()
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,ct,REASON_COST) end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	e:GetHandler():RemoveOverlayCard(tp,ct,ct,REASON_COST)
+end
+function c85692042.thfilter(c,ct)
+	if not c:IsAbleToHand() then return false end
+	if ct==2 then return c:IsSetCard(0x132)
+	elseif ct==3 then return c:IsType(TYPE_MONSTER) and c:IsLevel(4)
+	else return c:IsType(TYPE_SPELL+TYPE_TRAP) end
+end
+function c85692042.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c85692042.thfilter,tp,LOCATION_DECK,0,1,nil,e:GetLabel()) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c85692042.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c85692042.thfilter,tp,LOCATION_DECK,0,1,1,nil,e:GetLabel())
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c85692042.costfilter(c,tp)
+	return Duel.GetMZoneCount(tp,c)>0
+end
+function c85692042.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c85692042.costfilter,1,nil,tp) end
+	local g=Duel.SelectReleaseGroup(tp,c85692042.costfilter,1,1,nil,tp)
+	Duel.Release(g,REASON_COST)
+end
+function c85692042.spfilter(c,e,tp)
+	return c:IsSetCard(0x132) and c:IsLevel(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c85692042.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c85692042.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+end
+function c85692042.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c85692042.spfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
