5be4f89ff2364f68a74aa36f536d26310807bad0
nekrozar <nekrozar.s@gmail.com>
2019-09-26 15:15:21 +0900

    fix different name
    http://yugioh-wiki.net/index.php?%A1%D4%A5%AF%A5%ED%A5%B9%A1%A6%A5%D6%A5%EA%A1%BC%A5%C9%A1%D5#faq
    
    Ｑ：《クロス・ブリード》を発動する際に《エッジインプ・シザー》と、《エッジインプ・ソウ》のカード名を得た《エッジインプ・トマホーク》を除外した場合、手札に加える事ができるのは《エッジインプ・ソウ》と《エッジインプ・トマホーク》のどちらでしょうか？
    Ａ：ご質問の場合、《エッジインプ・トマホーク》を手札に加えることができます。(18/09/23)
    
    http://yugioh-wiki.net/index.php?%A1%D4%C5%B4%A4%CE%B2%A6%20%A5%C9%A5%F4%A5%A7%A5%EB%A5%B0%A5%B9%A1%D5#faq2
    Ｑ：フィールドでのみカード名が変更されるモンスターをリリースした場合、カード名はフィールドと墓地のどちらに従いますか？
    Ａ：フィールドでのカード名に従います。(19/09/25)

--- c76382116.lua
+++ c76382116.lua
@@ -14,20 +14,24 @@ function c76382116.initial_effect(c)
 	e1:SetOperation(c76382116.spop)
 	c:RegisterEffect(e1)
 end
-function c76382116.spfilter(c,e,tp,g)
+function c76382116.spfilter(c,e,tp,...)
 	return (c:IsSetCard(0x134) or c:IsRace(RACE_MACHINE)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
-		and not g:IsExists(Card.IsCode,1,nil,c:GetCode())
+		and not c:IsCode(...)
 end
 function c76382116.costfilter1(c,e,tp)
 	return (c:IsSetCard(0x134) or c:IsRace(RACE_MACHINE)) and Duel.GetMZoneCount(tp,c)>0
-		and Duel.IsExistingMatchingCard(c76382116.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp,Group.FromCards(c))
+		and Duel.IsExistingMatchingCard(c76382116.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp,c:GetCode())
 end
 function c76382116.costfilter2(c,e,tp)
 	return (c:IsSetCard(0x134) or c:IsRace(RACE_MACHINE))
-		and Duel.IsExistingMatchingCard(c76382116.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp,Group.FromCards(c))
+		and Duel.IsExistingMatchingCard(c76382116.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp,c:GetCode())
 end
 function c76382116.fselect(g,e,tp)
-	local sg=Duel.GetMatchingGroup(c76382116.spfilter,tp,LOCATION_HAND,0,nil,e,tp,g)
+	local code={}
+	for tc in aux.Next(g) do
+		table.insert(code,tc:GetCode())
+	end
+	local sg=Duel.GetMatchingGroup(c76382116.spfilter,tp,LOCATION_HAND,0,nil,e,tp,table.unpack(code))
 	if sg:CheckSubGroup(aux.dncheck,g:GetCount(),g:GetCount()) and Duel.GetMZoneCount(tp,g)>=g:GetCount() then
 		Duel.SetSelectedCard(g)
 		return Duel.CheckReleaseGroup(tp,nil,0,nil)
@@ -36,25 +40,31 @@ end
 function c76382116.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckReleaseGroup(tp,c76382116.costfilter1,1,nil,e,tp) end
 	local rg=Duel.GetReleaseGroup(tp):Filter(c76382116.costfilter2,nil,e,tp)
+	local ft=Duel.GetMZoneCount(tp,rg)
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local sg=rg:SelectSubGroup(tp,c76382116.fselect,false,1,99,e,tp)
-	sg:KeepAlive()
-	e:SetLabelObject(sg)
+	local sg=rg:SelectSubGroup(tp,c76382116.fselect,false,1,ft,e,tp)
+	local code={}
+	for tc in aux.Next(sg) do
+		table.insert(code,tc:GetCode())
+	end
+	e:SetLabel(table.unpack(code))
 	Duel.Release(sg,REASON_COST)
 end
 function c76382116.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local g=e:GetLabelObject()
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,g:GetCount(),tp,LOCATION_HAND)
+	local code={e:GetLabel()}
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,#code,tp,LOCATION_HAND)
 end
 function c76382116.spop(e,tp,eg,ep,ev,re,r,rp)
-	local g=e:GetLabelObject()
+	local code={e:GetLabel()}
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft<g:GetCount() then return end
-	local sg=Duel.GetMatchingGroup(c76382116.spfilter,tp,LOCATION_HAND,0,nil,e,tp,g)
+	if ft>1 and Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+	if ft<#code then return end
+	local sg=Duel.GetMatchingGroup(c76382116.spfilter,tp,LOCATION_HAND,0,nil,e,tp,table.unpack(code))
 	if sg:GetCount()==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=sg:SelectSubGroup(tp,aux.dncheck,false,g:GetCount(),g:GetCount())
+	local tg=sg:SelectSubGroup(tp,aux.dncheck,false,#code,#code)
 	if tg and tg:GetCount()>0 then
 		Duel.SpecialSummon(tg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 	end
