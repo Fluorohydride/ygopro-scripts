a77deffc63230a9a145a4b5c03cd9af898d9f12f
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-26 06:38:36 +0800

    fix 始祖竜ワイアーム

--- c10817524.lua
+++ c10817524.lua
@@ -1,6 +1,6 @@
 --始祖竜ワイアーム
 function c10817524.initial_effect(c)
-	c:SetUniqueOnField(1,0,10817524)
+	c:SetUniqueOnField(1,0,10817524,LOCATION_MZONE)
 	--fusion material
 	c:EnableReviveLimit()
 	aux.AddFusionProcFunRep(c,aux.FilterBoolFunction(Card.IsFusionType,TYPE_NORMAL),2,false)
