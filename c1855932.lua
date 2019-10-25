42e3a61844f1d9f08447cae6a8d686640ae5c6e3
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-26 05:10:27 +0800

    fix 武神帝－カグツチ

--- c1855932.lua
+++ c1855932.lua
@@ -1,6 +1,6 @@
 --武神帝－カグツチ
 function c1855932.initial_effect(c)
-	c:SetUniqueOnField(1,0,1855932)
+	c:SetUniqueOnField(1,0,1855932,LOCATION_MZONE)
 	--xyz summon
 	aux.AddXyzProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_BEASTWARRIOR),4,2)
 	c:EnableReviveLimit()
