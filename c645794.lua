a958e9c199d05e59913c689fcba37c1994a28eea
Mercury233 <me@mercury233.me>
2019-11-08 11:44:51 +0800

    fix マジェスペクター・フロッグ

--- c645794.lua
+++ c645794.lua
@@ -48,7 +48,7 @@ function c645794.setop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CANNOT_TRIGGER)
-		e1:SetReset(RESET_EVENT+0x17a0000+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
 end
