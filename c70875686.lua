40c53722f4676deab5992627378c5db17e1a299f
nekrozar <nekrozar.s@gmail.com>
2019-11-19 14:32:36 +0900

    fix Prank-Kids Rip-Roarin-Roaster

--- c70875686.lua
+++ c70875686.lua
@@ -8,7 +8,7 @@ function c70875686.initial_effect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e0:SetValue(aux.lnklimit)
+	e0:SetValue(aux.linklimit)
 	c:RegisterEffect(e0)
 	--destroy
 	local e1=Effect.CreateEffect(c)
