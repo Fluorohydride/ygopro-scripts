71fe61b8cbea9377cfaabcf163ac3291cf8068a8
mercury233 <me@mercury233.me>
2019-11-28 16:48:08 +0800

    update

--- c15083304.lua
+++ c15083304.lua
@@ -17,8 +17,8 @@ function c15083304.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetTargetParam(3)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,3)
 end
-function c15083304.filter(c)
-	return not c:IsSetCard(0x11e)
+function c15083304.gselect(g)
+	return g:IsExists(Card.IsSetCard,1,nil,0x11e)
 end
 function c15083304.activate(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
@@ -26,17 +26,9 @@ function c15083304.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ShuffleHand(p)
 		Duel.BreakEffect()
 		local g=Duel.GetFieldGroup(p,LOCATION_HAND,0)
-		if g:IsExists(Card.IsSetCard,1,nil,0x11e) then
+		if #g>=2 and g:IsExists(Card.IsSetCard,1,nil,0x11e) then
 			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_DISCARD)
-			local g1=g:Select(p,1,1,nil)
-			if g1:GetFirst():IsSetCard(0x11e) then
-				g:RemoveCard(g1:GetFirst())
-			else
-				g:Remove(c15083304.filter,nil)
-			end
-			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_DISCARD)
-			local g2=g:Select(p,1,1,nil)
-			g1:Merge(g2)
+			local g1=g:SelectSubGroup(tp,c15083304.gselect,false,2,2)
 			Duel.SendtoGrave(g1,REASON_DISCARD+REASON_EFFECT)
 		else
 			Duel.ConfirmCards(1-p,g)
