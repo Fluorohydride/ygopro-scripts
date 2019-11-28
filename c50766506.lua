fc880178a948cff14e041c911dd895006b4158ac
mercury233 <me@mercury233.me>
2019-11-28 14:22:33 +0800

    update

--- c50766506.lua
+++ c50766506.lua
@@ -34,34 +34,29 @@ function c50766506.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(rg,REASON_COST)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c50766506.spfilter2(c,e,tp)
-	return c:IsSetCard(0x2b) and
-		(c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK) or c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE))
+function c50766506.gselect(g,slv)
+	return g:GetSum(Card.GetLevel)<=slv
 end
 function c50766506.operation(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft<=0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
 	local c=e:GetHandler()
 	local slv=e:GetLabel()
-	local sg=Duel.GetMatchingGroup(c50766506.spfilter2,tp,LOCATION_DECK,0,nil,e,tp)
-	sg:Remove(Card.IsLevelAbove,nil,slv+1)
+	local sg=Duel.GetMatchingGroup(c50766506.spfilter,tp,LOCATION_DECK,0,nil,slv,e,tp)
 	if sg:GetCount()==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tg=sg:SelectSubGroup(tp,c50766506.gselect,false,1,ft,slv)
 	local cg=Group.CreateGroup()
-	repeat
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tc=sg:Select(tp,1,1,nil):GetFirst()
-		sg:RemoveCard(tc)
-		slv=slv-tc:GetLevel()
+	for tc in aux.Next(tg) do
 		local spos=0
 		if tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK) then spos=spos+POS_FACEUP_ATTACK end
 		if tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE) then spos=spos+POS_FACEDOWN_DEFENSE end
 		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,spos)
 		if tc:IsFacedown() then cg:AddCard(tc) end
 		c:SetCardTarget(tc)
-		sg:Remove(Card.IsLevelAbove,nil,slv+1)
-		ft=ft-1
-	until ft<=0 or sg:GetCount()==0 or not Duel.SelectYesNo(tp,aux.Stringid(50766506,0))
+	end
 	Duel.SpecialSummonComplete()
 	Duel.ConfirmCards(1-tp,cg)
 end
