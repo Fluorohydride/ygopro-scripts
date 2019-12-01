13b283cbd690e7119322be779fc557e7227f08a5
mercury233 <me@mercury233.me>
2019-12-01 11:13:25 +0800

    add card YMAB

---
+++ c37984331.lua
@@ -0,0 +1,28 @@
+--真エクゾディア
+function c37984331.initial_effect(c)
+	--win
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EVENT_ADJUST)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(c37984331.condition)
+	e1:SetOperation(c37984331.operation)
+	c:RegisterEffect(e1)
+end
+function c37984331.winfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x40) and c:IsType(TYPE_NORMAL)
+end
+function c37984331.cfilter(c)
+	return not c37984331.winfilter(c)
+end
+function c37984331.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(c37984331.winfilter,tp,LOCATION_MZONE,LOCATION_MZONE,c)
+	local ct=g:GetClassCount(Card.GetCode)
+	return ct==4 and not Duel.IsExistingMatchingCard(c37984331.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,c)
+end
+function c37984331.operation(e,tp,eg,ep,ev,re,r,rp)
+	local WIN_REASON_EXODIA = 0x10
+	Duel.Win(1-tp,WIN_REASON_EXODIA)
+end
