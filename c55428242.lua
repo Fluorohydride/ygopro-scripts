08e0a5b01b000476b4d2f2c153e00181bdb6989c
nekrozar <nekrozar.s@gmail.com>
2019-12-08 12:10:56 +0900

    fix Traptrix Atrax

--- c55428242.lua
+++ c55428242.lua
@@ -29,14 +29,6 @@ function c55428242.initial_effect(c)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetValue(c55428242.chainfilter)
 	c:RegisterEffect(e4)
-	--cannot disable
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_FIELD)
-	e5:SetCode(EFFECT_CANNOT_DISABLE)
-	e5:SetRange(LOCATION_MZONE)
-	e5:SetTargetRange(LOCATION_ONFIELD,0)
-	e5:SetTarget(c55428242.distarget)
-	c:RegisterEffect(e5)
 end
 function c55428242.efilter(e,te)
 	local c=te:GetHandler()
@@ -45,11 +37,8 @@ end
 function c55428242.etarget(e,c)
 	return c:GetType()==TYPE_TRAP and c:IsSetCard(0x4c,0x89)
 end
-function c55428242.distarget(e,c)
-	return c:GetType()==TYPE_TRAP or c:GetType()==TYPE_TRAP+TYPE_EQUIP
-end
 function c55428242.chainfilter(e,ct)
 	local p=e:GetHandlerPlayer()
 	local te,tp=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER)
-	return p==tp and te:GetActiveType()==TYPE_TRAP
+	return p==tp and te:IsHasType(EFFECT_TYPE_ACTIVATE) and te:GetActiveType()==TYPE_TRAP
 end
