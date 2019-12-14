9d50bc9f19c9859eddfe7d40be1b7137c460d702
mercury233 <me@mercury233.me>
2019-12-14 14:34:31 +0800

    minor

--- c8512558.lua
+++ c8512558.lua
@@ -24,12 +24,12 @@ c8512558.combination3[2]=aux.CreateChecks(Card.IsSetCard,{0x54,0x82,0x8f})
 c8512558.combination3[3]=aux.CreateChecks(Card.IsSetCard,{0x54,0x59,0x8f})
 c8512558.combination3[4]=aux.CreateChecks(Card.IsSetCard,{0x54,0x59,0x82})
 c8512558.combination4=aux.CreateChecks(Card.IsSetCard,{0x54,0x59,0x82,0x8f})
-function c8512558.spfilter(c,e,tp,...)
-	return c:IsSetCard(...) and not c:IsCode(8512558) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function c8512558.spfilter(c,e,tp)
+	return c:IsSetCard(0x54,0x59,0x82,0x8f) and not c:IsCode(8512558) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c8512558.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c8512558.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp,0x54,0x59,0x82,0x8f)
+		and Duel.IsExistingMatchingCard(c8512558.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
@@ -53,7 +53,7 @@ function c8512558.gcheck(g)
 	end
 end
 function c8512558.spop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c8512558.spfilter,tp,LOCATION_HAND,0,nil,e,tp,0x54,0x59,0x82,0x8f)
+	local g=Duel.GetMatchingGroup(c8512558.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft>0 and #g>0 then
 		if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
