016980d3163a1e836eebef7b0e4a4072b5ee882e
Clara <lyris89@live.com>
2019-12-17 15:14:37 -0500

    Fix "Power Wall"
    Interaction between "Power Wall" & "Blue-Eyes Chaos MAX Dragon"

--- c76403456.lua
+++ c76403456.lua
@@ -20,10 +20,6 @@ function c76403456.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,g)
 end
 function c76403456.activate(e,tp,eg,ep,ev,re,r,rp)
-	local val=math.ceil(Duel.GetBattleDamage(tp)/500)
-	Duel.DiscardDeck(tp,val,REASON_EFFECT)
-	local og=Duel.GetOperatedGroup()
-	if og:FilterCount(Card.IsLocation,nil,LOCATION_GRAVE)<val then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
@@ -32,5 +28,9 @@ function c76403456.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterEffect(e1,tp)
 end
 function c76403456.damop(e,tp,eg,ep,ev,re,r,rp)
+	local val=math.ceil(Duel.GetBattleDamage(tp)/500)
+	Duel.DiscardDeck(tp,val,REASON_EFFECT)
+	local og=Duel.GetOperatedGroup()
+	if og:FilterCount(Card.IsLocation,nil,LOCATION_GRAVE)<val then return end
 	Duel.ChangeBattleDamage(tp,0)
 end
