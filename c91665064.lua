14a89637155ea672fd7f6c59cea758029cd8c0e3
DailyShana <p2357111317192329@gmail.com>
2020-01-03 12:33:53 +0800

    battle damage become 0

--- c91665064.lua
+++ c91665064.lua
@@ -21,10 +21,9 @@ end
 function c91665064.activate(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,1)
-	e1:SetValue(c91665064.damval)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	local tc=Duel.GetFirstTarget()
@@ -35,8 +34,4 @@ function c91665064.activate(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	e2:SetValue(1)
 	tc:RegisterEffect(e2)
-end
-function c91665064.damval(e,re,val,r,rp,rc)
-	if bit.band(r,REASON_BATTLE)~=0 then return 0 end
-	return val
 end
