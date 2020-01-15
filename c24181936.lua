f073fd14da4096d5c44515b7413a4bf8170dbd92
DailyShana <p2357111317192329@gmail.com>
2020-01-15 11:35:49 +0800

    fix 闇味鍋パーティー

--- c24181936.lua
+++ c24181936.lua
@@ -32,28 +32,40 @@ function c24181936.atkop1(e,tp,eg,ep,ev,re,r,rp)
 	if tc:IsRelateToEffect(e) then
 		if tc:GetFlagEffect(24181936)==0 then
 			tc:RegisterFlagEffect(24181936,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
-			local e3=Effect.CreateEffect(e:GetHandler())
-			e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-			e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-			e3:SetCondition(c24181936.atkcon2)
-			e3:SetOperation(c24181936.atkop2)
-			e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			tc:RegisterEffect(e3)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetCode(EFFECT_PATRICIAN_OF_DARKNESS)
+			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+			e1:SetTargetRange(1,0)
+			e1:SetCondition(c24181936.effcon)
+			e1:SetLabelObject(tc)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			Duel.RegisterEffect(e1,tp)
+			local e2=Effect.CreateEffect(e:GetHandler())
+			e2:SetType(EFFECT_TYPE_FIELD)
+			e2:SetCode(EFFECT_UPDATE_ATTACK)
+			e2:SetTargetRange(LOCATION_MZONE,0)
+			e2:SetCondition(c24181936.atkcon2)
+			e2:SetTarget(c24181936.atktg)
+			e2:SetValue(c24181936.atkval)
+			e2:SetLabelObject(tc)
+			e2:SetReset(RESET_PHASE+PHASE_END)
+			Duel.RegisterEffect(e2,tp)
 		end
 	end
 end
-function c24181936.atkcon2(e,tp,eg,ep,ev,re,r,rp)
-	local ag=eg:GetFirst():GetAttackableTarget()
-	local at=e:GetHandler():GetBattleTarget()
-	return at~=nil and ag:IsExists(aux.TRUE,1,at)
+function c24181936.effcon(e)
+	local tc=e:GetLabelObject()
+	return tc:GetFlagEffect(24181936)~=0 and Duel.GetAttacker()==tc
 end
-function c24181936.atkop2(e,tp,eg,ep,ev,re,r,rp)
-	local ag=eg:GetFirst():GetAttackableTarget()
-	local at=e:GetHandler():GetBattleTarget()
-	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_ATTACKTARGET)
-	local g=ag:Select(1-tp,1,1,at)
-	local tc=g:GetFirst()
-	if tc then
-		Duel.ChangeAttackTarget(tc)
-	end
+function c24181936.atkcon2(e)
+	local tc=e:GetLabelObject()
+	return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL
+		and tc:GetFlagEffect(24181936)~=0 and Duel.GetAttacker()==tc
+end
+function c24181936.atktg(e,c)
+	return c==Duel.GetAttacker()
+end
+function c24181936.atkval(e,c)
+	return c:GetBaseAttack()
 end
