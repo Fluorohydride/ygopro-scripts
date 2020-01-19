2c8b9575cbad33663664520da5cfb3236cbe9bd8
DailyShana <p2357111317192329@gmail.com>
2020-01-19 10:10:45 +0800

    fix ガーディアン・デスサイス
    https://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=5&fid=6112&keyword=&tag=-1&request_locale=ja

--- c18175965.lua
+++ c18175965.lua
@@ -99,7 +99,7 @@ function c18175965.spcon2(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
 function c18175965.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsStatus(STATUS_PROC_COMPLETE) end
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,0,0,tp,1)
 end
@@ -107,6 +107,6 @@ function c18175965.spop2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT)==0 then return end
 	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,true,true,POS_FACEUP)
+		Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)
 	end
 end
