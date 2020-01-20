07e2260d8862d87790f22339d19f349c7663573f
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-01-20 16:37:57 +0800

    fix 幻影騎士団トゥーム・シールド

--- c51606429.lua
+++ c51606429.lua
@@ -48,7 +48,7 @@ function c51606429.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return aux.exccon(e) and Duel.GetTurnPlayer()==tp
 end
 function c51606429.negfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_TRAP) and not c:IsDisabled()
+	return aux.disfilter1(c,true) and c:IsType(TYPE_TRAP)
 end
 function c51606429.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and c51606429.negfilter(chkc) end
@@ -59,7 +59,7 @@ end
 function c51606429.negop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsDisabled() then
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and (not tc:IsDisabled() or tc:IsType(TYPE_TRAPMONSTER)) then
 		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
