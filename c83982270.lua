0a4744ff69e3d6d5842232c9f4fea17d85a60217
Lyris <Lyris89@live.com>
2020-01-26 14:20:51 -0500

    Fix "Cloudian - Poison Cloud"
    `if it was face-up at the start of the Damage Step` is absent from the OCG version when compared to the TCG version, but the script reflects the latter version.

--- c83982270.lua
+++ c83982270.lua
@@ -13,7 +13,7 @@ function c83982270.initial_effect(c)
 end
 function c83982270.condition(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsLocation(LOCATION_GRAVE) and c:IsReason(REASON_BATTLE) and c:IsPreviousPosition(POS_FACEUP)
+	return c:IsLocation(LOCATION_GRAVE) and c:IsReason(REASON_BATTLE)
 end
 function c83982270.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
