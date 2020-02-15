320b820b173a0f012c94d92d31766e28c12c13fe
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-02-15 10:27:18 +0800

    fix 海晶乙女グレート・バブル・リーフ (#1336)

--- c47910940.lua
+++ c47910940.lua
@@ -65,12 +65,14 @@ end
 function c47910940.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local ct=eg:FilterCount(c47910940.atkfilter,nil)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(ct*600)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
-	c:RegisterEffect(e1)
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(ct*600)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
 end
 function c47910940.costfilter(c)
 	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsAbleToGraveAsCost()
