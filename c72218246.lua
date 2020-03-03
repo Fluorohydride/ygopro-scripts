27497f9ef839512fbc06e54c9745b33fae367f85
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-03-03 23:56:37 +0800

    fix クロスローズ・ドラゴン

--- c72218246.lua
+++ c72218246.lua
@@ -77,7 +77,7 @@ function c72218246.spfilter2(c,e,tp)
 end
 function c72218246.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c72218246.spfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c72218246.spfilter2,tp,LOCATION_GRAVE,0,1,e:GetHandler(),e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function c72218246.spop2(e,tp,eg,ep,ev,re,r,rp)
