2dfe99ffe9aa70aaffb5898ca1881c8c447b9c5e
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-03-06 15:11:20 +0800

    fix 繁華の花笑み (#1353)

--- c32887445.lua
+++ c32887445.lua
@@ -2,7 +2,7 @@
 function c32887445.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_DECKDES)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_DECKDES+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,32887445+EFFECT_COUNT_CODE_OATH)
