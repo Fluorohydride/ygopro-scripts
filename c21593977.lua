f255e4c9247c11812009c9d420245f38d0cf37f9
purerosefallen <78877@qq.com>
2020-03-15 20:40:05 +0800

    changes for eratta on 2020.4.1

--- c21593977.lua
+++ c21593977.lua
@@ -2,10 +2,17 @@
 function c21593977.initial_effect(c)
 	--activate trap in hand
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCountLimit(1,21593977)
+	e1:SetCondition(c21593977.condition)
 	e1:SetOperation(c21593977.operation)
 	c:RegisterEffect(e1)
+end
+function c21593977.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE)
 end
 function c21593977.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -13,6 +20,7 @@ function c21593977.operation(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_TRAP_ACT_IN_HAND)
 	e1:SetTargetRange(LOCATION_HAND,0)
+	e1:SetCountLimit(1)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end
