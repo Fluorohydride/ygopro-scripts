e48204a5aecf4f7883d4b17cd71b11c994e0224d
Mercury233 <me@mercury233.me>
2020-03-19 10:21:40 +0800

    fix 円卓の聖騎士

--- c55742055.lua
+++ c55742055.lua
@@ -59,7 +59,7 @@ function c55742055.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 function c55742055.confilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x107a)
+	return (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE)) and c:IsSetCard(0x107a)
 end
 function c55742055.effcon(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetTurnPlayer()~=tp then return false end
