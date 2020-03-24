4e9f317dd0b173573e9230cec86231e5f274c4f3
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-03-24 13:44:36 +0800

    fix バブル・クラッシュ

--- c61622107.lua
+++ c61622107.lua
@@ -4,7 +4,6 @@ function c61622107.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 	e1:SetCondition(c61622107.condition)
 	e1:SetOperation(c61622107.activate)
 	c:RegisterEffect(e1)
@@ -14,17 +13,17 @@ function c61622107.condition(e,tp,eg,ep,ev,re,r,rp)
 end
 function c61622107.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ct=Duel.GetFieldGroupCount(tp,0xe,0)
-	local g=Group.CreateGroup()
+	local exc=nil
+	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then exc=e:GetHandler() end
 	if ct>=6 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		local sg=Duel.SelectMatchingCard(tp,nil,tp,0xe,0,ct-5,ct-5,nil)
-		g:Merge(sg)
+		local sg=Duel.SelectMatchingCard(tp,nil,tp,0xe,0,ct-5,ct-5,exc)
+		Duel.SendtoGrave(sg,REASON_RULE)
 	end
 	ct=Duel.GetFieldGroupCount(1-tp,0xe,0)
 	if ct>=6 then
 		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_TOGRAVE)
 		local sg=Duel.SelectMatchingCard(1-tp,nil,1-tp,0xe,0,ct-5,ct-5,nil)
-		g:Merge(sg)
+		Duel.SendtoGrave(sg,REASON_RULE)
 	end
-	Duel.SendtoGrave(g,REASON_EFFECT)
 end
