4fd1930392d3358a008963b3faa7f7868d60c4f2
Mercury233 <me@mercury233.me>
2020-04-03 08:09:54 +0800

    errata レアメタル・ドラゴン

--- c25236056.lua
+++ c25236056.lua
@@ -1,3 +1,13 @@
 --レアメタル・ドラゴン
 function c25236056.initial_effect(c)
+	--splimit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(c25236056.splimit)
+	c:RegisterEffect(e1)
+end
+function c25236056.splimit(e,se,sp,st)
+	return se:IsHasType(EFFECT_TYPE_ACTIONS)
 end
