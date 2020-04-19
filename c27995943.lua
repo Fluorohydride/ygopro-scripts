a9131aaccdfb55a6806b6c770d3dfd0cf490084b
Mercury233 <me@mercury233.me>
2020-04-19 08:21:48 +0800

    fix Dr.フランゲ

--- c27995943.lua
+++ c27995943.lua
@@ -37,7 +37,11 @@ function c27995943.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 		e1:SetTargetRange(1,0)
 		e1:SetCode(EFFECT_SKIP_DP)
-		e1:SetReset(RESET_PHASE+PHASE_DRAW+RESET_SELF_TURN)
+		if Duel.GetTurnPlayer()==tp and Duel.GetCurrentPhase()==PHASE_DRAW then
+			e1:SetReset(RESET_PHASE+PHASE_DRAW+RESET_SELF_TURN,2)
+		else
+			e1:SetReset(RESET_PHASE+PHASE_DRAW+RESET_SELF_TURN)
+		end
 		Duel.RegisterEffect(e1,tp)
 	end
 end
