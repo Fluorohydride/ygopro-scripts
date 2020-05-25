99a8a599787fb06082631a11f6d71073ff3d617f
mercury233 <me@mercury233.me>
2020-05-25 14:53:51 +0800

    add script VJMP

---
+++ c90788081.lua
@@ -0,0 +1,76 @@
+--百年竜
+function c90788081.initial_effect(c)
+	--lv/atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(90788081,0))
+	e1:SetCategory(CATEGORY_LVCHANGE+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c90788081.lvtg)
+	e1:SetOperation(c90788081.lvop)
+	c:RegisterEffect(e1)
+	--indes
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(90788081,1))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,90788081)
+	e2:SetTarget(c90788081.indtg)
+	e2:SetOperation(c90788081.indop)
+	c:RegisterEffect(e2)
+end
+function c90788081.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsLevelAbove(1) and Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)>0 end
+end
+function c90788081.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local lv=Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)
+	if c:IsFaceup() and c:IsRelateToEffect(e) and c:IsLevelAbove(1) and lv>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(lv)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_UPDATE_ATTACK)
+		e2:SetValue(lv*100)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e2)
+	end
+end
+function c90788081.indfilter(c)
+	return c:IsFaceup() and c:GetSummonLocation()==LOCATION_EXTRA
+end
+function c90788081.indtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c90788081.indfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c90788081.indfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c90788081.indfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c90788081.indop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(aux.Stringid(90788081,2))
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT+EFFECT_FLAG_NO_TURN_RESET)
+		e1:SetRange(LOCATION_ONFIELD)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+		e1:SetCountLimit(1)
+		e1:SetValue(c90788081.indct)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
+function c90788081.indct(e,re,r,rp)
+	if bit.band(r,REASON_BATTLE+REASON_EFFECT)~=0 then
+		return 1
+	else
+		return 0
+	end
+end
