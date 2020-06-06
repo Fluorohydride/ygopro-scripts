200247a2d889bf3cb4596f12d34bb4e7f051a65a
Lyris <Lyris12@users.noreply.github.com>
2020-06-06 08:55:24 -0400

    Fix "Silverrokket Dragon" strings
    Was missing a call to str1

--- c32476603.lua
+++ c32476603.lua
@@ -2,6 +2,7 @@
 function c32476603.initial_effect(c)
 	--destroy
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(32476603,0))
 	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
