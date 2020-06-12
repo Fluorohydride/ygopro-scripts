4730b1fa4eda5658dd12ffd70a649e7026f304a3
Mercury233 <me@mercury233.me>
2020-06-12 14:51:46 +0800

    fix PSYフレーム・サーキット

--- c575512.lua
+++ c575512.lua
@@ -37,9 +37,11 @@ function c575512.sccon(e,tp,eg,ep,ev,re,r,rp)
 end
 function c575512.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
+		if e:GetHandler():GetFlagEffect(575512)>0 then return false end
 		local mg=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0xc1)
 		return Duel.IsExistingMatchingCard(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,nil,nil,mg)
 	end
+	e:GetHandler():RegisterFlagEffect(575512,RESET_CHAIN,0,1)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function c575512.scop(e,tp,eg,ep,ev,re,r,rp)
