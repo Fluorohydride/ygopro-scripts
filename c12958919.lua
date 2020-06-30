385ad82be84dfb90dd0f27bf4d9ba2bcad20cc91
Mercury233 <me@mercury233.me>
2020-06-30 17:17:05 +0800

    fix 幻銃士

--- c12958919.lua
+++ c12958919.lua
@@ -52,6 +52,7 @@ function c12958919.spop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
 	if ft>ct then ft=ct end
 	if ft<=0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
 	if not Duel.IsPlayerCanSpecialSummonMonster(tp,12958920,0x49,0x4011,500,500,4,RACE_FIEND,ATTRIBUTE_DARK) then return end
 	local ctn=true
 	while ft>0 and ctn do
