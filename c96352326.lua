d7218f548c1a8afbdac67ee6f72cc5526e17820b
mercury233 <me@mercury233.me>
2020-07-01 23:53:56 +0800

    add card VJMP

---
+++ c96352326.lua
@@ -0,0 +1,70 @@
+--太陽の魔術師エダ
+function c96352326.initial_effect(c)
+	--SpecialSummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(96352326,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,96352326)
+	e1:SetTarget(c96352326.sptg)
+	e1:SetOperation(c96352326.spop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP)
+	c:RegisterEffect(e2)
+	--change pos
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(96352326,1))
+	e3:SetCategory(CATEGORY_POSITION)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,96352327)
+	e3:SetCondition(c96352326.poscon)
+	e3:SetTarget(c96352326.postg)
+	e3:SetOperation(c96352326.posop)
+	c:RegisterEffect(e3)
+end
+function c96352326.spfilter(c,e,tp)
+	return c:IsRace(RACE_SPELLCASTER) and c:IsDefense(1500) and not c:IsCode(96352326)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
+end
+function c96352326.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c96352326.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function c96352326.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c96352326.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
+function c96352326.poscon(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	return Duel.GetTurnPlayer()~=tp and (ph==PHASE_MAIN1 or ph==PHASE_MAIN2)
+end
+function c96352326.filter(c)
+	return c:IsRace(RACE_SPELLCASTER) and c:IsFacedown()
+end
+function c96352326.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c96352326.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
+end
+function c96352326.posop(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectMatchingCard(tp,c96352326.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		local pos=Duel.SelectPosition(tp,tc,POS_FACEUP_ATTACK+POS_FACEUP_DEFENSE)
+		Duel.ChangePosition(tc,pos)
+	end
+end
