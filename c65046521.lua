fbf4717abe8967993d26c6d951cec5590e1acd1b
mercury233 <me@mercury233.me>
2020-07-07 15:44:37 +0800

    add scripts SD39

---
+++ c65046521.lua
@@ -0,0 +1,77 @@
+--憑依連携
+function c65046521.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(65046521,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetCountLimit(1,65046521)
+	e1:SetTarget(c65046521.target)
+	e1:SetOperation(c65046521.activate)
+	c:RegisterEffect(e1)
+	--to field
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(65046521,2))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,65046521)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(c65046521.tftg)
+	e2:SetOperation(c65046521.tfop)
+	c:RegisterEffect(e2)
+end
+function c65046521.filter(c,e,tp)
+	return c:IsRace(RACE_SPELLCASTER) and c:IsDefense(1500)
+		and (c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK) or c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE))
+end
+function c65046521.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c65046521.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+end
+function c65046521.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c65046521.filter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc then
+		local spos=0
+		if tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK) then spos=spos+POS_FACEUP_ATTACK end
+		if tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE) then spos=spos+POS_FACEDOWN_DEFENSE end
+		if spos~=0 then Duel.SpecialSummon(tc,0,tp,tp,false,false,spos) end
+		if tc:IsFacedown() then Duel.ConfirmCards(1-tp,tc) end
+	end
+	local mg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if mg:GetClassCount(Card.GetAttribute)>=2 and g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(65046521,1)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if g:GetCount()>0 then
+			Duel.HintSelection(g)
+			Duel.Destroy(g,REASON_EFFECT)
+		end
+	end
+end
+function c65046521.tffilter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsType(TYPE_CONTINUOUS) and c:IsSetCard(0xc0) and not c:IsForbidden()
+end
+function c65046521.tftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c65046521.tffilter(chkc) and c:IsControler(tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(c65046521.tffilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectTarget(tp,c65046521.tffilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
+end
+function c65046521.tfop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+	end
+end
---
+++ c91530236.lua
@@ -0,0 +1,57 @@
+--精霊術の使い手
+function c91530236.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,91530236+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(c91530236.cost)
+	e1:SetTarget(c91530236.target)
+	e1:SetOperation(c91530236.activate)
+	c:RegisterEffect(e1)
+end
+function c91530236.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function c91530236.filter(c)
+	return c:IsSetCard(0xbf) and c:IsType(TYPE_MONSTER)
+		or c:IsSetCard(0x10c0) and c:IsType(TYPE_MONSTER)
+		or c:IsSetCard(0xc0) and c:IsType(TYPE_SPELL+TYPE_TRAP)
+end
+function c91530236.thfilter(c,e,tp,mft,sft)
+	return c91530236.filter(c) and c:IsAbleToHand()
+		and Duel.IsExistingMatchingCard(c91530236.setfilter,tp,LOCATION_DECK,0,1,c,e,tp,mft,sft,c:GetCode())
+end
+function c91530236.setfilter(c,e,tp,mft,sft,code)
+	return c91530236.filter(c) and not c:IsCode(code)
+		and (sft>0 and c:IsSSetable(true) or mft>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE))
+end
+function c91530236.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local mft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		local sft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+		if e:IsHasType(EFFECT_TYPE_ACTIVATE) and not e:GetHandler():IsLocation(LOCATION_SZONE) then sft=sft-1 end
+		return Duel.IsExistingMatchingCard(c91530236.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,mft,sft)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,0,tp,LOCATION_DECK)
+end
+function c91530236.activate(e,tp,eg,ep,ev,re,r,rp)
+	local mft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local sft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local tc1=Duel.SelectMatchingCard(tp,c91530236.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,mft,sft):GetFirst()
+	if tc1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+		local tc2=Duel.SelectMatchingCard(tp,c91530236.setfilter,tp,LOCATION_DECK,0,1,1,tc1,e,tp,mft,sft,tc1:GetCode()):GetFirst()
+		Duel.SendtoHand(tc1,nil,REASON_EFFECT)
+		if tc2:IsType(TYPE_MONSTER) then
+			Duel.SpecialSummon(tc2,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
+		else
+			Duel.SSet(tp,tc2,tp,false)
+		end
+		Duel.ConfirmCards(1-tp,Group.FromCards(tc1,tc2))
+	end
+end
