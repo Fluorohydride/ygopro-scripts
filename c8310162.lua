e6ddd94493ba2b41549d57ed2398fba0f2c4727e
salix5 <salix5@livemail.tw>
2020-07-23 09:55:16 +0800

    temp fix Sin パラドクス・ドラゴン (#1438)
    https://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=5&fid=10978&keyword=&tag=-1

--- c8310162.lua
+++ c8310162.lua
@@ -28,7 +28,7 @@ function c8310162.descon(e)
 	return not Duel.IsEnvironment(27564031)
 end
 function c8310162.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) and e:GetHandler():IsLocation(LOCATION_MZONE)
 end
 function c8310162.spfilter(c,e,tp)
 	return c:IsType(TYPE_SYNCHRO) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
