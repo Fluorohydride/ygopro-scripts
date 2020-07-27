4bc914ef35a0b99a20c9923e55e3a539da4018ce
Mercury233 <me@mercury233.me>
2020-07-27 10:48:28 +0800

    fix

--- c30537973.lua
+++ c30537973.lua
@@ -49,9 +49,6 @@ function c30537973.initial_effect(c)
 		Duel.RegisterEffect(ge2,0)
 	end
 end
-function c30537973.desfilter(c)
-	return c:IsFaceup() and c:GetSequence()~=5
-end
 function c30537973.reg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	e:GetHandler():RegisterFlagEffect(30537973,RESET_PHASE+PHASE_END,EFFECT_FLAG_OATH,1)
@@ -59,9 +56,12 @@ end
 function c30537973.descon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetFlagEffect(30537973)~=0
 end
+function c30537973.desfilter(c)
+	return c:IsFaceup() and c:GetSequence()<5
+end
 function c30537973.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
-	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and chkc:IsFaceup() and chkc~=c and c29590752.desfilter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and chkc~=c and c30537973.desfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c30537973.desfilter,tp,LOCATION_SZONE,LOCATION_SZONE,1,c) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,c30537973.desfilter,tp,LOCATION_SZONE,LOCATION_SZONE,1,1,c)
