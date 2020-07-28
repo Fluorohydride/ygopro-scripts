934574a0087842f90a58087bcbccf81fca1033d9
Mercury233 <me@mercury233.me>
2020-07-29 07:56:37 +0800

    fix ドラゴンを呼ぶ笛

--- c43973174.lua
+++ c43973174.lua
@@ -5,6 +5,7 @@ function c43973174.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(c43973174.condition)
 	e1:SetTarget(c43973174.target)
 	e1:SetOperation(c43973174.activate)
 	c:RegisterEffect(e1)
@@ -12,12 +13,14 @@ end
 function c43973174.cfilter(c)
 	return c:IsFaceup() and c:IsCode(17985575)
 end
+function c43973174.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c43973174.cfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+end
 function c43973174.filter(c,e,tp)
 	return c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c43973174.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c43973174.cfilter,tp,LOCATION_MZONE,0,1,nil)
 		and Duel.IsExistingMatchingCard(c43973174.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
@@ -26,7 +29,7 @@ function c43973174.activate(e,tp,eg,ep,ev,re,r,rp)
 	if ft<=0 then return end
 	if ft>2 then ft=2 end
 	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
-	if not Duel.IsExistingMatchingCard(c43973174.cfilter,tp,LOCATION_MZONE,0,1,nil) then return end
+	if not Duel.IsExistingMatchingCard(c43973174.cfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c43973174.filter,tp,LOCATION_HAND,0,1,ft,nil,e,tp)
 	if g:GetCount()>0 then
