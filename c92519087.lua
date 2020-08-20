51b30f77e992e92f5d9152de4bfb031bf25262bf
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-08-20 19:39:39 +0800

    fix 電脳堺 (#1480)
    * fix 電脳堺狐－仙々
    
    * fix 電脳堺豸－豸々
    
    * fix 電脳堺媛－瑞々
    
    * fix 電脳堺麟－麟々
    
    * fix 電脳堺悟－老々

--- c92519087.lua
+++ c92519087.lua
@@ -6,7 +6,7 @@ function c92519087.initial_effect(c)
 	--remove
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_TO_GRAVE_REDIRECT)
 	e1:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
