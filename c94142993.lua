f5eb22e5439433bc0bc975d9028a07cde7d9de9c
Mercury233 <me@mercury233.me>
2020-08-22 07:58:18 +0800

    update

--- c94142993.lua
+++ c94142993.lua
@@ -1,5 +1,12 @@
 --ティンダングル・イントルーダー
 function c94142993.initial_effect(c)
+	--same effect send this card to grave and spsummon another card check
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e0:SetCode(EVENT_TO_GRAVE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetOperation(c94142993.checkop)
+	c:RegisterEffect(e0)
 	--flip
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(94142993,0))
@@ -28,10 +35,40 @@ function c94142993.initial_effect(c)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e3:SetRange(LOCATION_GRAVE)
 	e3:SetCountLimit(1,94142995)
+	e3:SetLabelObject(e0)
 	e3:SetCondition(c94142993.spcon)
 	e3:SetTarget(c94142993.sptg)
 	e3:SetOperation(c94142993.spop)
 	c:RegisterEffect(e3)
+end
+function c94142993.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if (r&REASON_EFFECT)>0 then
+		e:SetLabelObject(re)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_CHAIN_END)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetOperation(c94142993.resetop)
+		e1:SetLabelObject(e)
+		Duel.RegisterEffect(e1,tp)
+		local e2=e1:Clone()
+		e2:SetCode(EVENT_BREAK_EFFECT)
+		e2:SetOperation(c94142993.resetop2)
+		e2:SetReset(RESET_CHAIN)
+		e2:SetLabelObject(e1)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function c94142993.resetop(e,tp,eg,ep,ev,re,r,rp)
+	--this will run after EVENT_SPSUMMON_SUCCESS
+	e:GetLabelObject():SetLabelObject(nil)
+	e:Reset()
+end
+function c94142993.resetop2(e,tp,eg,ep,ev,re,r,rp)
+	local e1=e:GetLabelObject()
+	e1:GetLabelObject():SetLabelObject(nil)
+	e1:Reset()
+	e:Reset()
 end
 function c94142993.thfilter(c)
 	return c:IsSetCard(0x10b) and c:IsAbleToHand()
@@ -62,11 +99,12 @@ function c94142993.tgop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoGrave(g,REASON_EFFECT)
 	end
 end
-function c94142993.cfilter(c,tp)
-	return c:IsPosition(POS_FACEDOWN_DEFENSE) and c:IsControler(tp)
+function c94142993.cfilter(c,tp,se)
+	return c:IsPosition(POS_FACEDOWN_DEFENSE) and c:IsControler(tp) and (se==nil or c:GetReasonEffect()~=se)
 end
 function c94142993.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c94142993.cfilter,1,nil,tp)
+	local se=e:GetLabelObject():GetLabelObject()
+	return eg:IsExists(c94142993.cfilter,1,nil,tp,se)
 end
 function c94142993.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
