9543ea135f63b992e4ff2446ea2d36302b2f4417
mercury233 <me@mercury233.me>
2020-10-09 08:30:48 +0800

    format

--- c69452756.lua
+++ c69452756.lua
@@ -33,7 +33,7 @@ function c69452756.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingTarget(c69452756.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=nil
-	if e:GetType()&EFFECT_TYPE_ACTIVATE then
+	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
 		g=Duel.SelectTarget(tp,c69452756.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,e:GetHandler())
 	else
 		g=Duel.SelectTarget(tp,c69452756.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
