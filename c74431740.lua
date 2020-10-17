f8290bb439bd06c76a7afd64bd958b0f69ef61b5
Mercury233 <me@mercury233.me>
2020-10-17 17:19:12 +0800

    update バスター・リブート

--- c74431740.lua
+++ c74431740.lua
@@ -67,7 +67,9 @@ function c74431740.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingTarget(c74431740.tdfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local g=Duel.GetMatchingGroup(c74431740.tdfilter,tp,LOCATION_GRAVE,0,e:GetHandler(),e)
-	local sg=g:SelectSubGroup(tp,aux.dncheck,false,1,#g)
+	aux.GCheckAdditional=aux.dncheck
+	local sg=g:SelectSubGroup(tp,aux.TRUE,false,1,#g)
+	aux.GCheckAdditional=nil
 	Duel.SetTargetCard(sg)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,sg,#sg,0,0)
 end
