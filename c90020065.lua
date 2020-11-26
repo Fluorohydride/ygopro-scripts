d6630df0af9fd5c323e49c291453b1e8873c1f48
salix5 <salix5@livemail.tw>
2020-11-26 13:59:56 +0800

    fix: タイム・ボマー
    ■「タイム・ボマー」自身がリバースした後、自分のスタンバイフェイズに必ず発動する効果です。
    ■『その総攻撃力の半分のダメージを相手に与える』処理は、『全ての自分のモンスターを破壊し』の処理によって破壊された、自分のモンスターゾーンに表側表示で存在するモンスターのフィールドにて適用されている攻撃力の合計の半分のダメージを相手に与える事になります。（破壊された裏側守備表示のモンスターの攻撃力は、ダメージには含まれません。）

--- c90020065.lua
+++ c90020065.lua
@@ -2,30 +2,29 @@
 function c90020065.initial_effect(c)
 	--flip
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(90020065,0))
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP)
-	e1:SetOperation(c90020065.operation)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_FLIP)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetOperation(c90020065.flipop)
+	c:RegisterEffect(e1)
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(90020065,1))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(c90020065.descon)
+	e1:SetCost(c90020065.descost)
+	e1:SetTarget(c90020065.destg)
+	e1:SetOperation(c90020065.desop)
 	c:RegisterEffect(e1)
 end
-function c90020065.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		local e1=Effect.CreateEffect(c)
-		e1:SetDescription(aux.Stringid(90020065,1))
-		e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-		e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
-		e1:SetRange(LOCATION_MZONE)
-		e1:SetCondition(c90020065.descon)
-		e1:SetCost(c90020065.descost)
-		e1:SetTarget(c90020065.destg)
-		e1:SetOperation(c90020065.desop)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY+RESET_SELF_TURN)
-		c:RegisterEffect(e1)
-	end
+function c90020065.flipop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(90020065,RESET_EVENT+RESETS_STANDARD,0,1)
 end
 function c90020065.descon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
+	return e:GetHandler():GetFlagEffect(90020065)~=0 and Duel.GetTurnPlayer()==tp
 end
 function c90020065.descost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsReleasable() end
@@ -38,8 +37,9 @@ function c90020065.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
 end
 function c90020065.desop(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,0,nil)
-	local dam=math.floor(g:GetSum(Card.GetAttack)/2)
-	Duel.Destroy(g,REASON_EFFECT)
+	local g1=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,0,nil)
+	local g2=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	local dam=math.floor(g2:GetSum(Card.GetAttack)/2)
+	Duel.Destroy(g1,REASON_EFFECT)
 	Duel.Damage(1-tp,dam,REASON_EFFECT)
 end
