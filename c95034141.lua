fa8d3e8d4fcf31561a2a269d2e6b71e377ea13f6
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-12-26 15:40:21 +0800

    fix 黄金郷の七摩天 (#1517)

--- c95034141.lua
+++ c95034141.lua
@@ -114,7 +114,7 @@ function c95034141.alop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CANNOT_TRIGGER)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DRAW)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
 end
