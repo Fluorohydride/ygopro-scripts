5c4ece8848f69e56135ace5bd7b49b4b31c4738c
Mercury233 <me@mercury233.me>
2021-02-05 03:15:35 +0000

    fix 明鏡止水の心

--- c64801562.lua
+++ c64801562.lua
@@ -35,6 +35,14 @@ function c64801562.initial_effect(c)
 	e5:SetCode(EFFECT_SELF_DESTROY)
 	e5:SetCondition(c64801562.descon)
 	c:RegisterEffect(e5)
+	--selfdes in damage step
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e6:SetRange(LOCATION_SZONE)
+	e6:SetCode(EVENT_ADJUST)
+	e6:SetOperation(c64801562.descheck)
+	c:RegisterEffect(e6)
 end
 function c64801562.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
@@ -56,6 +64,16 @@ function c64801562.indval(e,re,rp)
 		or (re:IsHasType(EFFECT_TYPE_CONTINUOUS) and rc:IsHasCardTarget(tc))
 end
 function c64801562.descon(e)
-	local tc=e:GetHandler():GetEquipTarget()
-	return tc and tc:GetAttack()>=1300
+	local c=e:GetHandler()
+	local tc=c:GetEquipTarget()
+	return tc and (tc:GetAttack()>=1300 or c:GetFlagEffect(64801562)>0)
+end
+function c64801562.descheck(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	local c=e:GetHandler()
+	local tc=c:GetEquipTarget()
+	if ph>=PHASE_DAMAGE and ph<=PHASE_DAMAGE_CAL
+		and tc and tc:GetAttack()>=1300 and c:GetFlagEffect(64801562)==0 then
+		c:RegisterFlagEffect(64801562,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE,0,1)
+	end
 end
