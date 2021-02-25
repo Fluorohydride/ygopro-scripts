b260cdd1a918a3d64b38d6305555124a6fc30b3a
mallu11 <54900811+mallu11@users.noreply.github.com>
2021-02-25 15:34:48 +0800

    fix 雨天気ラズラ

--- c92411493.lua
+++ c92411493.lua
@@ -49,7 +49,7 @@ function c92411493.spreg(e,tp,eg,ep,ev,re,r,rp)
 	if not re then return end
 	local c=e:GetHandler()
 	local rc=re:GetHandler()
-	if c:IsReason(REASON_COST) and rc:IsSetCard(0x109) then
+	if c:IsReason(REASON_COST) and rc:IsSetCard(0x109) and c:IsPreviousLocation(LOCATION_ONFIELD) and re:IsActivated() then
 		e:SetLabel(Duel.GetTurnCount()+1)
 		c:RegisterFlagEffect(92411493,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,2)
 	end
