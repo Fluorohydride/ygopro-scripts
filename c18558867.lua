35c8be67eaf66336740824d758e490f13a263d03
salix5 <salix5@livemail.tw>
2021-03-05 21:38:33 +0800

    fix id

--- c18558867.lua
+++ c18558867.lua
@@ -2,7 +2,7 @@
 function c18558867.initial_effect(c)
 	--direct attack
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(18558867,1))
+	e1:SetDescription(aux.Stringid(18558867,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetRange(LOCATION_MZONE)
