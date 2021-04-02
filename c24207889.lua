ab4ac212722a8679f9e54089e5da8175e8a20e03
Mercury233 <me@mercury233.me>
2021-04-02 09:16:56 +0800

    minor

--- c24207889.lua
+++ c24207889.lua
@@ -34,7 +34,7 @@ function c24207889.initial_effect(c)
 		c24207889[0]={}
 		c24207889[1]={}
 		local race=1
-		while bit.band(RACE_ALL,race)~=0 do
+		while race<RACE_ALL do
 			c24207889[0][race]=Group.CreateGroup()	
 			c24207889[0][race]:KeepAlive()	
 			c24207889[1][race]=Group.CreateGroup()	
