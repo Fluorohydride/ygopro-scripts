9f2aac54f7a62c57d6160ec6a7564353e1dfb895
mallu11 <54900811+mallu11@users.noreply.github.com>
2021-05-02 13:53:40 +0800

    Update c86686671.lua

--- c86686671.lua
+++ c86686671.lua
@@ -3,7 +3,6 @@ function c86686671.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,86686671+EFFECT_COUNT_CODE_OATH)
 	e1:SetCondition(c86686671.condition)
