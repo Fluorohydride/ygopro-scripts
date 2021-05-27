13588a29c42eaeeae0224305b7ed4a6937a54220
Chrono-Genex <79453236+chronogenexx@users.noreply.github.com>
2021-05-27 21:54:39 +0900

    fix Terror of Trishula  (#1647)

--- c6075533.lua
+++ c6075533.lua
@@ -2,7 +2,7 @@
 function c6075533.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_GRAVE_ACTION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,6075533)
