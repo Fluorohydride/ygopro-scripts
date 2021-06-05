a58d3f9eba66d76692c58a2c6b492e717ee1fcef
Mercury233 <me@mercury233.me>
2021-06-05 20:16:41 +0800

    fix 夢魔鏡の使徒－ネイロイ

--- c18189187.lua
+++ c18189187.lua
@@ -29,7 +29,7 @@ function c18189187.filter(c)
 	return c:IsFaceup() and c:IsSetCard(0x131)
 end
 function c18189187.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(c18189187.filter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(c18189187.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 end
 function c18189187.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
