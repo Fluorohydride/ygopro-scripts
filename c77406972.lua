7d5140b8a4cc8edb262651880e0902f89d49a9cd
salix5 <salix5@livemail.tw>
2021-06-10 10:03:38 +0800

    EFFECT_SUMMON_PROC

--- c77406972.lua
+++ c77406972.lua
@@ -3,7 +3,6 @@ function c77406972.initial_effect(c)
 	--summon with no tribute
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(77406972,0))
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_SUMMON_PROC)
 	e1:SetCondition(c77406972.ntcon)
