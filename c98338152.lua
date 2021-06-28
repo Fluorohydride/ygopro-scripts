9c5424d5e5cff4760a14d06bf05a7c842e981a04
Mercury233 <me@mercury233.me>
2021-06-28 18:29:07 +0800

    fix 閃刀機－ウィドウアンカー

--- c98338152.lua
+++ c98338152.lua
@@ -43,7 +43,7 @@ function c98338152.activate(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e2)
 		if Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)>=3
-			and tc:IsDisabled() and tc:IsControler(1-tp) and tc:IsControlerCanBeChanged()
+			and not tc:IsImmuneToEffect(e) and tc:IsControler(1-tp) and tc:IsControlerCanBeChanged()
 			and Duel.SelectYesNo(tp,aux.Stringid(98338152,0)) then
 			Duel.BreakEffect()
 			Duel.GetControl(tc,tp,PHASE_END,1)
