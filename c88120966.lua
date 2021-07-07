5745fa45fd483785c1e5729a0a536293ac0d8f98
Mercury233 <me@mercury233.me>
2021-07-07 15:31:19 +0800

    fix No.15 ギミック・パペット－ジャイアントキラー

--- c88120966.lua
+++ c88120966.lua
@@ -34,6 +34,9 @@ function c88120966.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,c88120966.filter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	if g:GetFirst():IsType(TYPE_XYZ) then
+		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
+	end
 end
 function c88120966.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
