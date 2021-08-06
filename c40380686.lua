3586043e6c8047be1568c1e056a920dd8c25aa59
Lyris <Lyris12@users.noreply.github.com>
2021-08-06 08:19:36 -0400

    Add cards by Lyris
    "D.D.D. - Different Dimension Derby"
    "Baby Mudragon"

---
+++ c40380686.lua
@@ -0,0 +1,61 @@
+--coded by Lyris
+--Baby Mudragon
+function c40380686.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_NONTUNER)
+	e1:SetValue(c40380686.tnval)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCondition(c40380686.conditiom)
+	e2:SetTarget(c40380686.target)
+	e2:SetOperation(c40380686.operation)
+	c:RegisterEffect(e2)
+end
+function c40380686.tnval(e,c)
+	return e:GetHandler():IsControler(c:GetControler())
+end
+function c40380686.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE) and r==REASON_SYNCHRO
+end
+function c40380686.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() and chkc:IsType(TYPE_SYNCHRO) end
+	if chk==0 then return Duel.IsExistingTarget(aux.AND(Card.IsFaceup,Card.IsType),tp,LOCATION_MZONE,0,1,nil,TYPE_SYNCHRO) end
+	local ar=0
+	local op=Duel.SelectOption(tp,HINTMSG_ATTRIBUTE,HINTMSG_RACE)
+	if op==0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
+		ar=Duel.AnnounceAttribute(tp,1,ATTRIBUTE_ALL)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RACE)
+		ar=Duel.AnnounceRace(tp,1,RACE_ALL)
+	end
+	e:SetLabel(op,ar)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,aux.AND(Card.IsFaceup,Card.IsType),tp,LOCATION_MZONE,0,1,1,nil,TYPE_SYNCHRO)
+end
+function c40380686.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc:IsRelateToEffect(e)) or tc:IsFacedown() or tc:IsImmuneToEffect(e) then return end
+	local c,op,ar=e:GetHandler(),e:GetLabel()
+	if op>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_RACE)
+		e1:SetValue(ar)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	else
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+		e1:SetValue(ar)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
---
+++ c76552147.lua
@@ -0,0 +1,45 @@
+--coded by Lyris
+--D.D.D. - Different Dimension Derby
+function c76552147.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetHintTiming(TIMING_ATTACK,0x11e0)
+	e1:SetCondition(function() return Duel.IsExistingMatchingCard(c76552147.cfilter,c:GetControler(),LOCATION_MZONE,0,1,nil) end)
+	e1:SetTarget(c76552147.target)
+	e1:SetOperation(c76552147.activate)
+	c:RegisterEffect(e1)
+	if not c76552147.global_check then
+		c76552147.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD)
+		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_RANGE)
+		ge1:SetCode(EFFECT_MATERIAL_CHECK)
+		ge1:SetValue(c76552147.valcheck)
+		Duel.RegisterEffect(ge1,0)
+	end
+end
+function c76552147.valcheck(e,c)
+	local g=c:GetMaterial()
+	if g:IsExists(Card.IsType,1,nil,TYPE_NORMAL) then c:RegisterFlagEffect(76552147,RESET_EVENT+0x4fe0000,0,1) end
+end
+function c76552147.cfilter(c)
+	if c:IsFacedown() or c:GetFlagEffect(76552147)==0 then return false end
+	for _,st in ipairs{SUMMON_TYPE_RITUAL,SUMMON_TYPE_FUSION,SUMMON_TYPE_SYNCHRO,SUMMON_TYPE_XYZ,SUMMON_TYPE_LINK} do
+		if c:IsSummonType(st) then return true end
+	end
+	return false
+end
+function c76552147.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToRemove() and chkc~=c end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,c),1,0,0)
+end
+function c76552147.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then Duel.Remove(tc,POS_FACEUP,REASON_EFFECT) end
+end
