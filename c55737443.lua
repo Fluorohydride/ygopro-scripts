a1d9ee1ac9278265e9026be9ac844e57391d7df7
mercury233 <me@mercury233.me>
2021-08-18 14:45:29 +0800

    revert

--- c55737443.lua
+++ c55737443.lua
@@ -55,6 +55,7 @@ function c55737443.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
 end
 function c55737443.damop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
 end
