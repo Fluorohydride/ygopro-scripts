09b579b8ae1392f01636340b01ba1d6776d4d50d
Hidarime_Yume <s.sonic@qq.com>
2021-08-23 18:56:04 +0800

    Update c17841166.lua (#1684)
    * Update c17841166.lua
    
    * Update c17841166.lua

--- c17841166.lua
+++ c17841166.lua
@@ -21,6 +21,6 @@ function c17841166.etarget(e,c)
 	return bit.band(c:GetOriginalRace(),RACE_MACHINE+RACE_ROCK)~=0
 end
 function c17841166.efilter(e,te,c)
-	return te:IsActiveType(TYPE_MONSTER) and te:GetOwner()~=c
+	return te:IsActiveType(TYPE_MONSTER) and (te:GetOwner()~=c or te:IsActivated() and not c:IsRelateToEffect(te))
 		and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
 end
