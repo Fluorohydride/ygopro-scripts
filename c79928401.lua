ccfc2cfc52476d6ac313ad820ea193040c49d627
salix5 <salix5@livemail.tw>
2021-09-06 22:23:02 +0800

    fix string id

--- c79928401.lua
+++ c79928401.lua
@@ -13,6 +13,7 @@ function c79928401.initial_effect(c)
 	c:RegisterEffect(e1)
 	--tohand
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(79928401,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_DESTROYED)
