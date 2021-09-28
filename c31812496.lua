971a83e5335253d82f1dcf38e1792f22041ed311
salix5 <salix5@livemail.tw>
2021-09-28 21:27:33 +0800

    add EFFECT_CHANGE_INVOLVING_BATTLE_DAMAGE (#1706)
    EFFECT_CHANGE_BATTLE_DAMAGE
    Change the battle damage this card inflicts.
    
    EFFECT_CHANGE_INVOLVING_BATTLE_DAMAGE
    Change the battle damage from battles involving this card.

--- c31812496.lua
+++ c31812496.lua
@@ -3,7 +3,7 @@ function c31812496.initial_effect(c)
 	--deepen damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+	e1:SetCode(EFFECT_CHANGE_INVOLVING_BATTLE_DAMAGE)
 	e1:SetCondition(c31812496.dcon)
 	e1:SetValue(aux.ChangeBattleDamage(1,DOUBLE_DAMAGE))
 	c:RegisterEffect(e1)
