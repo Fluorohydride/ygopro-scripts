31cc4cfe030eae9d3dcf70e37b0011c950db62b9
salix5 <salix5@livemail.tw>
2021-10-06 20:02:44 +0800

    fix race condition
    。その_族モンスター
    。その__族モンスター
    。その___族モンスター

--- c30587695.lua
+++ c30587695.lua
@@ -35,7 +35,7 @@ function c30587695.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c30587695.spop(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) and tc:IsRace(RACE_INSECT) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 	end
 end
