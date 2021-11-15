98c2c10f2c13b83a7c58091fcb3aa2183a1e4cb1
Chrono-Genex <79453236+chronogenexx@users.noreply.github.com>
2021-11-15 09:27:48 +0900

    fix Broww, Huntsman of Dark World (#1740)

--- c79126789.lua
+++ c79126789.lua
@@ -19,15 +19,17 @@ end
 function c79126789.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
 	if tp==rp or tp~=e:GetLabel() then
-		Duel.SetTargetParam(1)
 		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 	else
-		Duel.SetTargetParam(2)
 		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
 	end
 end
 function c79126789.drop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Draw(p,d,REASON_EFFECT)
+	if Duel.Draw(p,d,REASON_EFFECT)~=0 and tp~=rp and tp==e:GetLabel() then
+		Duel.BreakEffect()
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
 end
