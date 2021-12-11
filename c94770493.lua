f78670add5ad86cd54dcbe2099ad13279028a954
Mercury233 <me@mercury233.me>
2021-12-11 09:27:54 +0800

    fix ダブル・アップ・チャンス

--- c94770493.lua
+++ c94770493.lua
@@ -19,12 +19,12 @@ function c94770493.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) and tc:GetFlagEffect(94770493)==0 then
-		tc:RegisterFlagEffect(94770493,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+		tc:RegisterFlagEffect(94770493,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE,0,1)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_EXTRA_ATTACK)
 		e1:SetValue(1)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
 		tc:RegisterEffect(e1)
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
@@ -33,13 +33,17 @@ function c94770493.operation(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetCode(EFFECT_SET_ATTACK_FINAL)
 		e2:SetCondition(c94770493.atkcon)
 		e2:SetValue(c94770493.atkval)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL+PHASE_END)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
 		tc:RegisterEffect(e2)
 	end
 end
 function c94770493.atkcon(e)
 	local ph=Duel.GetCurrentPhase()
-	return ph==PHASE_DAMAGE or ph==PHASE_DAMAGE_CAL
+	if (ph==PHASE_DAMAGE or ph==PHASE_DAMAGE_CAL) and Duel.GetAttacker()==e:GetHandler() then
+		e:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL+PHASE_BATTLE)
+		return true
+	end
+	return false
 end
 function c94770493.atkval(e,c)
 	return e:GetHandler():GetAttack()*2
