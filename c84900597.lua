4851263f4860083ba8fe6bdd8603c0b041764f22
salix5 <salix5@livemail.tw>
2021-12-31 21:57:34 +0800

    fix: 22PP string id

--- c84900597.lua
+++ c84900597.lua
@@ -2,7 +2,7 @@
 function c84900597.initial_effect(c)
 	--to hand
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(84900597,1))
+	e1:SetDescription(aux.Stringid(84900597,0))
 	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
@@ -16,7 +16,7 @@ function c84900597.initial_effect(c)
 	c:RegisterEffect(e2)
 	--indes
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(84900597,2))
+	e3:SetDescription(aux.Stringid(84900597,1))
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_REMOVE)
 	e3:SetCountLimit(1,84900598)
