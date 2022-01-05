23d67d2b66eef408312affae65e6ad1565917505
Mercury233 <me@mercury233.me>
2022-01-05 16:08:03 +0800

    Update c97926515.lua

--- c97926515.lua
+++ c97926515.lua
@@ -31,6 +31,5 @@ function c97926515.activate(e,tp,eg,ep,ev,re,r,rp)
 		local tg=sg:Select(1-tp,1,1,nil)
 		tg:GetFirst():SetStatus(STATUS_TO_HAND_WITHOUT_CONFIRM,true)
 		Duel.SendtoHand(tg,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,tg)
 	end
 end
