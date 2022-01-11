417fc3a7ff132d931554ad5115322e9d73de9f5c
Mercury233 <me@mercury233.me>
2022-01-11 08:43:07 +0800

    fix 斬機超階乗

--- c87804365.lua
+++ c87804365.lua
@@ -59,8 +59,15 @@ function c87804365.spfilter2(c,e,tp)
 end
 function c87804365.activate(e,tp,eg,ep,ev,re,r,rp)
 	local op=e:GetLabel()
-	if Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(c87804365.spfilter2,nil,e,tp)
+	if #g==0 then return end
+	if #g>1 and Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
+	if #g>ft then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		g=g:Select(tp,ft,ft,nil)
+	end
 	local tc=g:GetFirst()
 	while tc do
 		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
