b8502154f98a81423cbb4d0bc9e3e75a4f4ac276
Chrono-Genex <79453236+chronogenexx@users.noreply.github.com>
2022-01-17 09:55:17 +0900

    fix 機怪獣ダレトン (#1793)

--- c86271510.lua
+++ c86271510.lua
@@ -6,8 +6,15 @@ function c86271510.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
+	e1:SetTarget(c86271510.target)
 	e1:SetOperation(c86271510.operation)
 	c:RegisterEffect(e1)
+end
+function c86271510.filter(c)
+	return c:IsFaceup() and not c:IsAttack(c:GetBaseAttack())
+end
+function c86271510.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c86271510.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 end
 function c86271510.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
