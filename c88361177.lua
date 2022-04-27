0fd229bfa9b0d3bd278668b544989d90d8cf4103
Mercury233 <me@mercury233.me>
2022-04-27 09:45:54 +0800

    Update c88361177.lua

--- c88361177.lua
+++ c88361177.lua
@@ -8,7 +8,7 @@ function c88361177.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c88361177.pfilter(c)
-	return c:IsSetCard(0x29) and c:IsRace(RACE_DRAGON)
+	return c:IsSetCard(0x29) and c:IsRace(RACE_DRAGON) and c:IsFaceup()
 end
 function c88361177.pcon(e)
 	return e:GetHandler():GetEquipGroup():IsExists(c88361177.pfilter,1,nil)
