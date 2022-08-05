8c9775d215b0ae43e8957e5a5b6069bc0e0c694a
Mercury233 <me@mercury233.me>
2022-08-05 17:18:27 +0800

    update 宝玉の氾濫

--- c72881007.lua
+++ c72881007.lua
@@ -11,35 +11,33 @@ function c72881007.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c72881007.costfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x1034) and c:IsAbleToGraveAsCost()
+	return c:IsFaceup() and c:IsSetCard(0x1034) and c:IsAbleToGraveAsCost() and c:GetSequence()<5
+end
+function c72881007.gcheck(g,tg)
+	return tg:FilterCount(aux.TRUE,g)>0
 end
 function c72881007.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	e:SetLabel(1)
-	if chk==0 then return Duel.IsExistingMatchingCard(c72881007.costfilter,tp,LOCATION_SZONE,0,4,nil) end
+	local g=Duel.GetMatchingGroup(c72881007.costfilter,tp,LOCATION_SZONE,0,nil)
+	local tg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	if chk==0 then return g:CheckSubGroup(c72881007.gcheck,4,4,tg) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c72881007.costfilter,tp,LOCATION_SZONE,0,4,4,nil)
-	Duel.SendtoGrave(g,REASON_COST)
+	local sg=g:SelectSubGroup(tp,c72881007.gcheck,false,4,4,tg)
+	Duel.SendtoGrave(sg,REASON_COST)
 end
 function c72881007.filter(c,e,tp)
 	return c:IsSetCard(0x1034) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c72881007.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		if e:GetLabel()~=1 then
-			return Duel.IsExistingMatchingCard(c72881007.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
-		else
-			e:SetLabel(0)
-			return Duel.IsExistingMatchingCard(c72881007.filter,tp,LOCATION_SZONE,0,1,nil,e,tp)
-		end
-	end
-	e:SetLabel(0)
+	local tg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	if chk==0 then return e:IsCostChecked() or #tg>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,tg,#tg,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function c72881007.ctfilter(c,tp)
 	return c:IsPreviousControler(tp) and c:IsLocation(LOCATION_GRAVE)
 end
 function c72881007.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetFieldGroup(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,aux.ExceptThisCard(e))
 	Duel.SendtoGrave(g,REASON_EFFECT)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	g=Duel.GetOperatedGroup()
