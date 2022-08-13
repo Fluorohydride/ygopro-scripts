758517accb6fcbca7206c13d9f71309feee50efc
Mercury233 <me@mercury233.me>
2022-08-13 10:42:31 +0800

    Update c72881007.lua

--- c72881007.lua
+++ c72881007.lua
@@ -20,12 +20,12 @@ function c72881007.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(c72881007.costfilter,tp,LOCATION_SZONE,0,nil)
 	local tg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
 	if chk==0 then return g:CheckSubGroup(c72881007.gcheck,4,4,tg) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local sg=g:SelectSubGroup(tp,c72881007.gcheck,false,4,4,tg)
+	local sg=g
+	if #g~=4 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		sg=g:SelectSubGroup(tp,c72881007.gcheck,false,4,4,tg)
+	end
 	Duel.SendtoGrave(sg,REASON_COST)
-end
-function c72881007.filter(c,e,tp)
-	return c:IsSetCard(0x1034) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c72881007.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
@@ -36,18 +36,22 @@ end
 function c72881007.ctfilter(c,tp)
 	return c:IsPreviousControler(tp) and c:IsLocation(LOCATION_GRAVE)
 end
+function c72881007.spfilter(c,e,tp)
+	return c:IsSetCard(0x1034) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
 function c72881007.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,aux.ExceptThisCard(e))
-	Duel.SendtoGrave(g,REASON_EFFECT)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	g=Duel.GetOperatedGroup()
+	local tg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,aux.ExceptThisCard(e))
+	Duel.SendtoGrave(tg,REASON_EFFECT)
+	local g=Duel.GetOperatedGroup()
 	local ct=g:FilterCount(c72881007.ctfilter,nil,1-tp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft>ct then ft=ct end
+	if ft<=0 then return end
 	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	g=Duel.SelectMatchingCard(tp,c72881007.filter,tp,LOCATION_GRAVE,0,ft,ft,nil,e,tp)
-	if g:GetCount()>0 then
+	local sg=Duel.SelectMatchingCard(tp,c72881007.spfilter,tp,LOCATION_GRAVE,0,ft,ft,nil,e,tp)
+	if sg:GetCount()>0 then
 		Duel.BreakEffect()
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
