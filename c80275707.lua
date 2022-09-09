38200e76ea41a3e5dfef517dd697fc41dbf2fea7
Mercury233 <me@mercury233.me>
2022-09-10 07:52:36 +0800

    fix 道化鳥ラフィンパフィン

--- c80275707.lua
+++ c80275707.lua
@@ -23,7 +23,7 @@ function s.initial_effect(c)
 	e2:SetCost(s.tohcost)
 	e2:SetTarget(s.tohtg)
 	e2:SetOperation(s.tohop)
-	c:RegisterEffect(e1)
+	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_FREE_CHAIN)
@@ -50,11 +50,11 @@ end
 function s.confilter(c)
 	return not (c:IsFaceup() and c:IsRace(RACE_WINDBEAST)) or c:IsFacedown()
 end
-function s.tohcon1(e,tp)
+function s.tohcon1(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
 	return g:FilterCount(s.confilter,nil)>0
 end
-function s.tohcon2(e,tp)
+function s.tohcon2(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
 	return #g>0 and g:FilterCount(s.confilter,nil)==0
 end
