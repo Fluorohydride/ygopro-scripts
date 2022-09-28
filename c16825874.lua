079f97a993f7e449e62fee3abab0f3db7bbd2286
Aephiex <34618932+Aephiex@users.noreply.github.com>
2022-09-28 17:15:59 +0800

    Update c16825874.lua

--- c16825874.lua
+++ c16825874.lua
@@ -19,7 +19,7 @@ function c16825874.initial_effect(c)
 	--extra hand synchro
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_HAND_SYNCHRO_MATERIAL)
+	e3:SetCode(EFFECT_HAND_SYNCHRO)
 	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e3:SetValue(1)
 	c:RegisterEffect(e3)
