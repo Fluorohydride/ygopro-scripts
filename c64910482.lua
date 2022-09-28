ec9679079e4ab195642d695e8498c631d7f97ec0
Aephiex <34618932+Aephiex@users.noreply.github.com>
2022-09-28 16:45:25 +0800

    Update c64910482.lua

--- c64910482.lua
+++ c64910482.lua
@@ -9,18 +9,13 @@ function c64910482.initial_effect(c)
 	c:RegisterEffect(e2)
 	--extra hand link
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_HAND_SYNCHRO_MATERIAL)
 	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetTargetRange(LOCATION_HAND,0)
+	e3:SetValue(99)
 	e3:SetTarget(c64910482.hsyntg)
-	e3:SetValue(c64910482.matval)
 	c:RegisterEffect(e3)
 end
 function c64910482.hsyntg(e,c)
 	return c:IsSetCard(0x27) and not c:IsType(TYPE_TUNER)
-end
-function c64910482.matval(e,lc,mg,c,tp)
-	return true,not mg or mg:IsContains(e:GetHandler())
 end
