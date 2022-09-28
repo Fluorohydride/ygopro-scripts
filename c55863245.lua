7796080db573c20f6058ea458f697e0b7d8627bf
Aephiex <34618932+Aephiex@users.noreply.github.com>
2022-09-28 16:45:30 +0800

    Update c55863245.lua

--- c55863245.lua
+++ c55863245.lua
@@ -13,20 +13,15 @@ function c55863245.initial_effect(c)
 	c:RegisterEffect(e2)
 	--extra hand link
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_HAND_SYNCHRO_MATERIAL)
 	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetTargetRange(LOCATION_HAND,0)
-	e3:SetValue(c55863245.matval)
+	e3:SetValue(1)
 	e3:SetCondition(c55863245.syncon)
 	c:RegisterEffect(e3)
 end
 function c55863245.efilter(e,te)
 	return te:IsActiveType(TYPE_MONSTER) and te:GetOwner()~=e:GetOwner()
-end
-function c55863245.matval(e,sync,mg,c,tp)
-	return true,not mg or mg:IsContains(e:GetHandler()) and not mg:IsExists(Card.IsLocation,1,nil,LOCATION_HAND)
 end
 function c55863245.syncon(e)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
