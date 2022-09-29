aa2ef3141c7a051184ec5a5c4b8c1fefd6aa3803
Aephiex <34618932+Aephiex@users.noreply.github.com>
2022-09-29 14:36:48 +0800

    Update c1686814.lua

--- c1686814.lua
+++ c1686814.lua
@@ -1,7 +1,7 @@
 --アルティマヤ・ツィオルキン
 function c1686814.initial_effect(c)
 	c:EnableReviveLimit()
-	aux.AddGenericSpSummonProcedure(c,LOCATION_EXTRA,c1686814.sprfilter,c1686814.sprgoal,2,2,LOCATION_MZONE,0,nil,HINTMSG_TOGRAVE,Duel.SendtoGrave,HINTMSG_TOGRAVE)
+	aux.AddGenericSpSummonProcedure(c,LOCATION_EXTRA,c1686814.sprfilter,c1686814.sprgoal,2,2,LOCATION_MZONE,0,nil,HINTMSG_TOGRAVE,Duel.SendtoGrave,REASON_COST)
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
