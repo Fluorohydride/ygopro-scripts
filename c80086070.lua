4989a2fdaaeafc396cb2a84b75ea78d79486492e
Aephiex <34618932+Aephiex@users.noreply.github.com>
2022-09-29 14:53:46 +0800

    Update c80086070.lua

--- c80086070.lua
+++ c80086070.lua
@@ -40,7 +40,7 @@ function c80086070.sprfilter1(c)
 	return c:IsLevelAbove(8) and c:IsType(TYPE_TUNER)
 end
 function c80086070.sprfilter2(c)
-	return c:IsType(TYPE_SYNCHRO) and not c:IsType(TYPE_TUNER)
+	return c:IsLevelAbove(1) and c:IsType(TYPE_SYNCHRO) and not c:IsType(TYPE_TUNER)
 end
 function c80086070.sprgoal(g,sync)
 	if not aux.gffcheck(g,c80086070.sprfilter1,nil,c80086070.sprfilter2,nil) then return false end
