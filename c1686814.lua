e7a22678a46a031d9912fc0c28be8190acf3d18b
Aephiex <34618932+Aephiex@users.noreply.github.com>
2022-09-29 14:35:57 +0800

    Update c1686814.lua

--- c1686814.lua
+++ c1686814.lua
@@ -1,21 +1,13 @@
 --アルティマヤ・ツィオルキン
 function c1686814.initial_effect(c)
 	c:EnableReviveLimit()
+	aux.AddGenericSpSummonProcedure(c,LOCATION_EXTRA,c1686814.sprfilter,c1686814.sprgoal,2,2,LOCATION_MZONE,0,nil,HINTMSG_TOGRAVE,Duel.SendtoGrave,HINTMSG_TOGRAVE)
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	c:RegisterEffect(e1)
-	--special summon rule
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_SPSUMMON_PROC)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetRange(LOCATION_EXTRA)
-	e2:SetCondition(c1686814.sprcon)
-	e2:SetOperation(c1686814.sprop)
-	c:RegisterEffect(e2)
 	--special summon
 	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -44,30 +36,8 @@ end
 function c1686814.sprfilter(c)
 	return c:IsFaceup() and c:IsLevelAbove(5) and c:IsAbleToGraveAsCost()
 end
-function c1686814.sprfilter1(c,tp,g,sc)
-	local lv=c:GetLevel()
-	return c:IsType(TYPE_TUNER) and g:IsExists(c1686814.sprfilter2,1,c,tp,c,sc,lv)
-end
-function c1686814.sprfilter2(c,tp,mc,sc,lv)
-	local sg=Group.FromCards(c,mc)
-	return c:IsLevel(lv) and not c:IsType(TYPE_TUNER)
-		and Duel.GetLocationCountFromEx(tp,tp,sg,sc)>0
-end
-function c1686814.sprcon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	local g=Duel.GetMatchingGroup(c1686814.sprfilter,tp,LOCATION_MZONE,0,nil)
-	return g:IsExists(c1686814.sprfilter1,1,nil,tp,g,c)
-end
-function c1686814.sprop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.GetMatchingGroup(c1686814.sprfilter,tp,LOCATION_MZONE,0,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g1=g:FilterSelect(tp,c1686814.sprfilter1,1,1,nil,tp,g,c)
-	local mc=g1:GetFirst()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g2=g:FilterSelect(tp,c1686814.sprfilter2,1,1,mc,tp,mc,c,mc:GetLevel())
-	g1:Merge(g2)
-	Duel.SendtoGrave(g1,REASON_COST)
+function c1686814.sprgoal(g,sync)
+	return g:GetClassCount(Card.GetLevel)==1 and g:FilterCount(Card.IsType,nil,TYPE_TUNER)==1
 end
 function c1686814.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(Card.IsControler,1,nil,tp)
