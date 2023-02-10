5d84fae08b9f0101e246400a55b483975b92e9bc
chronogenexx <chronogenexx@gmail.com>
2023-02-10 19:40:02 +0900

    fix

--- c14507213.lua
+++ c14507213.lua
@@ -35,7 +35,12 @@ function c14507213.activate(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SYNCHRO_MATERIAL)
+		e1:SetOwnerPlayer(tp)
+		e1:SetCondition(c14507213.matcon)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
+end
+function c14507213.matcon(e)
+	return e:GetHandler():IsControler(1-e:GetOwnerPlayer())
 end
