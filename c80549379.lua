cc40294eabdc1b7109f1bd73f5bb041df1ae9499
salix5 <salix5566@gmail.com>
2023-03-16 16:27:34 +0800

    fix ドラグニティ－ジャベリン

--- c80549379.lua
+++ c80549379.lua
@@ -2,21 +2,21 @@
 function c80549379.initial_effect(c)
 	--send replace
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_TO_GRAVE_REDIRECT_CB)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCondition(c80549379.repcon)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EFFECT_DESTROY_REPLACE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(c80549379.reptg)
 	e1:SetOperation(c80549379.repop)
 	c:RegisterEffect(e1)
 end
 function c80549379.filter(c)
 	return c:IsFaceup() and c:IsSetCard(0x29) and c:IsRace(RACE_WINDBEAST)
 end
-function c80549379.repcon(e)
+function c80549379.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local tp=c:GetControler()
-	return c:IsFaceup() and c:IsLocation(LOCATION_MZONE) and c:IsReason(REASON_DESTROY)
-		and Duel.IsExistingMatchingCard(c80549379.filter,tp,LOCATION_MZONE,0,1,c)
+	if chk==0 then return not c:IsReason(REASON_REPLACE) and Duel.IsExistingMatchingCard(c80549379.filter,tp,LOCATION_MZONE,0,1,c) end
+	return Duel.SelectEffectYesNo(tp,c,96)
 end
 function c80549379.repop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
