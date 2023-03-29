d981b4ca17288d6a0dbf1ec40c8c2c96298bb485
salix5 <salix5566@gmail.com>
2023-03-29 17:15:03 +0800

    str id: Gold Pride - Nytro Blaster
    add str3: 2nd effect

--- c58884063.lua
+++ c58884063.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--special summon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,2))
 	e2:SetCategory(CATEGORY_TOEXTRA+CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_PHASE+PHASE_END)
