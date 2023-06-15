ff2be2566d089b7d3a2249d8753ac8cf6d1ac722
salix5 <salix5566@gmail.com>
2023-06-15 22:30:08 +0800

    fix エレキャッスル
    ■「エレキャッスル」の効果によってダウンする攻撃力は1000のみとなります。「エレキャッスル」の効果によって攻撃力がダウンしているモンスターが、さらに「エレキ」と名のついたモンスターを攻撃した場合でも、その攻撃力がさらに1000ダウンする事はありません。

--- c58924378.lua
+++ c58924378.lua
@@ -5,23 +5,35 @@ function c58924378.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--atk down
+	--flag effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_BATTLED)
 	e2:SetRange(LOCATION_FZONE)
+	e2:SetCondition(c58924378.atkcon)
 	e2:SetOperation(c58924378.atkop)
 	c:RegisterEffect(e2)
+	--atk down
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetTarget(c58924378.target)
+	e3:SetValue(-1000)
+	c:RegisterEffect(e3)
+end
+function c58924378.target(e,c)
+	local fid=e:GetHandler():GetFieldID()
+	return c:GetFlagEffect(58924378)~=0 and c:GetFlagEffectLabel(58924378)==fid
+end
+function c58924378.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return d and d:IsSetCard(0xe) and not c58924378.target(e,a)
 end
 function c58924378.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
-	local d=Duel.GetAttackTarget()
-	if d and d:IsSetCard(0xe) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e1:SetValue(-1000)
-		a:RegisterEffect(e1)
-	end
+	local fid=e:GetHandler():GetFieldID()
+	a:RegisterFlagEffect(58924378,RESETS_STANDARD,0,1,fid)
 end
