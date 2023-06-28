858fb01bc60f77383b7bc06f8891b13d22b602e6
mercury233 <me@mercury233.me>
2023-06-28 15:48:08 +0800

    fix オルターガイスト

--- c27132400.lua
+++ c27132400.lua
@@ -55,7 +55,8 @@ function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.spop2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+	if tc:IsRelateToEffect(e) and aux.NecroValleyFilter()(tc)
+		and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetDescription(aux.Stringid(id,2))
 		e1:SetType(EFFECT_TYPE_SINGLE)
