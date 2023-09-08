caf04cc8a9862cca8ed9e7d9bbd29056c201a708
songtongtong <songtongtong@hitrontech.com>
2023-09-08 12:39:31 +0800

    fix 反逆の罪宝－スネークアイ

--- c24081957.lua
+++ c24081957.lua
@@ -14,8 +14,10 @@ function s.initial_effect(c)
 end
 function s.filter(c,tp,ft)
 	local r=LOCATION_REASON_TOFIELD
+	if c:IsFacedown() then return false end
 	if not c:IsControler(c:GetOwner()) then r=LOCATION_REASON_CONTROL end
-	return c:IsFaceup() and Duel.GetLocationCount(c:GetOwner(),LOCATION_SZONE,tp,r)>ft
+	local ft=c:GetOwner()==tp and ft or 0
+	return Duel.GetLocationCount(c:GetOwner(),LOCATION_SZONE,tp,r)>0
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc,tp,0) end
