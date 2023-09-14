29a2d37d9f75f2f6f4c093ba49dc2bdbe749d1c2
Mercury233 <me@mercury233.me>
2023-09-14 14:31:17 +0800

    update Herald of the Abyss

--- c94016752.lua
+++ c94016752.lua
@@ -16,7 +16,7 @@ function c94016752.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.PayLPCost(tp,1500)
 end
 function c94016752.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,1-tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
 	local att=Duel.AnnounceAttribute(tp,1,ATTRIBUTE_ALL)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RACE)
