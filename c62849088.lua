af9b35fbbf9dd6667249c7303d8425b7886b0c05
Wind2009-Louse <wind.jackey@qq.com>
2023-10-07 20:02:23 +0800

    Update 妖眼の相剣師
    Make option appears before activation

--- c62849088.lua
+++ c62849088.lua
@@ -13,18 +13,46 @@ function c62849088.initial_effect(c)
 	e1:SetTarget(c62849088.sptg)
 	e1:SetOperation(c62849088.spop)
 	c:RegisterEffect(e1)
-	--destroy
+	--spsummon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(62849088,1))
+	e2:SetDescription(aux.Stringid(62849088,2))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,62849089)
-	e2:SetCondition(c62849088.descon)
-	e2:SetTarget(c62849088.destg)
-	e2:SetOperation(c62849088.desop)
+	e2:SetCondition(c62849088.tcon)
+	e2:SetTarget(c62849088.hsptg)
+	e2:SetOperation(c62849088.hspop)
 	c:RegisterEffect(e2)
+	--draw
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(62849088,3))
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,62849089)
+	e3:SetCondition(c62849088.tcon)
+	e3:SetTarget(c62849088.dtg)
+	e3:SetOperation(c62849088.dop)
+	c:RegisterEffect(e3)
+	--destroy
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(62849088,4))
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_CUSTOM+62849088)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,62849089)
+	e4:SetCondition(c62849088.tcon)
+	e4:SetTarget(c62849088.destg)
+	e4:SetOperation(c62849088.desop)
+	c:RegisterEffect(e4)
+	aux.RegisterMergedDelayedEvent(c,62849088,EVENT_SPSUMMON_SUCCESS)
 end
 function c62849088.cfilter(c)
 	return c:IsFaceup() and c:IsDisabled() and c:IsType(TYPE_EFFECT)
@@ -45,7 +73,7 @@ function c62849088.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function c62849088.descon(e,tp,eg,ep,ev,re,r,rp)
+function c62849088.tcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(Card.IsSummonPlayer,1,nil,1-tp)
 end
 function c62849088.spfilter(c,loc)
@@ -54,68 +82,49 @@ end
 function c62849088.desfilter(c,tp)
 	return c:IsSummonPlayer(1-tp) and c:IsSummonLocation(LOCATION_EXTRA) and c:IsLocation(LOCATION_MZONE)
 end
-function c62849088.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c62849088.hsptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b1=eg:IsExists(c62849088.spfilter,1,nil,LOCATION_HAND) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(Card.IsCanBeSpecialSummoned,tp,LOCATION_HAND,0,1,nil,e,0,tp,false,false)
-	local b2=eg:IsExists(c62849088.spfilter,1,nil,LOCATION_DECK) and Duel.IsPlayerCanDraw(tp,2)
-	local b3=eg:IsExists(c62849088.spfilter,1,nil,LOCATION_EXTRA)
-	if chk==0 then return b1 or b2 or b3 end
-	local off=1
-	local ops={}
-	local opval={}
-	if b1 then
-		ops[off]=aux.Stringid(62849088,2)
-		opval[off-1]=1
-		off=off+1
-	end
-	if b2 then
-		ops[off]=aux.Stringid(62849088,3)
-		opval[off-1]=2
-		off=off+1
-	end
-	if b3 then
-		ops[off]=aux.Stringid(62849088,4)
-		opval[off-1]=3
-		off=off+1
-	end
-	local op=Duel.SelectOption(tp,table.unpack(ops))
-	e:SetLabel(opval[op])
-	if opval[op]==1 then
-		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-	elseif opval[op]==2 then
-		e:SetCategory(CATEGORY_DRAW)
-		Duel.SetTargetPlayer(tp)
-		Duel.SetTargetParam(2)
-		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
-	else
-		e:SetCategory(CATEGORY_DESTROY)
-		local g=eg:Filter(c62849088.desfilter,nil,tp)
-		Duel.SetTargetCard(g)
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	if chk==0 then return b1 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function c62849088.hspop(e,tp,eg,ep,ev,re,r,rp)
+	local sel=e:GetLabel()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,Card.IsCanBeSpecialSummoned,tp,LOCATION_HAND,0,1,1,nil,e,0,tp,false,false)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function c62849088.desop(e,tp,eg,ep,ev,re,r,rp)
+function c62849088.dtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b2=eg:IsExists(c62849088.spfilter,1,nil,LOCATION_DECK) and Duel.IsPlayerCanDraw(tp,2)
+	if chk==0 then return b2 end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function c62849088.dop(e,tp,eg,ep,ev,re,r,rp)
 	local sel=e:GetLabel()
-	if sel==1 then
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,Card.IsCanBeSpecialSummoned,tp,LOCATION_HAND,0,1,1,nil,e,0,tp,false,false)
-		if g:GetCount()>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
+
+function c62849088.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b3=eg:IsExists(c62849088.spfilter,1,nil,LOCATION_EXTRA)
+	if chk==0 then return b3 end
+	local g=eg:Filter(c62849088.desfilter,nil,tp)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c62849088.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e):Filter(Card.IsSummonLocation,nil,LOCATION_EXTRA)
+	if g:GetCount()>0 then
+		if g:GetCount()>1 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+			g=g:Select(tp,1,1,nil)
 		end
-	elseif sel==2 then
-		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-		Duel.Draw(p,d,REASON_EFFECT)
-	else
-		local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e):Filter(Card.IsSummonLocation,nil,LOCATION_EXTRA)
-		if g:GetCount()>0 then
-			if g:GetCount()>1 then
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-				g=g:Select(tp,1,1,nil)
-			end
-			Duel.HintSelection(g)
-			Duel.Destroy(g,REASON_EFFECT)
-		end
+		Duel.HintSelection(g)
+		Duel.Destroy(g,REASON_EFFECT)
 	end
 end
