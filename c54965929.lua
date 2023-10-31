0569c2e89af17bbe04398f4f2f8b212f34a638e7
salix5 <salix5566@gmail.com>
2023-10-31 12:17:31 +0800

    replace 1 to FLAG_ID_CHAINING

--- c54965929.lua
+++ c54965929.lua
@@ -72,7 +72,7 @@ function c54965929.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c54965929.acop(e,tp,eg,ep,ev,re,r,rp)
-	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and e:GetHandler():GetFlagEffect(1)>0 then
+	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and e:GetHandler():GetFlagEffect(FLAG_ID_CHAINING)>0 then
 		e:GetHandler():AddCounter(0x1,1)
 	end
 end
