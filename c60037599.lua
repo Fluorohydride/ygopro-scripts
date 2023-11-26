67dcd27471689045fe79d02c3aec9d5dbb17805f
salix5 <salix5566@gmail.com>
2023-11-26 13:33:53 +0800

    fix 竜輝巧－エルγ

--- c60037599.lua
+++ c60037599.lua
@@ -27,6 +27,6 @@ function c60037599.extraop(e,tp)
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local sg=g:Select(tp,1,1,nil)
-		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(sg,0,tp,tp,false,true,POS_FACEUP)
 	end
 end
