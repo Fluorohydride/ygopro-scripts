a81c86801ff1e37692613aea7322ad13ad77b275
salix5 <salix5566@gmail.com>
2023-11-29 23:59:08 +0800

    fix Duel.CheckReleaseGroupEx reason

--- c42600274.lua
+++ c42600274.lua
@@ -29,13 +29,13 @@ function c42600274.hspcon(e,c)
 	local ct=0
 	if Duel.CheckReleaseGroup(REASON_SPSUMMON,tp,c42600274.hspfilter,1,nil,tp) then ct=ct-1 end
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>ct
-		and Duel.CheckReleaseGroupEx(REASON_COST,tp,Card.IsRace,1,e:GetHandler(),RACE_WARRIOR+RACE_FAIRY)
+		and Duel.CheckReleaseGroupEx(REASON_SPSUMMON,tp,Card.IsRace,1,e:GetHandler(),RACE_WARRIOR+RACE_FAIRY)
 end
 function c42600274.hspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	local g=nil
 	if ft>0 then
-		g=Duel.SelectReleaseGroupEx(REASON_COST,tp,Card.IsRace,1,1,e:GetHandler(),RACE_WARRIOR+RACE_FAIRY)
+		g=Duel.SelectReleaseGroupEx(REASON_SPSUMMON,tp,Card.IsRace,1,1,e:GetHandler(),RACE_WARRIOR+RACE_FAIRY)
 	else
 		g=Duel.SelectReleaseGroup(REASON_SPSUMMON,tp,c42600274.hspfilter,1,1,nil,tp)
 	end
