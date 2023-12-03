8ba269bf26a0adb2549aa936a82060733c4322a9
chronogenexx <chronogenexx@gmail.com>
2023-12-03 22:12:54 +0900

    fix

--- c52709508.lua
+++ c52709508.lua
@@ -116,8 +116,7 @@ function c52709508.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c52709508.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) and tc:IsAttribute(ATTRIBUTE_LIGHT) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
-		Duel.ConfirmCards(1-tp,tc)
 	end
 end
