c947264459f78ec0f26ed6d64bd755df7b6529be
salix5 <salix5566@gmail.com>
2023-12-24 16:59:09 +0800

    Revert "Fix サブマリンロイド (#2170)"
    This reverts commit b7bdd35750b92955457e9da095279d1bc1c37a06.

--- c99861526.lua
+++ c99861526.lua
@@ -29,8 +29,7 @@ function c99861526.rdcon(e)
 		and c:GetEffectCount(EFFECT_DIRECT_ATTACK)<2 and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
 end
 function c99861526.rval(e,damp)
-	local c=e:GetHandler()
-	if damp==1-e:GetHandlerPlayer() and not c:IsHasEffect(EFFECT_CHANGE_BATTLE_DAMAGE) then
+	if damp==1-e:GetHandlerPlayer() then
 		return e:GetHandler():GetBaseAttack()
 	else return -1 end
 end
