ff245c88889d4c947516ca4450542324cb0e37cb
想象力很好的毛虫 <71340108+Crescent0v0@users.noreply.github.com>
2023-12-28 22:05:35 +0800

    Update c9113513.lua

--- c9113513.lua
+++ c9113513.lua
@@ -43,6 +43,7 @@ function c9113513.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		return res
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,0,tp,LOCATION_GRAVE)
 end
 function c9113513.filter0(c,e)
 	return c:IsCanBeFusionMaterial() and not c:IsImmuneToEffect(e)
