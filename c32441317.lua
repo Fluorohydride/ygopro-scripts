8592dbb90707f3928a935d940682a2e58ccd1d56
a597449807 <148566174+a597449807@users.noreply.github.com>
2024-01-03 19:48:12 +0800

    fix シンクロキャンセル

--- c32441317.lua
+++ c32441317.lua
@@ -32,8 +32,7 @@ function c32441317.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ct=mg:GetCount()
 	local sumtype=tc:GetSummonType()
 	if Duel.SendtoDeck(tc,nil,SEQ_DECKTOP,REASON_EFFECT)~=0 and sumtype==SUMMON_TYPE_SYNCHRO
-		and ct>0 and not Duel.IsPlayerAffectedByEffect(tp,59822133)
-		and ct<=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		and tc:IsLocation(LOCATION_EXTRA) and ct>0 and not Duel.IsPlayerAffectedByEffect(tp,59822133) and ct<=Duel.GetLocationCount(tp,LOCATION_MZONE)
 		and mg:FilterCount(aux.NecroValleyFilter(c32441317.mgfilter),nil,e,tp,tc)==ct
 		and Duel.SelectYesNo(tp,aux.Stringid(32441317,0)) then
 		Duel.BreakEffect()
