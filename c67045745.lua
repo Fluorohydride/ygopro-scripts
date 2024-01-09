5f9daf5b340eacd3ef3ec815320dc446761f33eb
八宫一月 <135235980+kiritosoft@users.noreply.github.com>
2024-01-09 15:14:30 +0800

    Update c67045745.lua

--- c67045745.lua
+++ c67045745.lua
@@ -21,7 +21,7 @@ function c67045745.initial_effect(c)
 	e0:SetCode(EVENT_CHAINING)
 	e0:SetRange(LOCATION_MZONE)
 	e0:SetCondition(c67045745.spchk)
-	e0:SetOperation(aux.chainreg)
+	e0:SetOperation(c67045745.chainreg)
 	c:RegisterEffect(e0)
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(67045745,0))
@@ -30,6 +30,7 @@ function c67045745.initial_effect(c)
 	e3:SetCode(EVENT_CHAIN_SOLVED)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCondition(c67045745.lvcon)
+	e3:SetTarget(c67045745.lvtg)
 	e3:SetOperation(c67045745.lvop)
 	c:RegisterEffect(e3)
 	--attack twice
@@ -56,8 +57,17 @@ end
 function c67045745.spchk(e,tp,eg,ep,ev,re,r,rp)
 	return re:IsActiveType(TYPE_SPELL+TYPE_TRAP) and re:GetHandler():IsSetCard(0x107)
 end
+function c67045745.chainreg(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(67045745,RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_CHAIN,0,1)
+end
 function c67045745.lvcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetFlagEffect(1)>0
+	e:SetLabel(e:GetHandler():GetFlagEffect(67045745))
+	return e:GetHandler():GetFlagEffect(67045745)>0
+end
+function c67045745.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local count=e:GetLabel()
+	if chk==0 then return true end
+	e:SetCountLimit(count,EFFECT_COUNT_CODE_CHAIN)
 end
 function c67045745.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
