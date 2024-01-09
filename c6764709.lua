b6fc18f97808c4d6a34deed472cccf9ed5317ad4
八宫一月 <135235980+kiritosoft@users.noreply.github.com>
2024-01-09 15:07:39 +0800

    Update c6764709.lua

--- c6764709.lua
+++ c6764709.lua
@@ -23,7 +23,7 @@ function c6764709.initial_effect(c)
 	e0:SetCode(EVENT_CHAINING)
 	e0:SetRange(LOCATION_MZONE)
 	e0:SetCondition(c6764709.spchk)
-	e0:SetOperation(aux.chainreg)
+	e0:SetOperation(c6764709.chainreg)
 	c:RegisterEffect(e0)
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(6764709,0))
@@ -32,6 +32,7 @@ function c6764709.initial_effect(c)
 	e4:SetCode(EVENT_CHAIN_SOLVED)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCondition(c6764709.lvcon)
+	e4:SetTarget(c6764709.lvtg)
 	e4:SetOperation(c6764709.lvop)
 	c:RegisterEffect(e4)
 	--destroy
@@ -65,8 +66,17 @@ end
 function c6764709.spchk(e,tp,eg,ep,ev,re,r,rp)
 	return re:IsActiveType(TYPE_SPELL+TYPE_TRAP) and re:GetHandler():IsSetCard(0x107)
 end
+function c6764709.chainreg(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(6764709,RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_CHAIN,0,1)
+end
 function c6764709.lvcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetFlagEffect(1)>0
+	e:SetLabel(e:GetHandler():GetFlagEffect(6764709))
+	return e:GetHandler():GetFlagEffect(6764709)>0
+end
+function c6764709.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local count=e:GetLabel()
+	if chk==0 then return true end
+	e:SetCountLimit(count,EFFECT_COUNT_CODE_CHAIN)
 end
 function c6764709.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
