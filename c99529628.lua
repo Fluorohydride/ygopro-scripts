d2912bb87be2cfc1bbbf62af5546b7aa202e8cdd
Lyris <lyris89@live.com>
2024-01-14 20:21:46 -0500

    fix `ゴーティスの朧キーフ`
    User was allowed to control no Fish monsters for it to activate its self-Summoning effect, while the opponent controlled any

--- c99529628.lua
+++ c99529628.lua
@@ -49,7 +49,7 @@ function s.cfilter(c)
 	return c:IsFaceup() and c:IsRace(RACE_FISH)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
