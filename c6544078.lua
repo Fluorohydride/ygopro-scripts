f251b90bd681ffc49a59b0bd1f2a37263e8a302b
chronogenexx <chronogenexx@gmail.com>
2024-01-18 17:07:47 +0900

    update special summon proc

--- c6544078.lua
+++ c6544078.lua
@@ -8,6 +8,7 @@ function c6544078.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCondition(c6544078.spcon)
+	e1:SetTarget(c6544078.sptg)
 	e1:SetOperation(c6544078.spop)
 	c:RegisterEffect(e1)
 	--spirit may not return
@@ -26,8 +27,16 @@ function c6544078.spcon(e,c)
 	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(c6544078.filter,c:GetControler(),LOCATION_HAND,0,1,nil)
 end
-function c6544078.spop(e,tp,eg,ep,ev,re,r,rp,c)
+function c6544078.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local g=Duel.GetMatchingGroup(c6544078.filter,tp,LOCATION_HAND,0,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c6544078.filter,tp,LOCATION_HAND,0,1,1,nil)
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
+	local tc=g:SelectUnselect(nil,tp,false,true,1,1)
+	if tc then
+		e:SetLabelObject(tc)
+		return true
+	else return false end
+end
+function c6544078.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	Duel.Remove(g,POS_FACEUP,REASON_SPSUMMON)
 end
