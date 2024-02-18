f222d67a72a0990143d4ce23e76dea7168e89abf
VforViolin <144184504+VforViolin@users.noreply.github.com>
2024-02-18 12:45:58 -0500

    Update c72270339.lua
    Fix Diabellstar to make it check if it was in it's owner's hand/field.

--- c72270339.lua
+++ c72270339.lua
@@ -63,7 +63,7 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	if tc then Duel.SSet(tp,tc) end
 end
 function s.rvcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousLocation(LOCATION_HAND+LOCATION_ONFIELD) and Duel.GetTurnPlayer()==1-tp
+	return e:GetHandler():IsPreviousLocation(LOCATION_HAND+LOCATION_ONFIELD) and c:IsPreviousControler(tp) and Duel.GetTurnPlayer()==1-tp
 end
 function s.rvtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
