dad78b2da207822b608e141761344b00ced1e3fe
Wind2009-Louse <wind.jackey@qq.com>
2024-03-06 16:45:22 +0800

    Fix Salamandra Fusion (#2437)
    Add AddCodeList

--- c9102835.lua
+++ c9102835.lua
@@ -1,6 +1,7 @@
 --Salamandra Fusion
 local s,id,o=GetID()
 function s.initial_effect(c)
+	aux.AddCodeList(c,45231177)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
