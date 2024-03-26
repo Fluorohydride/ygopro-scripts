0c4e41eb245b91fb33bf90d275e69e6f585fd38b
八宫一月 <135235980+kiritosoft@users.noreply.github.com>
2024-03-26 08:25:37 +0800

    Fix  シンセシス・ミュートリアス  極神聖帝オーディン (#2451)

--- c79194594.lua
+++ c79194594.lua
@@ -65,7 +65,7 @@ function c79194594.immop(e,tp,eg,ep,ev,re,r,rp)
 	if c:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetRange(LOCATION_MZONE)
 		e1:SetCode(EFFECT_IMMUNE_EFFECT)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
