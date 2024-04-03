fe0bcb4dd0837432ebb87ff1044ec2fdc900563a
八宫一月 <135235980+kiritosoft@users.noreply.github.com>
2024-04-03 18:52:09 +0800

    Update c41685633.lua

--- c41685633.lua
+++ c41685633.lua
@@ -58,7 +58,7 @@ function c41685633.fusioncodechk(c,codetab)
 	end
 end
 function c41685633.sprfilter2(c)
-	local codetab={c:GetFusionCode()} 
+	local codetab={c:GetFusionCode()}
 	local codechk=c41685633.fusioncodechk(c,codetab)
 	return c:IsLocation(LOCATION_MZONE) and c:IsFusionType(TYPE_FUSION) and codechk
 end
