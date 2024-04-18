470ffc538ada12819f5b1dc293dd47feeb1d2b7c
想象力很好的毛虫 <71340108+Crescent0v0@users.noreply.github.com>
2024-04-18 09:16:32 +0800

    Fix トイ・タンク & トイ・ソルジャー (#2485)

--- c65504487.lua
+++ c65504487.lua
@@ -38,7 +38,8 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return c:IsPreviousLocation(LOCATION_SZONE) and c:IsPreviousPosition(POS_FACEDOWN)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
