391abd45066bb04d9f3f079bd9a83796f851bbb9
VforViolin <144184504+VforViolin@users.noreply.github.com>
2024-07-08 12:53:42 -0400

    Update c18973184.lua

--- c18973184.lua
+++ c18973184.lua
@@ -46,7 +46,7 @@ function c18973184.efilter(e,ct)
 	return p==tp and te:IsHasCategory(CATEGORY_FUSION_SUMMON)
 end
 function c18973184.limfilter(c,tp)
-	return c:IsSummonPlayer(tp) and c:IsSummonType(SUMMON_TYPE_FUSION)
+	return c:IsSummonPlayer(tp) and c:IsSummonType(SUMMON_TYPE_FUSION) and c:GetSpecialSummonInfo(SUMMON_INFO_REASON_EFFECT):IsHasCategory(CATEGORY_FUSION_SUMMON)
 end
 function c18973184.limcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(c18973184.limfilter,1,nil,tp)
