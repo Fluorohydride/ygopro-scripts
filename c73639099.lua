813a19009b134e3e69f2b09c5058bf77e47e61e4
Wind2009-Louse <wind.jackey@qq.com>
2024-07-22 09:42:22 +0800

    Add Duel.Hint for Traptrix Sera

--- c73639099.lua
+++ c73639099.lua
@@ -60,6 +60,7 @@ end
 function c73639099.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(c73639099.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function c73639099.spop(e,tp,eg,ep,ev,re,r,rp)
@@ -80,6 +81,7 @@ function c73639099.setfilter(c)
 end
 function c73639099.settg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c73639099.setfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 end
 function c73639099.setop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
