4ef51ed7a2ca4cf92fbf9b73cdfe5182cd09f3e7
a597449807 <148566174+a597449807@users.noreply.github.com>
2024-09-01 19:37:25 +0800

    fix 古代の機械戦車兵
    修复②效果能以里侧表示的卡为对象发动的问题。

--- c91098230.lua
+++ c91098230.lua
@@ -45,10 +45,10 @@ function c91098230.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c91098230.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() end
-	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,0,1,nil) end
+	if chkc then return chkc:IsOnField() and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_ONFIELD,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function c91098230.desop(e,tp,eg,ep,ev,re,r,rp)
