37665718d5212681198c005db41b8563a81b89d3
Wind2009-Louse <wind.jackey@qq.com>
2024-09-05 22:10:08 +0800

    Remove CATEGORY_SPECIAL_SUMMON for 贖いのエンブレーマ (#2644)

--- c61950680.lua
+++ c61950680.lua
@@ -15,7 +15,6 @@ function s.initial_effect(c)
 	--
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_DESTROYED)
