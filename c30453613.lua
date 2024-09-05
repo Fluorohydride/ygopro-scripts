8965d79d6643d0b61ee98593cc3bb6edece8ea87
a597449807 <148566174+a597449807@users.noreply.github.com>
2024-09-05 22:31:33 +0800

    fix Awakening of Veidos (#2648)
    修复对方场上存在超雷龙，我方发动此卡把场地放置在对方场上后会提示选择加入手卡，然后没有实际加入的问题（应该不会提示加入手卡）。

--- c30453613.lua
+++ c30453613.lua
@@ -20,7 +20,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
 end
 function s.thfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_PYRO) and c:IsLevelAbove(5)
+	return c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_PYRO) and c:IsLevelAbove(5) and c:IsAbleToHand()
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
