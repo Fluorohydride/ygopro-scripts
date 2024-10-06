af42a750ac81354432c96885fd43a2f2e2a2a961
八宫一月 <135235980+kiritosoft@users.noreply.github.com>
2024-10-06 22:45:49 +0800

    Update c64182380.lua

--- c64182380.lua
+++ c64182380.lua
@@ -86,6 +86,7 @@ end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ct=e:GetHandler():GetPreviousOverlayCountOnField()
 	if chk==0 then return ct>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsStatus(STATUS_PROC_COMPLETE)
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	e:SetLabel(ct)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
