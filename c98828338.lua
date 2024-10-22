9a621c85ccd6705cbff5c2f0570feee72e292cfd
想象力很好的毛虫 <71340108+Crescent0v0@users.noreply.github.com>
2024-10-23 01:19:58 +0800

    Update c98828338.lua (#2706)

--- c98828338.lua
+++ c98828338.lua
@@ -3,7 +3,7 @@ local s,id,o=GetID()
 function s.initial_effect(c)
 	--
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SEARCH+CATEGORY_GRAVE_ACTION)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_GRAVE_ACTION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id)
