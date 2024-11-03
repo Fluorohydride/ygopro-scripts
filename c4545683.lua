c4e6009e8759d28e9b617a251cdbffb897807e82
salix5 <salix5566@gmail.com>
2024-11-03 10:01:32 +0800

    remove EFFECT_FLAG_COPY_INHERIT

--- c4545683.lua
+++ c4545683.lua
@@ -100,7 +100,7 @@ function c4545683.eqop(e,tp,eg,ep,ev,re,r,rp)
 		--Add Equip limit
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
+		e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
 		e1:SetCode(EFFECT_EQUIP_LIMIT)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		e1:SetValue(c4545683.eqlimit)
