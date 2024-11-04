954711087d1ce14f82d59294d704f63a94a37621
salix5 <salix5566@gmail.com>
2024-11-04 20:23:55 +0800

    add s.fusion_effect

--- c99599062.lua
+++ c99599062.lua
@@ -24,6 +24,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
+s.fusion_effect=true
 function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	local ph=Duel.GetCurrentPhase()
 	return Duel.GetTurnPlayer()==tp and (ph==PHASE_MAIN1 or ph==PHASE_MAIN2)
