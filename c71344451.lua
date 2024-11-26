7678cb1171b7de8fe3375f74121cb1e0401cac3d
Wind2009-Louse <wind.jackey@qq.com>
2024-11-26 20:16:22 +0800

    Update 一撃必殺！居合いドロー (#2760)

--- c71344451.lua
+++ c71344451.lua
@@ -51,6 +51,7 @@ function c71344451.activate(e,tp,eg,ep,ev,re,r,rp)
 				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 				local dg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(Card.IsAbleToDeck),tp,LOCATION_GRAVE,0,ct2,ct2,nil)
 				if dg:GetCount()>0 then
+					Duel.HintSelection(dg)
 					Duel.SendtoDeck(dg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
 				end
 			end
