6b90f68a41877067cea26a0ed50cd14408a254d7
Wind2009-Louse <wind.jackey@qq.com>
2024-12-08 16:21:12 +0800

    Add HintSelection

--- c92607427.lua
+++ c92607427.lua
@@ -81,6 +81,7 @@ function c92607427.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c92607427.thfilter),tp,LOCATION_GRAVE,0,1,1,nil)
 	if g:GetCount()>0 then
+		Duel.HintSelection(g)
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 	end
 end
