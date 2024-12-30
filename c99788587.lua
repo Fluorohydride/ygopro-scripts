e40c5bbfe5a2396def1b3d8eb2d9e583560dd98f
Wind2009-Louse <wind.jackey@qq.com>
2024-12-30 23:10:27 +0800

    Fix 爆導索

--- c99788587.lua
+++ c99788587.lua
@@ -13,16 +13,25 @@ function c99788587.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c99788587.condition(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsAllColumn()
+	return e:GetHandler():IsAllColumn() and e:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
 function c99788587.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
+	local c=e:GetHandler()
+	e:SetLabel(aux.GetColumn(c)+1)
 	local g=e:GetHandler():GetColumnGroup()
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 end
+function c99788587.opfilter(c,col)
+	return aux.GetColumn(c)==col
+end
 function c99788587.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=e:GetHandler():GetColumnGroup()
-	if g:GetCount()>0 then
-		Duel.Destroy(g,REASON_EFFECT)
+	local col=e:GetLabel()
+	if col>0 then
+		col=col-1
+		local g=Duel.GetMatchingGroup(c99788587.opfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,aux.ExceptThisCard(e),col)
+		if g:GetCount()>0 then
+			Duel.Destroy(g,REASON_EFFECT)
+		end
 	end
 end
