9d7810e4f00842051e9244958dc18edf2edc0b1e
salix5 <salix5566@gmail.com>
2025-01-01 18:51:26 +0800

    24PP (#2309)
    * 24PP
    
    * card name
    
    * fix トイ・ボックス
    
    * fix 集いし光
    
    * typos
    
    * Fix 未来の柱－キアノス (#2355)
    
    * format
    
    * add set_as_spell for トイ・ボックス
    
    * fix
    
    * fix トイ・ボックス (#2489)
    
    * Fix トイ・タンク & トイ・ソルジャー (#2485)
    
    * fix 漆黒の太陽 (#2571)
    
    * fix
    
    * fix エレクトロ・ガンナー
    
    * Fix トイ・ボックス (#2641)
    
    * newline
    
    ---------
    
    Co-authored-by: Wind2009-Louse <wind.jackey@qq.com>
    Co-authored-by: mercury233 <me@mercury233.me>
    Co-authored-by: 想象力很好的毛虫 <71340108+Crescent0v0@users.noreply.github.com>
    Co-authored-by: 八宫一月 <135235980+kiritosoft@users.noreply.github.com>

---
+++ c96029570.lua
@@ -0,0 +1,119 @@
+--漆黒の太陽
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--reccover
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_RECOVER)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.reccon)
+	e2:SetTarget(s.rectg)
+	e2:SetOperation(s.recop)
+	c:RegisterEffect(e2)
+	--atk up
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetCode(EVENT_CUSTOM+id)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1,id+o)
+	e3:SetTarget(s.autg)
+	e3:SetOperation(s.auop)
+	c:RegisterEffect(e3)
+	aux.RegisterMergedDelayedEvent(c,id,EVENT_SPSUMMON_SUCCESS)
+	--back to hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCode(EVENT_CUSTOM+id+o)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e4:SetCountLimit(1,id+o*2)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+	aux.RegisterMergedDelayedEvent(c,id+o,EVENT_DISCARD)
+end
+function s.cfilter(c,tp)
+	return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsPreviousControler(tp)
+		and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousPosition(POS_FACEUP)
+end
+function s.reccon(e,tp,eg,ep,ev,re,r,rp)
+	local dg=eg:Filter(s.cfilter,nil,tp)
+	local atk=dg:GetSum(Card.GetTextAttack)
+	e:SetLabel(atk)
+	return atk>0
+end
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(e:GetLabel())
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,e:GetLabel())
+end
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Recover(p,d,REASON_EFFECT)
+end
+function s.aufilter(c,tp,e)
+	return c:IsFaceup() and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_GRAVE)
+		and c:IsLocation(LOCATION_MZONE) and (not e or c:IsCanBeEffectTarget(e))
+end
+function s.autg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g=eg:Filter(s.aufilter,nil,tp,e)
+	if chkc then return g:IsContains(chkc) end
+	if chk==0 then return #g>0 end
+	local tg=g:Clone()
+	if #g>1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		tg=g:Select(tp,1,1,nil)
+	end
+	Duel.SetTargetCard(tg)
+end
+function s.auop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetTargetsRelateToChain():GetFirst()
+	if tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(1000)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
+function s.thfilter(c,e,tp)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsPreviousControler(tp)
+		and c:IsPreviousLocation(LOCATION_HAND) and c:IsReason(REASON_DISCARD)
+		and c:IsCanBeEffectTarget(e) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:FilterCount(s.thfilter,nil,e,tp)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local tg
+	if #eg==1 then
+		tg=eg:Clone()
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+		tg=eg:FilterSelect(tp,s.thfilter,1,1,nil,e,tp)
+	end
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,tg,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
