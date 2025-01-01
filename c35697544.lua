abf8e50cbbdfe7057e3ca87f505c17e7031d05e4
salix5 <salix5566@gmail.com>
2025-01-01 18:55:59 +0800

    MZMI (#2408)
    * MZMI
    
    * Fix MZMI (#2414)
    
    * Remove CATEGORY_SEARCH for Salamandra Fusion.
    * Remove EFFECT_CANNOT_ATTACK for Salamandra with Chain.
    
    * Fix Salamandra Fusion (#2437)
    
    Add AddCodeList
    
    * fix Ultimate Flame Swordsman
    
    * Add Duel.Hint() for Fighting Flame Sword (#2543)
    
    * fix
    
    * fix Mirage Swordsman (#2649)
    
    修复被自身②效果破坏时，还能发动③效果的问题（这张卡被战斗或者其他卡的效果破坏的场合）。
    
    * fix Fighting Flame Sword (#2660)
    
    * newline
    
    * fix Flame Swordsrealm (#2679)
    
    * Fix Ultimate Flame Swordsman (#2684)
    
    https://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=4&cid=19727
    修复①效果没成功破坏对方的怪兽也会给与对方伤害的问题。
    
    * add s.fusion_effect
    
    ---------
    
    Co-authored-by: Wind2009-Louse <wind.jackey@qq.com>
    Co-authored-by: mercury233 <me@mercury233.me>
    Co-authored-by: a597449807 <148566174+a597449807@users.noreply.github.com>
    Co-authored-by: 八宫一月 <135235980+kiritosoft@users.noreply.github.com>

---
+++ c35697544.lua
@@ -0,0 +1,87 @@
+--Fighting Flame Sword
+local s,id,o=GetID()
+function s.initial_effect(c)
+	aux.AddCodeList(c,45231177)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.activate1)
+	c:RegisterEffect(e1)
+	--destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e2:SetCondition(s.condition2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.activate2)
+	c:RegisterEffect(e2)
+	--negate
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_DISABLE)
+	e3:SetType(EFFECT_TYPE_ACTIVATE)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e3:SetCondition(s.condition3)
+	e3:SetTarget(s.target3)
+	e3:SetOperation(s.activate3)
+	c:RegisterEffect(e3)
+end
+function s.thfilter(c)
+	return not c:IsCode(id) and aux.IsCodeListed(c,45231177) and c:IsAbleToHand()
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.activate1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetBattleMonster(tp)
+	return a and a:IsFaceup() and a:IsRace(RACE_WARRIOR) and a:IsAttribute(ATTRIBUTE_FIRE)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsOnField() and chkc~=c end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c) end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then Duel.Destroy(tc,REASON_EFFECT) end
+end
+function s.dfilter(c,tp)
+	return c:IsOnField() and c:IsFaceup() and c:IsControler(tp) and (c:IsCode(45231177) or aux.IsCodeListed(c,45231177)) and c:IsType(TYPE_MONSTER)
+end
+function s.condition3(e,tp,eg,ep,ev,re,r,rp)
+	if rp==tp or not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return g and g:IsExists(s.dfilter,1,nil,tp) and Duel.IsChainDisablable(ev)
+end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function s.activate3(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
+end
