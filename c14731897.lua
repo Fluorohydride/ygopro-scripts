247d3b900a98bf89b7e0775412a0441134b7b040
salix5 <salix5566@gmail.com>
2025-01-06 20:14:16 +0800

    update 結束 UNITY

--- c14731897.lua
+++ c14731897.lua
@@ -13,23 +13,19 @@ function c14731897.initial_effect(c)
 	e1:SetOperation(c14731897.activate)
 	c:RegisterEffect(e1)
 end
+function c14731897.sumfilter(c)
+	return c:IsFaceup() and c:IsDefenseAbove(0)
+end
 function c14731897.filter(c,def)
 	return c:IsFaceup() and c:IsDefenseAbove(0) and not c:IsDefense(def)
 end
 function c14731897.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-	local def=0
-	local sc=g:GetFirst()
-	while sc do
-		local cdef=sc:GetBaseDefense()
-		if cdef<0 then cdef=0 end
-		def=def+cdef
-		sc=g:GetNext()
-	end
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c14731897.filter(chkc,def) end
-	if chk==0 then return Duel.IsExistingTarget(c14731897.filter,tp,LOCATION_MZONE,0,1,nil,def) end
+	local g=Duel.GetMatchingGroup(c14731897.sumfilter,tp,LOCATION_MZONE,0,nil)
+	local sum=g:GetSum(Card.GetBaseDefense)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c14731897.filter(chkc,sum) end
+	if chk==0 then return Duel.IsExistingTarget(c14731897.filter,tp,LOCATION_MZONE,0,1,nil,sum) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c14731897.filter,tp,LOCATION_MZONE,0,1,1,nil,def)
+	Duel.SelectTarget(tp,c14731897.filter,tp,LOCATION_MZONE,0,1,1,nil,sum)
 end
 function c14731897.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
