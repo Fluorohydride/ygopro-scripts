977058e560773f51799758f7cdea0b43a44a878d
Wind2009-Louse <wind.jackey@qq.com>
2025-06-14 15:14:16 +0800

    Add new fusion procedure
    Remain 184 cards to do

--- c81788994.lua
+++ c81788994.lua
@@ -1,5 +1,6 @@
 --影牢の呪縛
-function c81788994.initial_effect(c)
+local s,id,o=GetID()
+function s.initial_effect(c)
 	c:EnableCounterPermit(0x16)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -12,8 +13,8 @@ function c81788994.initial_effect(c)
 	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e2:SetRange(LOCATION_FZONE)
-	e2:SetCondition(c81788994.ctcon)
-	e2:SetOperation(c81788994.ctop)
+	e2:SetCondition(s.ctcon)
+	e2:SetOperation(s.ctop)
 	c:RegisterEffect(e2)
 	--atkdown
 	local e3=Effect.CreateEffect(c)
@@ -21,31 +22,47 @@ function c81788994.initial_effect(c)
 	e3:SetCode(EFFECT_UPDATE_ATTACK)
 	e3:SetRange(LOCATION_FZONE)
 	e3:SetTargetRange(0,LOCATION_MZONE)
-	e3:SetCondition(c81788994.atkcon)
-	e3:SetValue(c81788994.atkval)
+	e3:SetCondition(s.atkcon)
+	e3:SetValue(s.atkval)
 	c:RegisterEffect(e3)
-	--
+	-- Each time you Fusion Summon a "Shaddoll" Fusion Monster, you can remove 3 Spellstone Counters from this card to use 1 appropriate face-up monster your opponent controls as 1 of the Fusion Materials.
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(81788994,0))
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_EXTRA_FUSION_MATERIAL)
 	e4:SetRange(LOCATION_FZONE)
-	e4:SetCode(81788994)
+	e4:SetCondition(s.extra_material_con)
+	e4:SetTargetRange(0,LOCATION_MZONE)
+	e4:SetTarget(function(_,mc) return mc:IsFaceup() and mc:IsType(TYPE_MONSTER) end)
+	e4:SetCost(s.extra_material_cost)
+	e4:SetOperation(function() return FusionSpell.FUSION_OPERATION_INHERIT end)
+	e4:SetValue(function(extra_material_effect,tc) return tc and tc:IsSetCard(0x9d) and tc:IsControler(extra_material_effect:GetHandlerPlayer()) end)
+	e4:SetLabel(1)  --- at most 1 material per fusion effect
 	c:RegisterEffect(e4)
 end
-function c81788994.cfilter(c)
+function s.cfilter(c)
 	return c:IsSetCard(0x9d) and c:IsType(TYPE_MONSTER) and c:IsReason(REASON_EFFECT)
 end
-function c81788994.ctcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c81788994.cfilter,1,nil)
+function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil)
 end
-function c81788994.ctop(e,tp,eg,ep,ev,re,r,rp)
-	local ct=eg:FilterCount(c81788994.cfilter,nil)
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=eg:FilterCount(s.cfilter,nil)
 	e:GetHandler():AddCounter(0x16,ct)
 end
-function c81788994.atkcon(e)
+function s.atkcon(e)
 	return Duel.GetTurnPlayer()~=e:GetHandlerPlayer()
 end
-function c81788994.atkval(e,c)
+function s.atkval(e,c)
 	return e:GetHandler():GetCounter(0x16)*-100
+end
+function s.extra_material_con(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsCanRemoveCounter(tp,0x16,3,REASON_ACTION)
+end
+---@param e Effect
+function s.extra_material_cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return e:GetHandler():IsCanRemoveCounter(tp,0x16,3,REASON_ACTION)
+	end
+	e:GetHandler():RemoveCounter(tp,0x16,3,REASON_ACTION)
 end
