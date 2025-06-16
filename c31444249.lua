2837c4729d00910cc0da4a37d0e376570eca6a1d
Vury Leo <i@vuryleo.com>
2025-06-16 21:51:38 +0900

    Add インフェルノイド to new fusion

--- c31444249.lua
+++ c31444249.lua
@@ -1,5 +1,6 @@
 --煉獄の虚夢
-function c31444249.initial_effect(c)
+local s,id,o=GetID()
+function s.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -12,7 +13,7 @@ function c31444249.initial_effect(c)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
 	e2:SetValue(1)
-	e2:SetTarget(c31444249.lvtg)
+	e2:SetTarget(s.lvtg)
 	c:RegisterEffect(e2)
 	--reduce battle damage
 	local e3=Effect.CreateEffect(c)
@@ -20,127 +21,54 @@ function c31444249.initial_effect(c)
 	e3:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetTargetRange(LOCATION_MZONE,0)
-	e3:SetTarget(c31444249.rdtg)
+	e3:SetTarget(s.rdtg)
 	e3:SetValue(aux.ChangeBattleDamage(1,HALF_DAMAGE))
 	c:RegisterEffect(e3)
-	--spsummon
-	local e4=Effect.CreateEffect(c)
+	--fusion summon
+	local e4=FusionSpell.CreateSummonEffect(c,{
+		fusfilter=s.fusfilter,
+		pre_select_mat_location=s.pre_select_mat_location,
+		additional_fcheck=s.fcheck
+	})
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON+CATEGORY_DECKDES)
 	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetRange(LOCATION_SZONE)
-	e4:SetCost(c31444249.spcost)
-	e4:SetTarget(c31444249.sptg)
-	e4:SetOperation(c31444249.spop)
+	e4:SetCost(s.spcost)
 	c:RegisterEffect(e4)
 end
-function c31444249.lvtg(e,c)
+
+function s.lvtg(e,c)
 	return c:IsSetCard(0xbb) and c:GetOriginalLevel()>=2
 end
-function c31444249.rdtg(e,c)
+
+function s.rdtg(e,c)
 	return c:IsSetCard(0xbb) and c:GetOriginalLevel()>=2
 end
-function c31444249.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
-function c31444249.filter0(c)
-	return c:IsType(TYPE_MONSTER) and c:IsCanBeFusionMaterial() and c:IsAbleToGrave()
+
+function s.fusfilter(c)
+	return c:IsSetCard(0xbb)
 end
-function c31444249.filter1(c,e)
-	return not c:IsImmuneToEffect(e)
-end
-function c31444249.filter2(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and c:IsSetCard(0xbb) and (not f or f(c))
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
-end
-function c31444249.fcheck(tp,sg,fc)
-	return sg:FilterCount(Card.IsLocation,nil,LOCATION_DECK)<=6
-end
-function c31444249.gcheck(sg)
-	return sg:FilterCount(Card.IsLocation,nil,LOCATION_DECK)<=6
-end
-function c31444249.dmcon(tp)
-	return not Duel.IsExistingMatchingCard(Card.IsSummonLocation,tp,LOCATION_MZONE,0,1,nil,LOCATION_EXTRA)
-		and Duel.IsExistingMatchingCard(Card.IsSummonLocation,tp,0,LOCATION_MZONE,1,nil,LOCATION_EXTRA)
-end
-function c31444249.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local chkf=tp
-		local mg1=Duel.GetFusionMaterial(tp)
-		if c31444249.dmcon(tp) then
-			local sg=Duel.GetMatchingGroup(c31444249.filter0,tp,LOCATION_DECK,0,nil)
-			if sg:GetCount()>0 then
-				mg1:Merge(sg)
-				aux.FCheckAdditional=c31444249.fcheck
-				aux.GCheckAdditional=c31444249.gcheck
-			end
-		end
-		local res=Duel.IsExistingMatchingCard(c31444249.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
-		aux.FCheckAdditional=nil
-		aux.GCheckAdditional=nil
-		if not res then
-			local ce=Duel.GetChainMaterial(tp)
-			if ce~=nil then
-				local fgroup=ce:GetTarget()
-				local mg2=fgroup(ce,e,tp)
-				local mf=ce:GetValue()
-				res=Duel.IsExistingMatchingCard(c31444249.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,chkf)
-			end
-		end
-		return res
+
+--- @type FUSION_SPELL_PRE_SELECT_MAT_LOCATION_FUNCTION
+function s.pre_select_mat_location(tc,tp)
+	local location=LOCATION_HAND|LOCATION_MZONE
+	if Duel.IsExistingMatchingCard(function(c) return c:IsSummonLocation(LOCATION_EXTRA) end,tp,0,LOCATION_MZONE,1,nil)
+			and not Duel.IsExistingMatchingCard(function(c) return c:IsSummonLocation(LOCATION_EXTRA) end,tp,LOCATION_MZONE,0,1,nil) then
+		location=location|LOCATION_DECK
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	return location
 end
-function c31444249.spop(e,tp,eg,ep,ev,re,r,rp)
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp):Filter(c31444249.filter1,nil,e)
-	local exmat=false
-	if c31444249.dmcon(tp) then
-		local sg=Duel.GetMatchingGroup(c31444249.filter0,tp,LOCATION_DECK,0,nil)
-		if sg:GetCount()>0 then
-			mg1:Merge(sg)
-			exmat=true
-		end
+
+--- @type FUSION_FGCHECK_FUNCTION
+function s.fcheck(tp,mg,fc,mg_all)
+	--- At most 6 monsters from deck
+	if mg:FilterCount(function(c) return c:IsLocation(LOCATION_DECK) end,nil)>6 then
+		return false
 	end
-	if exmat then
-		aux.FCheckAdditional=c31444249.fcheck
-		aux.GCheckAdditional=c31444249.gcheck
-	end
-	local sg1=Duel.GetMatchingGroup(c31444249.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-	aux.FCheckAdditional=nil
-	aux.GCheckAdditional=nil
-	local mg2=nil
-	local sg2=nil
-	local ce=Duel.GetChainMaterial(tp)
-	if ce~=nil then
-		local fgroup=ce:GetTarget()
-		mg2=fgroup(ce,e,tp)
-		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(c31444249.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
-	end
-	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
-		local sg=sg1:Clone()
-		if sg2 then sg:Merge(sg2) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			if exmat then
-				aux.FCheckAdditional=c31444249.fcheck
-				aux.GCheckAdditional=c31444249.gcheck
-			end
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-			aux.FCheckAdditional=nil
-			aux.GCheckAdditional=nil
-			tc:SetMaterial(mat1)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-		end
-		tc:CompleteProcedure()
-	end
+	return true
 end
