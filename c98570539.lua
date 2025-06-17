23f8b614784363e491259d7b998f353b57e8c44d
Vury Leo <i@vuryleo.com>
2025-06-17 22:21:59 +0900

    Add 夢魔鏡 to new fusion

--- c98570539.lua
+++ c98570539.lua
@@ -1,96 +1,40 @@
 --混沌の夢魔鏡
-function c98570539.initial_effect(c)
+local s,id,o=GetID()
+function s.initial_effect(c)
 	aux.AddCodeList(c,74665651,1050355)
-	--activate
-	local e1=Effect.CreateEffect(c)
+	--fusion summon
+	local e1=FusionSpell.CreateSummonEffect(c,{
+		fusfilter=s.fusfilter,
+		pre_select_mat_location=s.pre_select_mat_location,
+		extra_target=s.extra_target
+	})
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON+CATEGORY_REMOVE)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
-	e1:SetCountLimit(1,98570539+EFFECT_COUNT_CODE_OATH)
-	e1:SetTarget(c98570539.target)
-	e1:SetOperation(c98570539.activate)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
 end
-function c98570539.filter0(c)
-	return c:IsType(TYPE_MONSTER) and c:IsCanBeFusionMaterial() and c:IsAbleToRemove()
+
+function s.fusfilter(c)
+	return c:IsSetCard(0x131)
 end
-function c98570539.filter1(c,e)
-	return not c:IsImmuneToEffect(e)
-end
-function c98570539.filter2(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x131) and (not f or f(c)) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
-end
-function c98570539.target(e,tp,eg,ep,ev,re,r,rp,chk)
+
+function s.extra_target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		local chkf=tp
-		local mg1=Duel.GetFusionMaterial(tp):Filter(Card.IsOnField,nil)
-		local mg2=nil
-		if Duel.IsEnvironment(74665651,PLAYER_ALL,LOCATION_FZONE) then
-			mg2=Duel.GetFusionMaterial(tp):Filter(Card.IsLocation,nil,LOCATION_HAND)
-			mg1:Merge(mg2)
-		end
-		if Duel.IsEnvironment(1050355,PLAYER_ALL,LOCATION_FZONE) then
-			mg2=Duel.GetMatchingGroup(c98570539.filter0,tp,LOCATION_GRAVE,0,nil)
-			mg1:Merge(mg2)
-		end
-		local res=Duel.IsExistingMatchingCard(c98570539.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
-		if not res then
-			local ce=Duel.GetChainMaterial(tp)
-			if ce~=nil then
-				local fgroup=ce:GetTarget()
-				local mg3=fgroup(ce,e,tp)
-				local mf=ce:GetValue()
-				res=Duel.IsExistingMatchingCard(c98570539.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg3,mf,chkf)
-			end
-		end
-		return res
+		return true
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_GRAVE)
 end
-function c98570539.activate(e,tp,eg,ep,ev,re,r,rp)
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp):Filter(Card.IsOnField,nil):Filter(c98570539.filter1,nil,e)
-	local mg2=nil
-	if Duel.IsEnvironment(74665651,PLAYER_ALL,LOCATION_FZONE) then
-		mg2=Duel.GetFusionMaterial(tp):Filter(Card.IsLocation,nil,LOCATION_HAND)
-		mg1:Merge(mg2)
+
+--- @type FUSION_SPELL_PRE_SELECT_MAT_LOCATION_FUNCTION
+function s.pre_select_mat_location(tc,tp)
+	local location=LOCATION_MZONE
+	--- if 聖光の夢魔鏡 in FZONE, add hand
+	if Duel.IsExistingMatchingCard(function(c) return c:IsCode(74665651) and c:IsFaceup() end,tp,LOCATION_FZONE,LOCATION_FZONE,1,nil) then
+		location=location|LOCATION_HAND
 	end
-	if Duel.IsEnvironment(1050355,PLAYER_ALL,LOCATION_FZONE) then
-		mg2=Duel.GetMatchingGroup(c98570539.filter0,tp,LOCATION_GRAVE,0,nil)
-		mg1:Merge(mg2)
+	--- if 闇黒の夢魔鏡 in FZONE, add grave
+	if Duel.IsExistingMatchingCard(function(c) return c:IsCode(1050355) and c:IsFaceup() end,tp,LOCATION_FZONE,LOCATION_FZONE,1,nil) then
+		location=location|LOCATION_GRAVE
 	end
-	local sg1=Duel.GetMatchingGroup(c98570539.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-	local mg3=nil
-	local sg2=nil
-	local ce=Duel.GetChainMaterial(tp)
-	if ce~=nil then
-		local fgroup=ce:GetTarget()
-		mg3=fgroup(ce,e,tp)
-		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(c98570539.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg3,mf,chkf)
-	end
-	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
-		local sg=sg1:Clone()
-		if sg2 then sg:Merge(sg2) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-			tc:SetMaterial(mat1)
-			local mat2=mat1:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
-			mat1:Sub(mat2)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.Remove(mat2,POS_FACEUP,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg3,nil,chkf)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-		end
-		tc:CompleteProcedure()
-	end
+	return location
 end
