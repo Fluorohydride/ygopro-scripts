a6a79cc40c1d48be76d5f000d29fe2c42b40def9
mercury233 <me@mercury233.me>
2025-06-19 11:58:42 +0800

    update Duel.ConfirmCards

--- c17016131.lua
+++ c17016131.lua
@@ -105,7 +105,7 @@ function c17016131.tgtg2(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c17016131.tgop2(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,0,LOCATION_EXTRA)
-	Duel.ConfirmCards(tp,g)
+	Duel.ConfirmCards(tp,g,true)
 	local tg=g:Filter(Card.IsAbleToGrave,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local tc=tg:Select(tp,1,1,nil):GetFirst()
