8db59684bd85088c0d72f34b9ba80930517b86d7
Vury Leo <i@vuryleo.com>
2025-06-25 15:27:09 +0000

    Add 錬装 to new fusion

--- c58549532.lua
+++ c58549532.lua
@@ -1,89 +1,42 @@
 --混錬装融合
-function c58549532.initial_effect(c)
+local s,id,o=GetID()
+function s.initial_effect(c)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,58549532+EFFECT_COUNT_CODE_OATH)
-	e1:SetTarget(c58549532.target)
-	e1:SetOperation(c58549532.activate)
+	local e1=FusionSpell.CreateSummonEffect(c,{
+		fusfilter=s.fusfilter,
+		pre_select_mat_location=LOCATION_HAND|LOCATION_MZONE|LOCATION_EXTRA,
+		additional_fcheck=s.fcheck,
+		fusion_spell_matfilter=s.fusion_spell_matfilter
+	})
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
 end
-function c58549532.filter0(c,e)
-	return c:IsCanBeFusionMaterial() and c:IsFaceup() and c:IsType(TYPE_PENDULUM) and not c:IsImmuneToEffect(e)
+
+function s.fusfilter(c)
+	return c:IsSetCard(0xe1)
 end
-function c58549532.filter1(c,e)
-	return not c:IsImmuneToEffect(e)
-end
-function c58549532.filter2(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and c:IsSetCard(0xe1) and (not f or f(c))
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
-end
-function c58549532.fcheck(tp,sg,fc)
-	return sg:GetClassCount(Card.GetLocation)==#sg
-end
-function c58549532.gcheck(sg)
-	return sg:GetClassCount(Card.GetLocation)==#sg
-end
-function c58549532.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local chkf=tp
-		local mg1=Duel.GetFusionMaterial(tp)
-		mg1:Merge(Duel.GetMatchingGroup(c58549532.filter0,tp,LOCATION_EXTRA,0,nil,e))
-		aux.FCheckAdditional=c58549532.fcheck
-		aux.GCheckAdditional=c58549532.gcheck
-		local res=Duel.IsExistingMatchingCard(c58549532.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
-		if not res then
-			local ce=Duel.GetChainMaterial(tp)
-			if ce~=nil then
-				local fgroup=ce:GetTarget()
-				local mg2=fgroup(ce,e,tp)
-				local mf=ce:GetValue()
-				res=Duel.IsExistingMatchingCard(c58549532.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,chkf)
-			end
-		end
-		aux.FCheckAdditional=nil
-		aux.GCheckAdditional=nil
-		return res
+
+--- @param c Card
+function s.fusion_spell_matfilter(c)
+	if c:IsLocation(LOCATION_EXTRA) and not (c:IsType(TYPE_PENDULUM) and c:IsFaceup()) then
+		return false
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	return true
 end
-function c58549532.activate(e,tp,eg,ep,ev,re,r,rp)
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp):Filter(c58549532.filter1,nil,e)
-	mg1:Merge(Duel.GetMatchingGroup(c58549532.filter0,tp,LOCATION_EXTRA,0,nil,e))
-	aux.FCheckAdditional=c58549532.fcheck
-	aux.GCheckAdditional=c58549532.gcheck
-	local sg1=Duel.GetMatchingGroup(c58549532.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-	local mg2=nil
-	local sg2=nil
-	local ce=Duel.GetChainMaterial(tp)
-	if ce~=nil then
-		local fgroup=ce:GetTarget()
-		mg2=fgroup(ce,e,tp)
-		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(c58549532.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
+
+--- @type FUSION_FGCHECK_FUNCTION
+function s.fcheck(tp,mg,fc,mg_all)
+	--- Can only have 1 monster from hand
+	if mg_all:FilterCount(function(c) return c:IsLocation(LOCATION_HAND) end,nil)>1 then
+		return false
 	end
-	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
-		local sg=sg1:Clone()
-		if sg2 then sg:Merge(sg2) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-			tc:SetMaterial(mat1)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-		end
-		tc:CompleteProcedure()
+	--- Can only have 1 monster from field
+	if mg_all:FilterCount(function(c) return c:IsOnField() end,nil)>1 then
+		return false
 	end
-	aux.FCheckAdditional=nil
-	aux.GCheckAdditional=nil
+	--- Can only have 1 monster from extra
+	if mg_all:FilterCount(function(c) return c:IsLocation(LOCATION_EXTRA) end,nil)>1 then
+		return false
+	end
+	return true
 end
--- c77693536.lua
+++ c77693536.lua
@@ -1,5 +1,6 @@
 --フルメタルフォーゼ・アルカエスト
-function c77693536.initial_effect(c)
+local s,id,o=GetID()
+function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xe1),aux.FilterBoolFunction(Card.IsFusionType,TYPE_NORMAL),true)
@@ -12,7 +13,7 @@ function c77693536.initial_effect(c)
 	c:RegisterEffect(e1)
 	--equip
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(77693536,0))
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_EQUIP)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
@@ -20,36 +21,42 @@ function c77693536.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
-	e2:SetCondition(c77693536.eqcon)
-	e2:SetTarget(c77693536.eqtg)
-	e2:SetOperation(c77693536.eqop)
+	e2:SetCondition(s.eqcon)
+	e2:SetTarget(s.eqtg)
+	e2:SetOperation(s.eqop)
 	c:RegisterEffect(e2)
 	--equip fusion material
 	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(EFFECT_EXTRA_FUSION_MATERIAL)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(LOCATION_SZONE,0)
-	e3:SetTarget(c77693536.mttg)
-	e3:SetValue(c77693536.mtval)
+	e3:SetTarget(s.mttg)
+	e3:SetValue(s.mtval)
+	e3:SetOperation(function() return FusionSpell.FUSION_OPERATION_INHERIT end)
 	c:RegisterEffect(e3)
 end
-function c77693536.eqcon(e,tp,eg,ep,ev,re,r,rp)
+
+function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()~=tp
 end
-function c77693536.eqfilter(c,tp)
+
+function s.eqfilter(c,tp)
 	return c:IsFaceup() and c:IsType(TYPE_EFFECT)
 		and (c:IsControler(tp) or c:IsAbleToChangeControler())
 end
-function c77693536.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c77693536.eqfilter(chkc,tp) and chkc~=e:GetHandler() end
+
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.eqfilter(chkc,tp) and chkc~=e:GetHandler() end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(c77693536.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler(),tp) end
+		and Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler(),tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,c77693536.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,e:GetHandler(),tp)
+	local g=Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,e:GetHandler(),tp)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
 end
-function c77693536.eqop(e,tp,eg,ep,ev,re,r,rp)
+
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
@@ -68,17 +75,20 @@ function c77693536.eqop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_OWNER_RELATE)
 	e2:SetCode(EFFECT_EQUIP_LIMIT)
-	e2:SetValue(c77693536.eqlimit)
+	e2:SetValue(s.eqlimit)
 	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 	tc:RegisterEffect(e2)
 end
-function c77693536.eqlimit(e,c)
+
+function s.eqlimit(e,c)
 	return e:GetOwner()==c
 end
-function c77693536.mttg(e,c)
+
+function s.mttg(e,c)
 	return c:GetEquipTarget()==e:GetHandler() and c:GetOriginalType()&TYPE_MONSTER~=0
 end
-function c77693536.mtval(e,c)
+
+function s.mtval(e,c)
 	if not c then return false end
 	return c:IsSetCard(0xe1)
 end
