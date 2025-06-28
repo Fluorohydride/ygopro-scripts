adad86e75933400cff34d8dc8d79c85e78642ddf
Vury Leo <i@vuryleo.com>
2025-06-28 15:20:10 +0000

    Add ジェムナイト to new fusion

--- c39261576.lua
+++ c39261576.lua
@@ -1,112 +1,66 @@
 --パーティカル・フュージョン
-function c39261576.initial_effect(c)
+local s,id,o=GetID()
+function s.initial_effect(c)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(c39261576.target)
-	e1:SetOperation(c39261576.activate)
-	c:RegisterEffect(e1)
+	local e1=FusionSpell.RegisterSummonEffect(c,{
+		fusfilter=s.fusfilter,
+		pre_select_mat_location=LOCATION_MZONE,
+		stage_x_operation=s.stage_x_operation
+	})
 	--atk up
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_ATKCHANGE)
-	e2:SetDescription(aux.Stringid(39261576,0))
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCode(EVENT_CUSTOM+39261576)
+	e2:SetCode(EVENT_CUSTOM+id)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCondition(c39261576.atkcon)
-	e2:SetCost(c39261576.atkcost)
-	e2:SetTarget(c39261576.atktg)
-	e2:SetOperation(c39261576.atkop)
+	e2:SetCondition(s.atkcon)
+	e2:SetCost(s.atkcost)
+	e2:SetTarget(s.atktg)
+	e2:SetOperation(s.atkop)
 	e2:SetLabelObject(e1)
 	c:RegisterEffect(e2)
 end
-function c39261576.filter1(c,e)
-	return c:IsOnField() and not c:IsImmuneToEffect(e)
+
+function s.fusfilter(c)
+	return c:IsSetCard(0x1047)
 end
-function c39261576.filter2(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x1047) and (not f or f(c))
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
-end
-function c39261576.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local chkf=tp
-		local mg1=Duel.GetFusionMaterial(tp):Filter(Card.IsOnField,nil)
-		local res=Duel.IsExistingMatchingCard(c39261576.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
-		if not res then
-			local ce=Duel.GetChainMaterial(tp)
-			if ce~=nil then
-				local fgroup=ce:GetTarget()
-				local mg2=fgroup(ce,e,tp)
-				local mf=ce:GetValue()
-				res=Duel.IsExistingMatchingCard(c39261576.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,chkf)
-			end
-		end
-		return res
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
-end
-function c39261576.activate(e,tp,eg,ep,ev,re,r,rp)
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp):Filter(c39261576.filter1,nil,e)
-	local sg1=Duel.GetMatchingGroup(c39261576.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-	local mg2=nil
-	local sg2=nil
-	local ce=Duel.GetChainMaterial(tp)
-	if ce~=nil then
-		local fgroup=ce:GetTarget()
-		mg2=fgroup(ce,e,tp)
-		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(c39261576.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
-	end
-	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
-		local sg=sg1:Clone()
-		if sg2 then sg:Merge(sg2) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-			tc:SetMaterial(mat1)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-			e:SetLabelObject(tc)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-			e:SetLabelObject(tc)
-		end
-		tc:CompleteProcedure()
+
+--- @type FUSION_SPELL_STAGE_X_CALLBACK_FUNCTION
+function s.stage_x_operation(e,tc,tp,stage)
+	if stage==FusionSpell.STAGE_AT_SUMMON_OPERATION_FINISH then
+		e:SetLabelObject(tc)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_CHAIN_END)
-		e1:SetOperation(c39261576.evop)
+		e1:SetOperation(s.evop)
 		e1:SetLabelObject(e)
 		Duel.RegisterEffect(e1,tp)
 	end
 end
-function c39261576.atkcon(e,tp,eg,ep,ev,re,r,rp)
+
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return re==e:GetLabelObject()
 end
-function c39261576.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemove() end
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
-function c39261576.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local tc=eg:GetFirst()
 	local mat=tc:GetMaterial()
 	if chkc then return chkc:IsSetCard(0x1047) and mat:IsContains(chkc) end
 	if chk==0 then return mat:IsExists(Card.IsSetCard,1,nil,0x1047) end
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(39261576,1))
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
 	local g=mat:FilterSelect(tp,Card.IsSetCard,1,1,nil,0x1047)
 	tc:CreateEffectRelation(e)
 	Duel.SetTargetCard(g)
 end
-function c39261576.atkop(e,tp,eg,ep,ev,re,r,rp)
+
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local sc=eg:GetFirst()
 	if not sc:IsRelateToEffect(e) or sc:IsFacedown() then return end
 	local tc=Duel.GetFirstTarget()
@@ -118,10 +72,11 @@ function c39261576.atkop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	sc:RegisterEffect(e1)
 end
-function c39261576.evop(e,tp,eg,ep,ev,re,r,rp)
+
+function s.evop(e,tp,eg,ep,ev,re,r,rp)
 	local te=e:GetLabelObject()
 	local tc=te:GetLabelObject()
-	Duel.RaiseEvent(tc,EVENT_CUSTOM+39261576,te,0,tp,tp,0)
+	Duel.RaiseEvent(tc,EVENT_CUSTOM+id,te,0,tp,tp,0)
 	te:SetLabelObject(nil)
 	e:Reset()
 end
--- c71422989.lua
+++ c71422989.lua
@@ -1,85 +1,73 @@
 --吸光融合
-function c71422989.initial_effect(c)
+local s,id,o=GetID()
+function s.initial_effect(c)
+	-- fusion
+	local e0=FusionSpell.CreateSummonEffect(c,{
+		fusfilter=s.fusfilter,
+		matfilter=aux.NecroValleyFilter(),
+		mat_operation_code_map={
+			{ [LOCATION_REMOVED]=FusionSpell.FUSION_OPERATION_GRAVE },
+			{ [0xff]=FusionSpell.FUSION_OPERATION_BANISH }
+		},
+	})
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,71422989+EFFECT_COUNT_CODE_OATH)
-	e1:SetCost(c71422989.cost)
-	e1:SetTarget(c71422989.target)
-	e1:SetOperation(c71422989.activate)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	e1:SetLabelObject(e0)
 	c:RegisterEffect(e1)
-	Duel.AddCustomActivityCounter(71422989,ACTIVITY_SPSUMMON,c71422989.counterfilter)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,s.counterfilter)
 end
-function c71422989.counterfilter(c)
+
+function s.counterfilter(c)
 	return c:IsSetCard(0x1047)
 end
-function c71422989.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetCustomActivityCount(71422989,tp,ACTIVITY_SPSUMMON)==0 end
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(c71422989.splimit)
+	e1:SetTarget(s.splimit)
 	Duel.RegisterEffect(e1,tp)
 end
-function c71422989.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not c:IsSetCard(0x1047)
 end
-function c71422989.filter(c)
+
+function s.filter(c)
 	return c:IsSetCard(0x1047) and c:IsAbleToHand()
 end
-function c71422989.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c71422989.filter,tp,LOCATION_DECK,0,1,nil) end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c71422989.filter1(c,e)
-	return c:IsAbleToRemove() and not c:IsImmuneToEffect(e)
+
+
+function s.fusfilter(c)
+	return c:IsSetCard(0x1047)
 end
-function c71422989.filter2(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x1047) and (not f or f(c))
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
-end
-function c71422989.activate(e,tp,eg,ep,ev,re,r,rp)
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c71422989.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
-		local chkf=tp
-		local mg1=Duel.GetFusionMaterial(tp):Filter(c71422989.filter1,nil,e)
-		local sg1=Duel.GetMatchingGroup(c71422989.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-		local mg2=nil
-		local sg2=nil
-		local ce=Duel.GetChainMaterial(tp)
-		if ce~=nil then
-			local fgroup=ce:GetTarget()
-			mg2=fgroup(ce,e,tp)
-			local mf=ce:GetValue()
-			sg2=Duel.GetMatchingGroup(c71422989.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
-		end
-		if (sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0)) and Duel.SelectYesNo(tp,aux.Stringid(71422989,0)) then
+		local fusion_effect=e:GetLabelObject()
+		if fusion_effect:GetTarget()(e,tp,eg,ep,ev,re,r,rp,0) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 			Duel.BreakEffect()
-			local sg=sg1:Clone()
-			if sg2 then sg:Merge(sg2) end
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-			local tg=sg:Select(tp,1,1,nil)
-			local tc=tg:GetFirst()
-			if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-				local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-				tc:SetMaterial(mat1)
-				Duel.Remove(mat1,POS_FACEUP,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-				Duel.BreakEffect()
-				Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-			else
-				local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
-				local fop=ce:GetOperation()
-				fop(ce,e,tp,tc,mat2)
-			end
-			tc:CompleteProcedure()
+			fusion_effect:GetOperation()(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
