3486c5f41d4f4ce2e4879f4310c1992ee3df7ea9
Vee4 <144184504+VforViolin@users.noreply.github.com>
2025-07-14 11:52:03 -0400

    Update c32484853.lua
    Fix category

--- c32484853.lua
+++ c32484853.lua
@@ -4,7 +4,7 @@ function s.initial_effect(c)
 	--destroy
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
