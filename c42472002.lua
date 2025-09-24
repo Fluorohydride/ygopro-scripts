d4970aa1e330071bf55118b9664ee87ee9dd879d
Wind2009-Louse <wind.jackey@qq.com>
2025-09-24 15:41:15 +0800

    Fix extra summon effect (#2999)

--- c42472002.lua
+++ c42472002.lua
@@ -39,7 +39,6 @@ function c42472002.effop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(c42472002.sumcon)
 	e1:SetTarget(c42472002.sumtg)
 	e1:SetOperation(c42472002.sumop)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
@@ -53,21 +52,22 @@ function c42472002.effop(e,tp,eg,ep,ev,re,r,rp)
 		rc:RegisterEffect(e2,true)
 	end
 end
-function c42472002.sumcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,42472002)==0 and Duel.GetTurnPlayer()==tp
-end
 function c42472002.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanSummon(tp) and Duel.IsPlayerCanAdditionalSummon(tp) end
+	if chk==0 then return Duel.IsPlayerCanSummon(tp) and Duel.IsPlayerCanAdditionalSummon(tp)
+		and Duel.GetFlagEffect(tp,42472002)==0 and Duel.GetTurnPlayer()==tp end
 end
 function c42472002.sumop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(42472002,1))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
-	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
+	if Duel.GetFlagEffect(tp,42472002)==0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(aux.Stringid(42472002,1))
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+		e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+		Duel.RegisterFlagEffect(tp,42472002,RESET_PHASE+PHASE_END,0,1)
+	end
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -76,7 +76,6 @@ function c42472002.sumop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetTarget(c42472002.splimit)
 	e2:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e2,tp)
-	Duel.RegisterFlagEffect(tp,42472002,RESET_PHASE+PHASE_END,0,1)
 end
 function c42472002.splimit(e,c)
 	return not c:IsSetCard(0x107a) and c:IsLocation(LOCATION_EXTRA)
