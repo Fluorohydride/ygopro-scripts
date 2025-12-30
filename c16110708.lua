791860ccfe59194eaaa3d587e29e07d121f37a7c
Wind2009-Louse <wind.jackey@qq.com>
2025-12-30 13:46:39 +0800

    Fix SNo.38 タイタニック・ギャラクシー (#3093)

--- c16110708.lua
+++ c16110708.lua
@@ -110,6 +110,7 @@ function s.datop(e,tp,eg,ep,ev,re,r,rp)
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_DIRECT_ATTACK)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e2)
 	end
