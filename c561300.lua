299a8ab656f0cc8a42c613fb17b8879764433a21
salix5 <salix5566@gmail.com>
2026-01-01 00:26:49 +0800

    fix str3 of 猛毒マムシ

--- c561300.lua
+++ c561300.lua
@@ -25,7 +25,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 	--destroy
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_DESTROY)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
