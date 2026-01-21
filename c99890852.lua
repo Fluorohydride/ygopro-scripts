4e664cfe270566c740b89e241141a89f494d664b
Mercury233 <me@mercury233.me>
2026-01-21 14:00:24 +0800

    fix multi target effects (#3109)
    * Update Target_Option
    
    Co-authored-by: Mercury233 <me@mercury233.me>
    
    Co-authored-by: 想象力很好的毛虫 <71340108+Crescent0v0@users.noreply.github.com>

--- c99890852.lua
+++ c99890852.lua
@@ -13,32 +13,44 @@ function c99890852.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c99890852.filter(c,tp)
-	return c:IsFaceup() and c:IsSetCard(0xfb)
-		and c:IsAbleToHand() and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,c)
+	return c99890852.thfilter(c,tp)
+		and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,c)
+end
+function c99890852.thfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0xfb) and c:IsControler(tp)
+		and c:IsAbleToHand() and not c:IsExtraDeckMonster()
+end
+function c99890852.gcheck(g,tp)
+	return g:IsExists(c99890852.thfilter,1,nil,tp)
 end
 function c99890852.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	if chk==0 then return Duel.IsExistingTarget(c99890852.filter,tp,LOCATION_MZONE,0,1,nil,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-	local g=Duel.SelectTarget(tp,c99890852.filter,tp,LOCATION_MZONE,0,1,1,nil,tp)
-	e:SetLabelObject(g:GetFirst())
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,g)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	g=g:Filter(Card.IsCanBeEffectTarget,nil,e)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local sg=g:SelectSubGroup(tp,c99890852.gcheck,false,2,2,tp)
+	Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
 end
 function c99890852.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
-	if g:GetCount()~=2 then return end
-	local hc=g:GetFirst()
-	local tc=g:GetNext()
-	if hc~=e:GetLabelObject() then tc,hc=hc,tc end
-	if hc:IsFaceup() and tc:IsFaceup() and Duel.SendtoHand(hc,nil,REASON_EFFECT)~=0 then
-		local atk=hc:GetBaseAttack()
+	local g=Duel.GetTargetsRelateToChain()
+	if #g==0 then return end
+	local tg=g:Filter(c99890852.thfilter,nil,tp)
+	local tc1=tg:GetFirst()
+	if #tg>1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+		tc1=tg:Select(tp,1,1,nil):GetFirst()
+	end
+	if not tc1 then return end
+	local tc2=(g-tc1):GetFirst()
+	if Duel.SendtoHand(tc1,nil,REASON_EFFECT)~=0 and tc2 and tc2:IsFaceup() then
+		local atk=tc1:GetBaseAttack()
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(atk)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
+		tc2:RegisterEffect(e1)
 	end
 end
