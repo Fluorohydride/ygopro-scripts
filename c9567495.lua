d3c4f49f57b3589d86da497b93937444051160f5
a597449807 <148566174+a597449807@users.noreply.github.com>
2026-01-24 16:42:51 +0800

    Fix 闘龍門
    修复①效果关于“升降攻击力和守备力”部分不应该持续到“回合结束”的问题。
    
    原文效果：这张卡的攻击力上升取除的超量素材种类（怪兽·魔法·陷阱）×1000，对方场上有怪兽存在的场合，那些攻击力下降所上升的数值。

--- c9567495.lua
+++ c9567495.lua
@@ -49,7 +49,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_ATTACK_ALL)
 		e1:SetValue(1)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		c:RegisterEffect(e1)
 		if #og>0 then
 			local ct=0
@@ -70,7 +70,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 				e2:SetType(EFFECT_TYPE_SINGLE)
 				e2:SetCode(EFFECT_UPDATE_ATTACK)
 				e2:SetValue(ct*1000)
-				e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+				e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 				c:RegisterEffect(e2)
 				--atkdown
 				if not c:IsHasEffect(EFFECT_REVERSE_UPDATE) then
