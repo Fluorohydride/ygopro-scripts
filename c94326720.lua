91390dff6150578980a8ac4ce45af67cd8a8417b
八宫一月 <135235980+kiritosoft@users.noreply.github.com>
2026-02-04 00:27:48 +0800

    Update c94326720.lua

--- c94326720.lua
+++ c94326720.lua
@@ -58,7 +58,8 @@ function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToChain() and not tc:IsLevel(e:GetLabel()) then
+	local lv=e:GetLabel()
+	if tc:IsFaceup() and tc:IsRelateToChain() and not tc:IsLevel(lv) then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CHANGE_LEVEL)
