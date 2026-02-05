b4ae012b600efb05ad14000ed240763c4d03aa21
Mercury233 <me@mercury233.me>
2026-02-05 14:34:33 +0800

    Apply suggestion from @mercury233

--- c94326720.lua
+++ c94326720.lua
@@ -63,7 +63,7 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CHANGE_LEVEL)
-		e1:SetValue(e:GetLabel())
+		e1:SetValue(lv)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
