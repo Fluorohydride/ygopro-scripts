9716025387cfdd5c1fdf8dda95c0d8f75e7e527f
Wind2009-Louse <wind.jackey@qq.com>
2026-02-10 15:34:08 +0800

    Fix 相剣軍師－龍淵

--- c93490856.lua
+++ c93490856.lua
@@ -14,7 +14,7 @@ function c93490856.initial_effect(c)
 	--damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(93490856,1))
-	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetCategory(CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetCode(EVENT_BE_MATERIAL)
