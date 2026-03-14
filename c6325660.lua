b9ad68144b8ea913c569b29ccadb540c2d2ee040
a597449807 <148566174+a597449807@users.noreply.github.com>
2026-03-14 21:33:48 +0800

    Update c6325660.lua

--- c6325660.lua
+++ c6325660.lua
@@ -38,6 +38,8 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 	if e:GetHandler():IsStatus(STATUS_ACT_FROM_HAND) then
 		e:SetLabel(100)
+	else
+		e:SetLabel(0)
 	end
 end
 function s.spfilter(c,e,tp)
@@ -67,7 +69,6 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetTargetRange(1,0)
 		e1:SetValue(s.aclimit)
 		Duel.RegisterEffect(e1,tp)
-		e:SetLabel(0)
 	end
 end
 function s.aclimit(e,re,tp)
