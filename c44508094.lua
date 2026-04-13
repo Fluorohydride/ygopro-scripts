0ba09ae3479600d70cb647a886adf2c5dbbb7668
Vee4 <144184504+VforViolin@users.noreply.github.com>
2026-04-13 17:52:40 -0400

    Fix Stardust Dragon

--- c44508094.lua
+++ c44508094.lua
@@ -42,7 +42,7 @@ function c44508094.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Group.CreateGroup()
 	local c=e:GetHandler()
 	if c:IsReleasable() then g:AddCard(c) end
-	if c:IsSetCard(0xa3) then g:Merge(Duel.GetMatchingGroup(s.excostfilter,tp,LOCATION_GRAVE,0,nil,tp)) end
+	if c:IsSetCard(0xa3) then g:Merge(Duel.GetMatchingGroup(c44508094.excostfilter,tp,LOCATION_GRAVE,0,nil,tp)) end
 	if chk==0 then return #g>0 end
 	local tc
 	if #g>1 then
