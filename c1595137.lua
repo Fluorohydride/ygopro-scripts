41326d2204ef46cefc8476a8bdf7ffc3e284aa67
Wind2009-Louse <wind.jackey@qq.com>
2026-05-13 23:41:27 +0800

    Fix Evolved Daneen

--- c1595137.lua
+++ c1595137.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--recover
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_RECOVER)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_CUSTOM+id)
