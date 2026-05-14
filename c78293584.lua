d4bed2ee9a53245656a5d47deb7913fa4618ab52
Tad-Done <tjy140896@163.com>
2026-05-14 22:37:17 +0800

    Fix フィリアス・ディアベル
    Fix the issue where  ⌈フィリアス・ディアベル⌋ would prevent ⌈ディアベル⌋ monsters from being affected by other continuous attack-changing effects under specific situations.

--- c78293584.lua
+++ c78293584.lua
@@ -61,7 +61,6 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		for tc in aux.Next(g) do
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
 			e1:SetValue(500)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
