d0f0961cdb0556869bb69d5de20591a9e9941565
Mercury233 <me@mercury233.me>
2026-07-25 17:42:25 +0800

    Update c40041559.lua

--- c40041559.lua
+++ c40041559.lua
@@ -42,7 +42,9 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	if c:IsRelateToChain() then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToChain() and tc:IsFaceup() and tc:IsLocation(LOCATION_MZONE) and not tc:IsImmuneToEffect(e) then
 		local e1=Effect.CreateEffect(c)
