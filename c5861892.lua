cffde36b2ed2269ecc650ad98fe58cce3660c49f
Vee4 <144184504+VforViolin@users.noreply.github.com>
2026-08-02 20:46:43 -0400

    Errata for "Arcana Force EX - The Light Ruler"
    https://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=4&cid=7344&request_locale=ja
    
    Update script to match the errata, reducing Atk first, and only negating afterwards if successful.

--- c5861892.lua
+++ c5861892.lua
@@ -109,15 +109,15 @@ function c5861892.negop(e,tp,eg,ep,ev,re,r,rp)
 	if c:IsFacedown() or c:GetAttack()<1000 or not c:IsRelateToEffect(e) or Duel.GetCurrentChain()~=ev+1 then
 		return
 	end
-	if Duel.NegateActivation(ev) then
-		if re:GetHandler():IsRelateToEffect(re) then
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(-1000)
+	c:RegisterEffect(e1)
+	if re:GetHandler():IsRelateToEffect(re) and not c:IsHasEffect(EFFECT_REVERSE_UPDATE) then
+		if Duel.NegateActivation(ev) then
 			Duel.Destroy(re:GetHandler(),REASON_EFFECT)
 		end
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(-1000)
-		c:RegisterEffect(e1)
 	end
 end
