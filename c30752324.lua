737f2a8d44d3b5a21552a3d4b4813be89c7ff89c
Wind2009-Louse <wind.jackey@qq.com>
2026-09-23 11:09:08 +0800

    Fix 古の秘儀

--- c30752324.lua
+++ c30752324.lua
@@ -21,7 +21,7 @@ function s.spfilter2(c,e,tp)
 	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.cfilter(c)
-	return c:IsFaceup() and c:IsAllCardType(TYPE_NORMAL+TYPE_MONSTER)
+	return c:IsFaceup() and c:IsAllCardTypes(TYPE_NORMAL+TYPE_MONSTER)
 end
 function s.desfilter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP)
